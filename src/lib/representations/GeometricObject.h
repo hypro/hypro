ff1ccd0cdccff1afb1740fc39f6854f14cea89e3
benedikt.seidl <benedikt.seidl@googlemail.com>
2013-09-10 18:48:04 +0000

    added namespace to GeometricObject

--- src/lib/representations/GeometricObject.h
+++ src/lib/representations/GeometricObject.h
@@ -8,18 +8,21 @@
 #ifndef GEOMETRIC_OBJECT_H_
 #define GEOMETRIC_OBJECT_H_
 
-template<class T, class number>
-class GeometricObject
+namespace hypro
 {
-public:
-	virtual unsigned int get_dimension() const = 0;
-	
-	virtual void linear_transformation(T & result /*, ... */) const = 0;
-	virtual void minkowski_sum(T & result, const T & t) const = 0;
-	virtual bool intersection(T & result, const T & t) const = 0;
-	virtual void enclosure(T & result, const T & t) const = 0;
-	virtual bool contains(const vector<number> point) const = 0;
-	// virtual bool empty() const = 0;
-};
+	template<class T, class number>
+	class GeometricObject
+	{
+	public:
+		virtual unsigned int get_dimension() const = 0;
+		
+		virtual void linear_transformation(T & result /*, ... */) const = 0;
+		virtual void minkowski_sum(T & result, const T & t) const = 0;
+		virtual bool intersection(T & result, const T & t) const = 0;
+		virtual void enclosure(T & result, const T & t) const = 0;
+		virtual bool contains(const vector<number> point) const = 0;
+		// virtual bool empty() const = 0;
+	};
+}
 
 #endif /*GEOMETRIC_OBJECT_H_*/
