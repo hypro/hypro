619b8cb4079b2217d242ae6aa3cab6dce70abb74
sschupp <stefan.schupp@cs.rwth-aachen.de>
2013-11-22 15:46:47 +0000

    Removed defines as they are obsolete in the current version of the tests.

--- src/test/defines.h
+++ src/test/defines.h
@@ -6,11 +6,11 @@
 
 #pragma once
 
-#include "numbers/float/double_wrapper_t.h"
-#include "numbers/float/mpfr_wrapper_t.h"
-#include "numbers/int/int_wrapper_t.h"
-#include "numbers/HYPRO_FLOAT_T.h"
-#include "numbers/HYPRO_INT_T.h"
+//#include "numbers/float/double_wrapper_t.h"
+//#include "numbers/float/mpfr_wrapper_t.h"
+//#include "numbers/int/int_wrapper_t.h"
+//#include "numbers/HYPRO_FLOAT_T.h"
+//#include "numbers/HYPRO_INT_T.h"
 
 //typedef hypro::FLOAT_T<hypro::mpfr_wrapper_t> hypro_float_t;
 //typedef hypro::HYPRO_INT_T<hypro::int_wrapper_t> hypro_int_t;
