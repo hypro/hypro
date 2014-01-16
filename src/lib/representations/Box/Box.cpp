f735b2e613a77e8b6111553762175a45d5ca1037
sschupp <stefan.schupp@cs.rwth-aachen.de>
2014-01-16 16:44:08 +0000

    added Eigen3 support and started to implement boxes.

---
+++ src/lib/representations/Box/Box.cpp
@@ -0,0 +1,9 @@
+//
+//  Box.cpp
+//  HyPro
+//
+//  Created by Stefan Schupp on 16/01/14.
+//  Copyright (c) 2014 Stefan Schupp. All rights reserved.
+//
+
+#include "Box.h"
---
+++ src/lib/representations/Box/Box.h
@@ -0,0 +1,46 @@
+/**
+ * The class which represents a box.
+ *
+ * @file Box.h
+ * @author Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
+ *
+ * @since	2014-01-16
+ * @version 2014-01-16
+ */
+
+#pragma once
+
+#include <Eigen/dense>
+
+template<typename Number>
+class Box : GeometricObject<Box, Number>
+{
+	/***************************************************************************
+	 * Members
+	 **************************************************************************/
+protected:
+	MatrixXd mBoundaries;
+	
+public:
+	/***************************************************************************
+	 * Constructors
+	 **************************************************************************/
+	
+	Box();
+	
+	Box(unsigned dimension) : mBoundaries(dimension)
+	{}
+	
+	/***************************************************************************
+	 * General interface
+	 **************************************************************************/
+	
+	unsigned int get_dimension() const;
+	
+	void linear_transformation(Box& result /*, ... */) const;
+	void minkowski_sum(Box& result, const Box& rhs) const;
+	bool intersect(Box& result, const Box& rhs) const;
+	void hull(Box& result, const Box& rhs) const;
+	bool contains(const Point <Number> point) const;
+	void unite(Box& result, const Box& rhs) const;
+};
