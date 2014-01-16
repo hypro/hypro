400481c5d1c773a17c660682d12fa48bf0d35945
sschupp <stefan.schupp@cs.rwth-aachen.de>
2014-01-16 16:57:03 +0000

    Fixed stuff.

--- src/lib/representations/Box/Box.cpp
+++ src/lib/representations/Box/Box.cpp
@@ -1,9 +1,49 @@
-//
-//  Box.cpp
-//  HyPro
-//
-//  Created by Stefan Schupp on 16/01/14.
-//  Copyright (c) 2014 Stefan Schupp. All rights reserved.
-//
+/**
+ * Implementation of the box.
+ *
+ * @file Box.cpp
+ * @author Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
+ *
+ * @since	2014-01-16
+ * @version 2014-01-16
+ */
 
 #include "Box.h"
+
+unsigned int get_dimension() const;
+
+template<typename Number>
+void Box<Number>::linear_transformation(Box<Number>& result /*, ... */) const
+{
+	
+}
+
+template<typename Number>
+void Box<Number>::minkowski_sum(Box<Number>& result, const Box<Number>& rhs) const
+{
+	
+}
+
+template<typename Number>
+bool Box<Number>::intersect(Box<Number>& result, const Box<Number>& rhs) const
+{
+	
+}
+
+template<typename Number>
+void Box<Number>::hull(Box<Number>& result, const Box<Number>& rhs) const
+{
+	
+}
+
+template<typename Number>
+bool Box<Number>::contains(const Point <Number> point) const
+{
+	
+}
+
+template<typename Number>
+void Box<Number>::unite(Box<Number>& result, const Box<Number>& rhs) const
+{
+	
+}
