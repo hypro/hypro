57cc19598519f98e0e4b35778b7b2911ae7a6790
benedikt.seidl <benedikt.seidl@googlemail.com>
2014-03-17 10:52:37 +0000

    Test file for orthogonal polyhedron, removed some build files...

---
+++ src/test/representations/OrthogonalPolyhedronTest.cpp
@@ -0,0 +1,38 @@
+/**
+ * @file    OrthogonalPolyhedronTest.cpp
+ * 
+ * @covers  OrthogonalPolyhedron
+ * 
+ * @author  Benedikt Seidl
+ * 
+ * @since   2014-03-17
+ * @version 2014-03-17
+ */
+
+#include "gtest/gtest.h"
+#include "../defines.h"
+#include "../../lib/datastructures/Point.h"
+#include "../../lib/representations/OrthogonalPolyhedron/OrthogonalPolyhedron.h"
+
+using namespace hypro;
+
+class OrthogonalPolyhedronTest : public ::testing::Test
+{
+protected:
+    virtual void SetUp()
+    {
+        
+    }
+	
+    virtual void TearDown()
+    {
+    }
+
+    OrthogonalPolyhedron<number_t> polyhedron;
+};
+
+TEST_F(OrthogonalPolyhedronTest, Constructor)
+{
+    OrthogonalPolyhedron<number_t> op1 = OrthogonalPolyhedron<number_t>();
+    SUCCEED();
+}
