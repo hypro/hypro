775892a375655449d9a4b9742b2f7236163db2e6
ckugler <christopher.kugler@rwth-aachen.de>
2014-05-26 20:29:36 +0000

    - followup changes

--- src/test/datastructure/HybridAutomataTest.cpp
+++ src/test/datastructure/HybridAutomataTest.cpp
@@ -18,7 +18,7 @@ protected:
     	/*
     	 * Location
     	 */
-    	/*
+
     	vector_t<double> invariantVec;
     	//invariantVec.insert( std::make_pair(x, carl::FLOAT_T<double>(10)) );
     	//invariantVec.insert( std::make_pair(y, carl::FLOAT_T<double>(20)) );
@@ -26,14 +26,13 @@ protected:
     	invariantVec.insert( std::make_pair(y, 20) );
 
     	operator_e invariantOp = LEQ;
-*/
-    	//matrix_t<double> invariantMat;
-    	Eigen::Matrix<carl::FLOAT_T<double>, Eigen::Dynamic, Eigen::Dynamic> invariantMat;
+
+    	matrix_t<double> invariantMat(2,2);
     	invariantMat(0,0) = 2;
-    	//invariantMat(0,1) = 0;
-    	//invariantMat(1,0) = 0;
-    	//invariantMat(1,1) = 3;
-/*
+    	invariantMat(0,1) = 0;
+    	invariantMat(1,0) = 0;
+    	invariantMat(1,1) = 3;
+
     	loc1.setInvariant(&invariantMat,invariantVec,invariantOp);
 
     	struct Location<double>::invariant inv;
@@ -43,7 +42,7 @@ protected:
 
     	loc2.setInvariant(inv);
 
-    	matrix_t<double> locationMat;
+    	matrix_t<double> locationMat(2,2);
     	locationMat(0,0) = 2;
     	locationMat(0,1) = 0;
     	locationMat(1,0) = 0;
@@ -55,8 +54,8 @@ protected:
     	/*
     	 * Transition
     	 */
-    	/*
-    	struct Transition<double>::guard guard;
+
+    	struct hypro::Transition<double>::guard guard;
     	guard.op = inv.op;
     	guard.mat = inv.mat;
     	guard.vec = inv.vec;
@@ -68,7 +67,7 @@ protected:
     	/*
     	 * Hybrid Automaton
     	 */
-    	/*
+
     	hypro::Location<double>* locations[] = {&loc1, &loc2};
     	std::set<hypro::Location<double>*> locSet(locations, locations+2);
 
@@ -82,9 +81,11 @@ protected:
     	std::set<hypro::Transition<double>*> transSet(transition, transition+1);
 
     	hybrid.setTransitions(transSet);
+    	//loc1.setTransitions(transSet);
 
     	//Polytope for InitialValuation
-    	Point<double>::coordinates_map coordinates;
+    	//Point<double>::coordinates_map coordinates;
+    	vector_t<double> coordinates;
         //coordinates.insert( std::make_pair(x, FLOAT_T<double>(2)) );
         //coordinates.insert( std::make_pair(y, FLOAT_T<double>(3)) );
         coordinates.insert( std::make_pair(x, 2) );
@@ -94,7 +95,7 @@ protected:
     	hypro::Polytope<double> poly = Polytope<double>(p1);
 
     	hybrid.setValuation(poly);
-    	*/
+
 
     }
 
@@ -109,7 +110,7 @@ protected:
     // Location<double> loc1; ?
     Location<double> loc1 = Location<double>();
     Location<double> loc2 = Location<double>();
-    Transition<double> trans = Transition<double>();
+    hypro::Transition<double> trans = hypro::Transition<double>();
     HybridAutomaton<double> hybrid = HybridAutomaton<double>();
 
     Point<double> p1;
@@ -130,8 +131,8 @@ TEST_F(HybridAutomataTest, LocationConstructorTest)
 
 TEST_F(HybridAutomataTest, LocationTest)
 {
-    //EXPECT_EQ(loc1.invariant().op,LEQ);
-    //EXPECT_EQ(1,1);
+    EXPECT_EQ(loc1.invariant().op,LEQ);
+    EXPECT_EQ(1,2);
 }
 
 TEST_F(HybridAutomataTest, TransitionTest)
