2a789a51ed3c550c59c11ba28bdf181bea2eb57e
sschupp <stefan.schupp@cs.rwth-aachen.de>
2014-06-12 14:13:00 +0000

    finished example.

--- examples/example_matrixPolyhedra.cpp
+++ examples/example_matrixPolyhedra.cpp
@@ -4,11 +4,12 @@ int main(int argc, char** argv) {
     
     using namespace Parma_Polyhedra_Library::IO_Operators;
     
-    Parma_Polyhedra_Library::C_Polyhedron mPolyhedron = Parma_Polyhedra_Library::C_Polyhedron(2, Parma_Polyhedra_Library::EMPTY);
+    Parma_Polyhedra_Library::C_Polyhedron mPolyhedron = Parma_Polyhedra_Library::C_Polyhedron(2, Parma_Polyhedra_Library::UNIVERSE);
     Parma_Polyhedra_Library::Variable a(0);
     Parma_Polyhedra_Library::Variable b(1);
     
     Parma_Polyhedra_Library::Linear_Expression polynom;
+    polynom.set_space_dimension(2);
     polynom.set_coefficient(a, 1);
     polynom.set_coefficient(b, 3);
     polynom.set_inhomogeneous_term(-4);
