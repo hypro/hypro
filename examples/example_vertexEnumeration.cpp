df796fe54ec87c1592408331dd9ef42a0a002259
sschupp <stefan.schupp@cs.rwth-aachen.de>
2014-08-23 15:27:33 +0000

    Added missing example file.

---
+++ examples/example_vertexEnumeration.cpp
@@ -0,0 +1,14 @@
+/**
+ * A simple example file to test the vertex enumeration algorithm.
+ */
+
+#include "../src/lib/representations/Polytopes/HPolytope/HPolytope.h"
+
+using namespace std;
+using namespace hypro;
+
+
+
+int main(int argc, char** argv) {
+    
+}
