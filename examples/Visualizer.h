64eb30c2834c942fb039bd785d983071fd2fb880
jgan <gan@embedded.rwth-aachen.de>
2014-12-02 12:01:13 +0000

    added example twotankreachability and dependencies

---
+++ examples/Visualizer.h
@@ -0,0 +1,74 @@
+/* 
+ * File:   Visualizer.h
+ * Author: jongan
+ *
+ * Created on July 1, 2014, 6:50 PM
+ */
+
+#ifndef VISUALIZER_H
+#define	VISUALIZER_H
+#define BUFFER_SIZE 2048
+
+#include "engine.h"
+#include <vector>
+#include <Eigen/Dense>
+#include <Eigen/Core>
+#include <sstream>
+#include <iostream>
+#include "../../src/lib/representations/Zonotope/Zonotope.h"
+
+template <typename Number>
+Eigen::Matrix<Number, Eigen::Dynamic, Eigen::Dynamic> zonotope2Matrix(const Zonotope<Number>& z) {
+    Eigen::Matrix<Number, Eigen::Dynamic, 1> center = z.center();
+    Eigen::Matrix<Number, Eigen::Dynamic, Eigen::Dynamic> generator_matrix = z.generators();
+    Eigen::Matrix<Number, Eigen::Dynamic, Eigen::Dynamic> res;
+    res.resize(center.rows(), generator_matrix.cols()+1);
+    res << center, generator_matrix;
+    return res;
+}
+
+class Visualizer {
+public:
+    Visualizer();
+    Visualizer(const Visualizer& orig);
+    virtual ~Visualizer();
+    
+    char buffer[BUFFER_SIZE+1];
+    
+    void sendVariable2Matlab(const char name[], const mxArray *var);
+    
+    void executeMatlabScript(const char name[]);
+    
+    mxArray * getVariablefromWorkspace(const char name[]);
+    
+    template<typename T>
+    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> mxArray2Eigen(mxArray* input_mx);
+    
+    template<typename T>
+    T mxArray2Scalar(mxArray* input_mx);
+    
+    mxArray * createMatrix(int rows, int cols);
+    
+    mxArray * createCellArray(unsigned int ndim, const unsigned long dims[]);
+    
+    void setCellArray(mxArray *cell, mwIndex idx, mxArray *val);
+    
+    template <typename T>
+    void matrix2mxArray(const Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>& matrix, mxArray * res);
+    
+    template <typename Number>
+    void draw2DZonotope(mxArray * res, const Zonotope<Number>& zonotope, unsigned d1, unsigned d2);
+    
+    template <typename Number>
+    Eigen::Matrix<Number, Eigen::Dynamic, Eigen::Dynamic> arrayVectorsToMatrix(std::vector<Eigen::Matrix<Number, Eigen::Dynamic,1> >& vectorArray);
+    
+    Engine * getEngine();
+    
+private:
+    Engine *ep_;
+    
+    
+};
+
+#endif	/* VISUALIZER_H */
+
