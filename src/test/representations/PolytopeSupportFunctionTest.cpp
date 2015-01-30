118f04383a5dbe7e5ef3c2da33087e866fa9c434
sschupp <stefan.schupp@cs.rwth-aachen.de>
2015-01-30 11:55:05 +0100

    minor refactoring and moving inside test.

--- src/test/representations/PolytopeSupportFunctionTest.cpp
+++ src/test/representations/PolytopeSupportFunctionTest.cpp
@@ -18,34 +18,37 @@ template<typename Number>
 class PolytopeSupportFunctionTest : public ::testing::Test {
 protected:
     virtual void SetUp() {
+		constraints = matrix_t<Number>::Zero(3,2);
+		constraints(0,0) = Number(2);
+		constraints(0,1) = Number(4);
+		constraints(1,0) = Number(1);
+		constraints(1,1) = Number(-2);
+		constraints(2,0) = Number(-4);
+		constraints(2,1) = Number(1);
+		constants = matrix_t<Number>(3,1);
+		constants(0,0) = Number(20);
+		constants(1,0) = Number(5);
+		constants(2,0) = Number(17);
+
+		ad = new artificialDirections<Number>();
+		ad->dir1 = getZeroVector<Number>(constraints.cols());
+		ad->dir2 = getZeroVector<Number>(constraints.cols());
+
+		ad->dir1(constraints.cols()-1,0) = 1;
+		ad->dir2(constraints.cols()-1,0) = -1;
     }
 	
     virtual void TearDown(){
+		delete ad;
     }
 	
+	matrix_t<Number> constraints;
+	matrix_t<Number> constants;
+	artificialDirections<Number>* ad;
 };
 
 TYPED_TEST(PolytopeSupportFunctionTest, constructor) {
-	matrix_t<TypeParam> constraints = matrix_t<TypeParam>::Zero(3,2);
-	constraints(0,0) = TypeParam(2);
-	constraints(0,1) = TypeParam(4);
-	constraints(1,0) = TypeParam(1);
-	constraints(1,1) = TypeParam(-2);
-	constraints(2,0) = TypeParam(-4);
-	constraints(2,1) = TypeParam(1);
-	matrix_t<TypeParam> constants = matrix_t<TypeParam>(3,1);
-	constants(0,0) = TypeParam(20);
-	constants(1,0) = TypeParam(5);
-	constants(2,0) = TypeParam(17);
-	
-	artificialDirections<TypeParam>* ad = new artificialDirections<TypeParam>();
-	ad->dir1 = getZeroVector<TypeParam>(constraints.cols());
-	ad->dir2 = getZeroVector<TypeParam>(constraints.cols());
-
-	ad->dir1(constraints.cols()-1,0) = 1;
-	ad->dir2(constraints.cols()-1,0) = -1;
-	PolytopeSupportFunction<TypeParam> defConstr = PolytopeSupportFunction<TypeParam>(constraints, constants, operator_e::LEQ, 2, ad);
-	delete ad;
+	PolytopeSupportFunction<TypeParam> psf1 = PolytopeSupportFunction<TypeParam>(this->constraints, this->constants, operator_e::LEQ, 2, this->ad);
 }
 
 TYPED_TEST(PolytopeSupportFunctionTest, access) {
