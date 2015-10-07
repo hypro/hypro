7e86751995ff0360c0634418ea00a60f00adf10e
Igor Nicolai Bongartz <ib797487@rwth-aachen.de>
2015-10-07 16:45:29 +0200

    implemented naive_DROP and naive_UNITE, uncommented datastructures, representations for compiling

--- examples/example_reduction.cpp
+++ examples/example_reduction.cpp
@@ -2,7 +2,7 @@
  * Example file for polytope reduction.
  * @brief [brief description]
  * @details [long description]
- * 
+ *
  */
 
  #include "../src/lib/util/Plotter.h"
@@ -22,20 +22,22 @@ int main(int argc, char const *argv[])
 
 	/* Do stuff here */
 
-	HPolytope<Number> poly;
+	HPolytope<Number> poly; //NikolausHaus
+  poly.insert(Hyperplane<Number>({-1,1},1));
 	poly.insert(Hyperplane<Number>({1,1},1));
-	poly.insert(Hyperplane<Number>({-1,1},1));
+  poly.insert(Hyperplane<Number>({1,0},1));
 	poly.insert(Hyperplane<Number>({0,-1},1));
-	poly.insert(Hyperplane<Number>({-1,1},-1));
+  poly.insert(Hyperplane<Number>({-1,0},1));
 
-	HPolytope<Number> poly2;
-	poly2.insert(Hyperplane<Number>({1,1},1));
-	poly2.insert(Hyperplane<Number>({-1,1},1));
-	poly2.insert(Hyperplane<Number>({0,-1},1));
+
+	HPolytope<Number> poly2 = poly.reduce();
+	//poly2.insert(Hyperplane<Number>({1,1},1));
+	//poly2.insert(Hyperplane<Number>({-1,1},1));
+	//poly2.insert(Hyperplane<Number>({0,-1},1));
 
 	unsigned p2 = plotter.addObject(poly2.vertices());
 	unsigned p1 = plotter.addObject(poly.vertices());
-	
+
 	plotter.setObjectColor(p2, colors[red]);
 
 	plotter.plot2d();
