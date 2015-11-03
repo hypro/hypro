5ffdc218a69cc319e2f0f6eb068baf32c83b7cdc
sschupp <stefan.schupp@cs.rwth-aachen.de>
2015-11-03 10:27:10 +0100

    added ptermpolytope tests.

---
+++ src/test/representations/PTermPolytopeTest.cpp
@@ -0,0 +1,65 @@
+/**
+ * A testfile for polytopes, please extend!
+ *
+ * @file    PTermPolytopeTest.cpp
+ * @covers  PTermPolytope
+ *
+ * @author  Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
+ * @author 	YOUR STORY!
+ *
+ * @since	2015-11-03
+ * @version 2015-11-03
+ */
+
+#include "gtest/gtest.h"
+#include "../defines.h"
+
+template<typename Number>
+class PTermPolytopeTest : public ::testing::Test
+{
+protected:
+    virtual void SetUp()
+    {
+    }
+
+    virtual void TearDown()
+    {
+    }
+
+};
+
+TYPED_TEST(PTermPolytopeTest, Constructor) {
+	FAIL();
+}
+
+TYPED_TEST(PTermPolytopeTest, Access) {
+	FAIL();
+}
+
+TYPED_TEST(PTermPolytopeTest, Insertion) {
+	FAIL();
+}
+
+TYPED_TEST(PTermPolytopeTest, Vertices) {
+	FAIL();
+}
+
+TYPED_TEST(PTermPolytopeTest, Union) {
+	FAIL();
+}
+
+TYPED_TEST(PTermPolytopeTest, LinearTransformation) {
+	FAIL();
+}
+
+TYPED_TEST(PTermPolytopeTest, MinkowskiSum) {
+	FAIL();
+}
+
+TYPED_TEST(PTermPolytopeTest, Intersection) {
+	FAIL();
+}
+
+TYPED_TEST(PTermPolytopeTest, Membership) {
+	FAIL();
+}
