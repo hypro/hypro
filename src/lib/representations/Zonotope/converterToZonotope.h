425e714aa43ee6f2bdae296f16e1c73d5fa48b7d
Simon Froitzheim <Simon.Froitzheim@rwth-aachen.de>
2016-02-01 15:03:45 +0100

    Fixed a few type errors in PolytopeSupportFunction plus simplified Zonotope -> Support Function conversion

--- src/lib/representations/Zonotope/converterToZonotope.h
+++ src/lib/representations/Zonotope/converterToZonotope.h
@@ -40,11 +40,11 @@ namespace hypro {
      *   
      * @param _source Box that needs to be converted.
      * @param _target An arbitrary Zonotope that gets overwritten with the converted object.
-     * @param mode The requested conversion mode (exact conversion or overapproximation).
+     * @param mode The requested conversion mode (exact conversion or overapproximation). Note that this function will always convert exactly.
      * @return Return value is true if the requested conversion was successful.
      */  
     template <typename Number>
-    static bool convert( const hypro::Box<Number>& _source, hypro::Zonotope<Number>& _target, const CONV_MODE mode = CONV_MODE::OVER  );
+    static bool convert( const hypro::Box<Number>& _source, hypro::Zonotope<Number>& _target, const CONV_MODE mode = CONV_MODE::EXACT  );
     
     /**
      * @brief Converts a V-Polytope into a zonotope.
