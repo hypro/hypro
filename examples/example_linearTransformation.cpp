a5bccb2dbe3ed057659128ac2dcf08627ece3698
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2016-02-09 09:48:48 +0100

    Added example_linearTransformation

---
+++ examples/example_linearTransformation.cpp
@@ -0,0 +1,41 @@
+/**
+ * Example file for linear transformation of a H-Polytope.
+ * @brief [brief description]
+ * @details [long description]
+ *
+ */
+
+ #include "../src/lib/util/Plotter.h"
+ #include "../src/lib/datastructures/Hyperplane.h"
+ #include "../src/lib/representations/Polytopes/HPolytope/HPolytope.h"
+
+using namespace hypro;
+
+int main(int argc, char const *argv[])
+{
+	typedef carl::FLOAT_T<mpq_class> Number;
+
+	HPolytope<Number> box;
+	box.insert(Hyperplane<Number>({1,0},1));
+	box.insert(Hyperplane<Number>({0,1},1));
+	box.insert(Hyperplane<Number>({-1,0},1));
+	box.insert(Hyperplane<Number>({0,-1},1));
+
+	matrix_t<Number> A = matrix_t<Number>(2,2);
+	A << 2,0,0,2;
+	vector_t<Number> b = vector_t<Number>(2);
+	b << 5,0;
+
+	HPolytope<Number> boxTrafoed = box.linearTransformation(A,b);
+	//HPolytope<Number> boxTrafoed = box.linearTransformation(A,vector_t<Number>::Zero(2));
+	std::cout << boxTrafoed << std::endl;
+
+	Plotter<Number>& plotter = Plotter<Number>::getInstance();
+	unsigned obj1 = plotter.addObject(box.vertices());
+	unsigned obj2 = plotter.addObject(boxTrafoed.vertices());
+	plotter.setObjectColor(obj2, colors[red]);
+
+	plotter.plot2d();
+
+	return 0;
+}
