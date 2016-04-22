1da39d9bca5816b146d442d7d9194946199e925e
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2016-04-22 13:06:56 +0200

    added cummulative gnuplot plotting and tests for that in an example.

--- examples/example_flowstarParser.cpp
+++ examples/example_flowstarParser.cpp
@@ -14,43 +14,45 @@ int main(int argc, char** argv) {
 	typedef hypro::SupportFunction<Number> sfValuation;
 	typedef hypro::HPolytope<Number> hpValuation;
 
-	std::string filename = "../examples/input/bouncing_ball.model";
+	//std::string filename = "../examples/input/bouncing_ball.model";
+	std::string filename = "../examples/input/simple_tank_plant.model";
 
 	hypro::parser::flowstarParser<Number> parser;
 	hypro::HybridAutomaton<Number> ha = parser.parseInput(filename);
 
-	hypro::reachability::Reach<Number,sfValuation> sfReach(ha, parser.mSettings);
-	std::vector<std::vector<sfValuation>> sfFlowpipes = sfReach.computeForwardReachability();
+	//hypro::reachability::Reach<Number,sfValuation> sfReach(ha, parser.mSettings);
+	//std::vector<std::vector<sfValuation>> sfFlowpipes = sfReach.computeForwardReachability();
 
-	//hypro::reachability::Reach<Number,hpValuation> hpReach(ha, parser.mSettings);
-	//std::vector<std::vector<hpValuation>> hpFlowpipes = hpReach.computeForwardReachability();
+	hypro::reachability::Reach<Number,hpValuation> hpReach(ha, parser.mSettings);
+	std::vector<std::vector<hpValuation>> hpFlowpipes = hpReach.computeForwardReachability();
 
 	hypro::Plotter<Number>& plotter = hypro::Plotter<Number>::getInstance();
 	plotter.setFilename(parser.mSettings.fileName);
 	std::vector<unsigned> plottingDimensions = parser.mSettings.plotDimensions;
 	plotter.rSettings().dimensions.first = plottingDimensions.front();
 	plotter.rSettings().dimensions.second = plottingDimensions.back();
+	plotter.rSettings().cummulative = true;
 
 	// bad states plotting
-	hypro::HybridAutomaton<Number>::locationSetMap badStateMapping = ha.localBadStates();
-	for(const auto& bsPair : badStateMapping) {
-		unsigned bs = plotter.addObject(hpValuation(bsPair.second.first, bsPair.second.second).vertices());
-		plotter.setObjectColor(bs, hypro::colors[hypro::red]);
-	}
+	hypro::HybridAutomaton<Number>::locationStateMap badStateMapping = ha.localBadStates();
+	//for(const auto& state : badStateMapping) {
+	//	unsigned bs = plotter.addObject(hpValuation(state.second.constraints, state.second.constants).vertices());
+	//	plotter.setObjectColor(bs, hypro::colors[hypro::red]);
+	//}
 
-	for(const auto& flowpipe : sfFlowpipes){
-		for(const auto& segment : flowpipe){
-			unsigned tmp = plotter.addObject(segment.vertices());
-			plotter.setObjectColor(tmp, hypro::colors[hypro::green]);
-		}
-	}
-
-	//for(const auto& flowpipe : hpFlowpipes){
+	//for(const auto& flowpipe : sfFlowpipes){
 	//	for(const auto& segment : flowpipe){
 	//		unsigned tmp = plotter.addObject(segment.vertices());
-	//		plotter.setObjectColor(tmp, hypro::colors[hypro::orange]);
+	//		plotter.setObjectColor(tmp, hypro::colors[hypro::green]);
 	//	}
 	//}
+
+	for(const auto& flowpipe : hpFlowpipes){
+		for(const auto& segment : flowpipe){
+			unsigned tmp = plotter.addObject(segment.vertices());
+			plotter.setObjectColor(tmp, hypro::colors[hypro::orange]);
+		}
+	}
 
 	plotter.plot2d();
 }
