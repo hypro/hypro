d232b056f9e600b0ce95562901f4e49eb39e2c7a
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2016-08-15 16:09:39 +0200

    added example for optimizer.

---
+++ examples/example_optimizer.cpp
@@ -0,0 +1,20 @@
+#include "../src/lib/representations/GeometricObject.h"
+
+int main(int argc, char** argv) {
+
+	using namespace hypro;
+	using Number = mpq_class;
+
+	matrix_t<Number> a = matrix_t<Number>(4,2);
+	a << 1,0,0,1,-1,0,0,-1;
+	vector_t<Number> b = vector_t<Number>(4);
+	b << 1,1,1,1;
+
+	HPolytope<Number> hpoly(a,b);
+
+	vector_t<Number> direction = vector_t<Number>(2);
+	direction << 1,1;
+
+	hpoly.evaluate(direction);
+
+}
