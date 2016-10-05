81db0236f62dab6dd621455f017e4128b3d02d0d
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2016-10-05 18:01:55 +0200

    minor fix to carl.cmake.

--- src/resources/carl.cmake
+++ src/resources/carl.cmake
@@ -11,7 +11,10 @@ list(APPEND hypro_INCLUDE_DIRS ${carl_INCLUDE_DIR})
 message("Current libraries (dynamic): ${hypro_LIBRARIES_DYNAMIC}")
 message("Current include directories: ${hypro_INCLUDE_DIRS}")
 
+message("Carl libraries (dynamic): ${carl_LIBRARIES}")
+message("Carl include directories: ${carl_INCLUDE_DIR}")
+
 add_imported_library(carl SHARED "${carl_LIBRARIES}" "${carl_INCLUDE_DIR}")
-add_imported_library(carl STATIC "${carl_LIBRARIES}" "${carl_INCLUDE_DIR}")
+#add_imported_library(carl STATIC "${carl_LIBRARIES}" "${carl_INCLUDE_DIR}")
 
 add_dependencies(resources carl)
