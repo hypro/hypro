4ab0dd6a902f0bb54285e93b11cac89878c3b664
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2016-10-24 13:58:58 +0200

    added external resource for log4cplus.

--- src/resources/log4cplus.cmake
+++ src/resources/log4cplus.cmake
@@ -8,8 +8,8 @@ add_dependencies(resources log4cplus)
 ExternalProject_Get_Property(log4cplus source_dir)
 ExternalProject_Get_Property(log4cplus binary_dir)
 
-#message("source_dir: ${source_dir}")
-#message("binary_dir: ${binary_dir}")
-#message("shared library suffix: ${CMAKE_SHARED_LIBRARY_SUFFIX}")
+message("source_dir: ${source_dir}")
+message("binary_dir: ${binary_dir}")
+message("shared library suffix: ${CMAKE_SHARED_LIBRARY_SUFFIX}")
 
-add_imported_library(log4cplus SHARED "${binary_dir}/src/${CMAKE_FIND_LIBRARY_PREFIXES}log4cplus${CMAKE_SHARED_LIBRARY_SUFFIX}" "${binary_dir}/include")
+add_imported_library(log4cplus SHARED "${binary_dir}/src/${CMAKE_FIND_LIBRARY_PREFIXES}log4cplus${CMAKE_SHARED_LIBRARY_SUFFIX}" "${source_dir}/include")
