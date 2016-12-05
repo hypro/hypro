760a94ede00bd7e1a14595fcfb167a02ced0af37
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2016-12-05 18:56:41 +0100

    updated coverage script.

--- processCoverage.sh
+++ processCoverage.sh
@@ -5,7 +5,7 @@ echo 'Starting coverage script ...'
 if [ "${FLAGS}" == "-DHYPRO_COVERAGE=ON" ]; then
 	echo 'Collecting files ...'
 	lcov --directory . --capture --output-file coverage.info # capture coverage info
-	lcov --remove coverage.info 'tests/*' '/usr/*' --output-file coverage.info # filter out system and test code
+	lcov --remove coverage.info 'test/*' '/usr/*' 'carl/' --output-file coverage.info # filter out system and test code
 	lcov --list coverage.info # debug before upload
 	echo 'Upload files ...'
 	coveralls-lcov --repo-token ${COVERALLS_TOKEN} coverage.info # uploads to coveralls
