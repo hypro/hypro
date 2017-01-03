95493707149fe33e90b29dd146d5b1bfcbdddcd1
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2017-01-03 10:25:28 +0100

    re-structured logger.

---
+++ src/hypro/util/logging/Logger.cpp
@@ -0,0 +1,22 @@
+#include "Logger.h"
+
+namespace hypro {
+
+	int initializeLogging() {
+		carl::logging::logger().configure("logfile", "hypro.log");
+		carl::logging::logger().filter("logfile")
+	    ("hypro", carl::logging::LogLevel::LVL_TRACE)
+		("hypro.representations", carl::logging::LogLevel::LVL_DEBUG)
+		;
+
+	  	carl::logging::logger().configure("stdout", std::cout);
+		carl::logging::logger().filter("stdout")
+	    ("hypro", carl::logging::LogLevel::LVL_TRACE)
+		("hypro.representations", carl::logging::LogLevel::LVL_DEBUG)
+		;
+
+	  	carl::logging::logger().resetFormatter();
+	  	return 0;
+	}
+
+} // namespace hypro
--- src/hypro/util/logging/Logger.h
+++ src/hypro/util/logging/Logger.h
@@ -18,22 +18,7 @@
 
 namespace hypro {
 
-	inline int initializeLogging() {
-		carl::logging::logger().configure("logfile", "hypro.log");
-		carl::logging::logger().filter("logfile")
-	    ("hypro", carl::logging::LogLevel::LVL_TRACE)
-		("hypro.representations", carl::logging::LogLevel::LVL_DEBUG)
-		;
-
-	  	carl::logging::logger().configure("stdout", std::cout);
-		carl::logging::logger().filter("stdout")
-	    ("hypro", carl::logging::LogLevel::LVL_TRACE)
-		("hypro.representations", carl::logging::LogLevel::LVL_DEBUG)
-		;
-
-	  	carl::logging::logger().resetFormatter();
-	  	return 0;
-	}
+	int initializeLogging();
 
 	static int initvar = initializeLogging();
 
