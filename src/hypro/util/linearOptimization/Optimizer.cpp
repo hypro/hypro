770c1420c03eca6ecfdd323aea99b85d92e24393
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2017-01-03 10:21:55 +0100

    fixed bug in linear optimizer explicit instanciations.

--- src/hypro/util/linearOptimization/Optimizer.cpp
+++ src/hypro/util/linearOptimization/Optimizer.cpp
@@ -2,20 +2,17 @@
 #include "flags.h"
 #ifdef USE_CLN_NUMBERS
 #include <cln/cln.h>
-#else
+#endif
 #include <gmp.h>
 #include <gmpxx.h>
-#endif
 
 template<typename Number>
 bool hypro::Optimizer<Number>::mWarnInexact = false;
 
 #ifdef USE_CLN_NUMBERS
-using Number = cln::cl_RA;
-#else
-using Number = mpq_class;
+template class hypro::Optimizer<cln::cl_RA>;
 #endif
 
-template class hypro::Optimizer<Number>;
+template class hypro::Optimizer<mpq_class>;
 
 template class hypro::Optimizer<double>;
