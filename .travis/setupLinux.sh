44f8760f22b93d77a991a98d728d1d4c755ccfbe
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2017-02-07 14:03:05 +0100

    changed setup to install full clang instead of clang++

--- .travis/setupLinux.sh
+++ .travis/setupLinux.sh
@@ -2,6 +2,7 @@
 
 #sudo add-apt-repository -y ppa:george-edison55/cmake-3.x
 sudo apt-get update
+sudo apt-get install cmake libboost1.55-all-dev libeigen3-dev libglpk-dev doxygen graphviz
 
 if [[ ${COMPILER} == "g++-5" ]]; then
 	#sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
@@ -13,27 +14,25 @@ elif [[ ${COMPILER} == "g++-6" ]]; then
 	defCXX gcc-6 g++-6
 elif [[ ${COMPILER} == "clang++-3.4" ]]; then
 	#sudo add-apt-repository -y "deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.4 main"
-	install_linux clang++-3.4
+	install_linux clang-3.4
 	defCXX clang-3.4 clang++-3.4
 elif [[ ${COMPILER} == "clang-3.5" ]]; then
 	#sudo add-apt-repository -y "deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.5 main"
-	install_linux clang++-3.5
+	install_linux clang-3.5
 	defCXX clang-3.5 clang++-3.5
 elif [[ ${COMPILER} == "clang++-3.6" ]]; then
 	#sudo add-apt-repository -y "deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.6 main"
-	install_linux clang++-3.6
+	install_linux clang-3.6
 	defCXX clang-3.6 clang++-3.6
 elif [[ ${COMPILER} == "clang++-3.7" ]]; then
 	#sudo add-apt-repository -y "deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.7 main"
-	install_linux clang++-3.7
+	install_linux clang-3.7
 	defCXX clang-3.7 clang++-3.7
 elif [[ ${COMPILER} == "clang++-3.8" ]]; then
 	#sudo add-apt-repository -y "deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.8 main"
-	install_linux clang++-3.8
+	install_linux clang-3.8
 	defCXX clang-3.8 clang++-3.8
 fi
-
-sudo apt-get install cmake libboost1.55-all-dev libeigen3-dev libglpk-dev doxygen graphviz
 
 if [[ ${FLAGS} == "-DHYPRO_COVERAGE=ON" ]]; then
   cd ${TRAVIS_BUILD_DIR}
