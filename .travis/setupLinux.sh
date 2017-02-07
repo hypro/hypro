2bd1ee04045d753378a67b785025eec1f9161dc9
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2017-02-07 12:15:09 +0100

    ...

--- .travis/setupLinux.sh
+++ .travis/setupLinux.sh
@@ -7,32 +7,32 @@ sudo add-apt-repository -y ppa:george-edison55/cmake-3.x
 if [[ ${COMPILER} == "g++-5" ]]; then
 	sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 	sudo apt-get update
-	sudo apt-get install g++-5
+	sudo apt-get install g++-5 -f
 	defCXX gcc-5 g++-5
 elif [[ ${COMPILER} == "g++-6" ]]; then
 	sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 	sudo apt-get update
-	sudo apt-get install g++-6
+	sudo apt-get install g++-6 -f
 	defCXX gcc-6 g++-6
 elif [[ ${COMPILER} == "clang++-3.4" ]]; then
 	sudo add-apt-repository -y "deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.4 main"
 	sudo apt-get update
-	sudo apt-get install llvm-3.4-dev
+	sudo apt-get install llvm-3.4-dev -f
 	defCXX clang-3.4 clang++-3.4
 elif [[ ${COMPILER} == "clang++-3.5" ]]; then
 	sudo add-apt-repository -y "deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.5 main"
 	sudo apt-get update
-	sudo apt-get install llvm-3.5-dev
+	sudo apt-get install llvm-3.5-dev -f
 	defCXX clang-3.5 clang++-3.5
 elif [[ ${COMPILER} == "clang++-3.6" ]]; then
 	sudo add-apt-repository -y "deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.6 main"
 	sudo apt-get update
-	sudo apt-get install llvm-3.6-dev
+	sudo apt-get install llvm-3.6-dev -f
 	defCXX clang-3.6 clang++-3.6
 elif [[ ${COMPILER} == "clang++-3.7" ]]; then
 	sudo add-apt-repository -y "deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.7 main"
 	sudo apt-get update
-	sudo apt-get install llvm-3.7-dev
+	sudo apt-get install llvm-3.7-dev -f
 	defCXX clang-3.7 clang++-3.7
 fi
 
