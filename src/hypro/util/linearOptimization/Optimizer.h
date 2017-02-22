4309b4d52eb5ee6a3d34814135c2bbb56ed3bd52
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2017-02-22 17:37:19 +0100

    added documentation.

--- src/hypro/util/linearOptimization/Optimizer.h
+++ src/hypro/util/linearOptimization/Optimizer.h
@@ -33,6 +33,11 @@
 
 namespace hypro {
 
+	/**
+	 * @brief      Wrapper class for linear optimization.
+	 *
+	 * @tparam     Number  The used number type.
+	 */
 	template<typename Number>
 	class Optimizer {
 
@@ -65,6 +70,9 @@ namespace hypro {
 
 	public:
 
+		/**
+		 * @brief      Default constructor.
+		 */
 		Optimizer() :
 			mConstraintMatrix(),
 			mConstraintVector(),
@@ -98,6 +106,11 @@ namespace hypro {
 		Optimizer(Optimizer<Number>&& orig) = delete;
 		Optimizer(const Optimizer<Number>& orig) = delete;
 
+		/**
+		 * @brief      Constructor which sets the problem.
+		 * @param[in]  constraints  The constraints.
+		 * @param[in]  constants    The constants.
+		 */
 		Optimizer(const matrix_t<Number>& constraints, const vector_t<Number>& constants) :
 			mConstraintMatrix(constraints),
 			mConstraintVector(constants),
@@ -106,6 +119,9 @@ namespace hypro {
 			mConsistencyChecked(false)
 		{	}
 
+		/**
+		 * @brief      Destroys the object.
+		 */
 		~Optimizer() {
 			if(mInitialized) {
 				glp_delete_prob(lp);
@@ -114,26 +130,96 @@ namespace hypro {
 		}
 
 	public:
+		/**
+		 * @brief      Comparison operator.
+		 * @param[in]  orig  The right-hand-side object.
+		 * @return     True, if both problem instances are equal, false otherwise.
+		 */
 		Optimizer<Number>& operator=(const Optimizer<Number>& orig);
 
+		/**
+		 * @brief      Returns the problem matrix.
+		 * @return     The problem matrix (row-wise constraints).
+		 */
 		const matrix_t<Number>& matrix() const;
+
+		/**
+		 * @brief      Returns the problem constants as a vector.
+		 * @return     The vector of constants.
+		 */
 		const vector_t<Number>& vector() const;
 
+		/**
+		 * @brief      Sets the problem matrix.
+		 * @param[in]  _matrix  The matrix.
+		 */
 		void setMatrix(const matrix_t<Number>& _matrix);
+
+		/**
+		 * @brief      Sets the problem vector.
+		 * @param[in]  _vector  The vector.
+		 */
 		void setVector(const vector_t<Number>& _vector);
+
+		/**
+		 * @brief      Clears the problem instance.
+		 */
 		void clear();
 
+		/**
+		 * @brief      Performs linear optimization in the given direction.
+		 * @param[in]  _direction    The direction.
+		 * @param[in]  useExactGlpk  The use exact glpk property. If set, glpk is used in its exact mode.
+		 * @return     A struct holding the linear optimization result.
+		 */
 		EvaluationResult<Number> evaluate(const vector_t<Number>& _direction, bool useExactGlpk) const;
+
+		/**
+		 * @brief      Checks consistency (i.e. existence of a solution) of the current problem instance.
+		 * @return     True, if there exists a solution, false otherwise.
+		 */
 		bool checkConsistency() const;
+
+		/**
+		 * @brief      Checks whether the given point lies inside the satisfiable solution space of the current problem.
+		 * @param[in]  _point  The point.
+		 * @return     True, if the point satisfies all constraints, false otherwise.
+		 */
 		bool checkPoint(const Point<Number>& _point) const;
+
+		/**
+		 * @brief      Gets some point being a possible solution.
+		 * @return     The internal point.
+		 */
 		EvaluationResult<Number> getInternalPoint() const;
+
+		/**
+		 * @brief      Determines the set of redundant constraints.
+		 * @details    A constraint is redundant, whenever the removal of this constraint does not modify the set of possible solutions.
+		 * @return     A vector of row-indices which represent redundant constraints.
+		 */
 		std::vector<std::size_t> redundantConstraints() const;
 
 	private:
+		/**
+		 * @brief      Calls required initialization methods.
+		 */
 		void initialize() const;
+
+		/**
+		 * @brief      Updates problem instances.
+		 */
 		void updateConstraints() const;
 
+		/**
+		 * @brief      Creates the required arrays for glpk.
+		 * @param[in]  size  The size.
+		 */
 		void createArrays( unsigned size ) const;
+
+		/**
+		 * @brief      Deletes the previously created arrays for glpk.
+		 */
 		void deleteArrays() const;
 
 	};
