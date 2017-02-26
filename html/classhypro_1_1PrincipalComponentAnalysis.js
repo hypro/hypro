a4253d13e632abfe6f46b79a7075492494041140
Travis CI <travis@travis-ci.org>
2017-02-26 13:26:43 +0000

    Deploy code docs to GitHub Pages Travis build: 197
    Commit: 9eddec11ee5b40839e43ee9a3decde3421009cd8

--- html/classhypro_1_1PrincipalComponentAnalysis.js
+++ html/classhypro_1_1PrincipalComponentAnalysis.js
@@ -3,10 +3,10 @@ var classhypro_1_1PrincipalComponentAnalysis =
     [ "PrincipalComponentAnalysis", "classhypro_1_1PrincipalComponentAnalysis.html#a5894a9266be8139d21d5573e4a17f2ac", null ],
     [ "PrincipalComponentAnalysis", "classhypro_1_1PrincipalComponentAnalysis.html#a3a44a1aa9b12247668c5672f6992cea1", null ],
     [ "PrincipalComponentAnalysis", "classhypro_1_1PrincipalComponentAnalysis.html#a9e07301dd180bd30cbf43b8835761013", null ],
-    [ "box", "classhypro_1_1PrincipalComponentAnalysis.html#adf49dd3b8a32ad6040a6474af027aaba", null ],
-    [ "computeMean", "classhypro_1_1PrincipalComponentAnalysis.html#a8acfb4fc66fc81de30285476d9d748fd", null ],
-    [ "computeOrientedBox", "classhypro_1_1PrincipalComponentAnalysis.html#af6b6899532b55b4eedc3e05f86dd1b57", null ],
-    [ "mean", "classhypro_1_1PrincipalComponentAnalysis.html#ac07cd587b12838018ca767aad07f6b2a", null ],
-    [ "samples", "classhypro_1_1PrincipalComponentAnalysis.html#a87064a4c1546546426e50ba433356116", null ],
+    [ "box", "classhypro_1_1PrincipalComponentAnalysis.html#a4d3856a1a4b9a62f6eed333563a592a8", null ],
+    [ "computeMean", "classhypro_1_1PrincipalComponentAnalysis.html#a05b22744c3c89979acf944a1dcf0f994", null ],
+    [ "computeOrientedBox", "classhypro_1_1PrincipalComponentAnalysis.html#a0fec1828eec14f67fa7c570150b88e0c", null ],
+    [ "mean", "classhypro_1_1PrincipalComponentAnalysis.html#a4e2fe250199158f5113027a3ca16f169", null ],
+    [ "samples", "classhypro_1_1PrincipalComponentAnalysis.html#a06312cbe5baaa7ddd187a1069fc78a25", null ],
     [ "setSamples", "classhypro_1_1PrincipalComponentAnalysis.html#aa4a3606e5c28d6af0e78a01e5a2aaa91", null ]
 ];
