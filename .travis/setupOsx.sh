5a7f48d974154e2d08c8a5300c732ce85596896b
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2017-02-26 11:45:38 +0100

    added osx to travis.

--- .travis/setupOsx.sh
+++ .travis/setupOsx.sh
@@ -3,14 +3,14 @@
 set -ev
 
 brew update --quiet
-brew install cmake doxygen eigen llvm
+brew install cmake doxygen eigen llvm latex
 
 if [[ ${COMPILER} == "g++-4.8" ]]; then
 	echo "g++-4.8 is not supported"
 	#install gcc-4.8 g++-4.8
 	#defCXX gcc-4.8 g++-4.8
 elif [[ ${COMPILER} == "g++-4.9" ]]; then
-	install gcc
+	brew install gcc
 	defCXX gcc-4.9 g++-4.9
 elif [[ ${COMPILER} == "clang++-3.4" ]]; then
 	echo "clang++-3.4 is not supported"
