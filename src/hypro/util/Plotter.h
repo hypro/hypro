303486443935b9696b6b09b004b755a4e8cbb590
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2017-03-01 15:59:25 +0100

    set keepAspectRatio to true (default).

--- src/hypro/util/Plotter.h
+++ src/hypro/util/Plotter.h
@@ -46,13 +46,13 @@ namespace plotting {
 	 */
 	struct gnuplotSettings {
 		std::string name = "";				// filename
-		std::size_t color = colors[blue];  // default petrol
+		std::size_t color = colors[blue];  // default blue
 		bool fill = false;					 // do not fill
 		bool axes = true;					 // plot axes
 		bool grid = true;					 // plot grid
 		double pointSize = 1.0;				 // pointsize
 		double linewidth = 0.2;				 // linewidth
-		bool keepAspectRatio = false; 		 // keep aspect ratio for both axes
+		bool keepAspectRatio = true; 		 // keep aspect ratio for both axes
 		std::pair<unsigned, unsigned> dimensions = std::make_pair(0,1); // dimensions to plot
 		bool cummulative = false;			 // if enabled, plot each new segment in a new plot, only works for gnuplot, not for tex (TODO)
 	};
