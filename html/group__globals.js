7cc04e14ad71d6f43b1a4d77c0f9bf513e26546b
Travis CI <travis@travis-ci.org>
2017-05-16 23:25:38 +0000

    Deploy code docs to GitHub Pages Travis build: 216
    Commit: 8da8f7c83d91015face5ae089d37dabfe57f73fc

--- html/group__globals.js
+++
@@ -1,4 +0,0 @@
-var group__globals =
-[
-    [ "Options ", "md__home_travis_build_hypro_hypro_doc_markdown_gettingStarted_installation.html#options", null ]
-];
---
+++ html/navtreedata.js
@@ -0,0 +1,51 @@
+var NAVTREE =
+[
+  [ "HyPro", "index.html", [
+    [ "Getting started", "gettingStarted.html", "gettingStarted" ],
+    [ "State set representations", "stateSets.html", "stateSets" ],
+    [ "Utility functions", "utilFunc.html", "utilFunc" ],
+    [ "Bibliography", "citelist.html", null ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Namespaces", null, [
+      [ "Namespace List", "namespaces.html", "namespaces" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Related Functions", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ]
+    ] ],
+    [ "Examples", "examples.html", "examples" ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+".html",
+"classhypro_1_1BoxT_3_01double_00_01Converter_01_4.html#ac1605baa18b754d96005cafcd4a8fe76",
+"classhypro_1_1GeometricObject.html#a44bae859470ea4cb3bc74c16f94b9638",
+"classhypro_1_1NeighborhoodContainer.html#acb94eae30d81ef3dd8c0ef56b5adabd9",
+"classhypro_1_1Range.html#a85a0dea8065db26dfea7555f792ce13a",
+"classhypro_1_1Transition.html",
+"classhypro_1_1statistics_1_1CounterRepository.html#a536caabe0dd9860483607251a73efceb",
+"structhypro_1_1State.html#a4f38baf9e2f60e607dbf060faae25b70",
+"structhypro_1_1plotting_1_1gnuplotSettings.html#ad4c831cb695d8c24d471d1b46f3411d1"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
