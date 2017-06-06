f6880c51c5c97dd80eebf3798ddc8111c444bddf
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2017-06-06 16:50:32 +0200

    Attempt to resolve #2.

--- src/resources/carl.cmake
+++ src/resources/carl.cmake
@@ -1,20 +1,40 @@
+
+
 ExternalProject_Add(
 	carl
 	GIT_REPOSITORY http://github.com/smtrat/carl.git
+	BUILD_COMMAND make lib_carl
 	INSTALL_COMMAND "")
 
-find_package(carl QUIET)
-list(APPEND hypro_LIBRARIES_DYNAMIC ${carl_LIBRARIES})
-list(APPEND hypro_LIBRARIES_STATIC ${carl_LIBRARIES})
-list(APPEND hypro_INCLUDE_DIRS ${carl_INCLUDE_DIR})
+ExternalProject_Add_Step(carl ReloadCMake
+	COMMAND cmake ../..
+	COMMENT "Re-run CMake."
+	DEPENDEES build
+	)
 
-message("Current libraries (dynamic): ${hypro_LIBRARIES_DYNAMIC}")
-message("Current include directories: ${hypro_INCLUDE_DIRS}")
+#ExternalProject_Get_Property(carl source_dir)
+#ExternalProject_Get_Property(carl binary_dir)
 
-message("Carl libraries (dynamic): ${carl_LIBRARIES}")
-message("Carl include directories: ${carl_INCLUDE_DIR}")
+#set( carl_INCLUDE_DIR "${EP_PREFIX_DIR}/src/carl/src" PARENT_SCOPE )
+#set( carl_LIBRARIES "${EP_PREFIX_DIR}/src/carl-build/libcarl${CMAKE_SHARED_LIBRARY_SUFFIX}" PARENT_SCOPE )
 
-#add_imported_library(carl SHARED "${carl_LIBRARIES}" "${carl_INCLUDE_DIR}")
-#add_imported_library(carl STATIC "${carl_LIBRARIES}" "${carl_INCLUDE_DIR}")
+#add_library(carl_SHARED SHARED IMPORTED)
+#set_target_properties(carl_SHARED PROPERTIES IMPORTED_LOCATION "${carl_LIBRARIES}")
+#set_target_properties(carl_SHARED PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${carl_INCLUDE_DIR}")
+#set(hypro_LIBRARIES_DYNAMIC ${hypro_LIBRARIES_DYNAMIC} ${carl_LIBRARIES} )
+
+#find_package(carl QUIET)
+#list(APPEND hypro_LIBRARIES_DYNAMIC ${EP_PREFIX_DIR}/src/carl-build/libcarl${CMAKE_SHARED_LIBRARY_PREFIX})
+#list(APPEND hypro_LIBRARIES_STATIC ${EP_PREFIX_DIR}/src/carl-build/libcarl${CMAKE_STATIC_LIBRARY_PREFIX})
+#list(APPEND hypro_INCLUDE_DIRS ${EP_PREFIX_DIR}/src/carl/src )
+
+#message("Current libraries (dynamic): ${hypro_LIBRARIES_DYNAMIC}")
+#message("Current include directories: ${hypro_INCLUDE_DIRS}")
+
+#message("Attempted carl dir: ${EP_PREFIX_DIR}/src/carl/src")
 
 add_dependencies(resources carl)
+
+#add_imported_library(carl SHARED "${EP_PREFIX_DIR}/src/carl-build/libcarl${CMAKE_SHARED_LIBRARY_SUFFIX}" "${EP_PREFIX_DIR}/src/carl/src")
+#add_imported_library(carl_static STATIC "${EP_PREFIX_DIR}/src/carl-build/libcarl${CMAKE_STATIC_LIBRARY_SUFFIX}" "${EP_PREFIX_DIR}/src/carl/src")
+
