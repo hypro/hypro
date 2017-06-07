072bf13c386fc3c69c3b732f5623b4568d1ccec2
Travis CI <travis@travis-ci.org>
2017-06-07 11:26:38 +0000

    Deploy code docs to GitHub Pages Travis build: 229
    Commit: ca2864cf3378b5e940e37944a53239a91a0621fd

--- html/search/classes_d.js
+++ html/search/classes_d.js
@@ -1,7 +1,6 @@
 var searchData=
 [
-  ['permutator',['Permutator',['../classhypro_1_1hypro_1_1Permutator.html',1,'hypro::hypro']]],
-  ['permutator',['Permutator',['../classhypro_1_1Permutator.html',1,'hypro']]],
+  ['permutator',['Permutator',['../classhypro_1_1hypro_1_1Permutator.html',1,'hypro::hypro::Permutator'],['../classhypro_1_1Permutator.html',1,'hypro::Permutator']]],
   ['plotter',['Plotter',['../classhypro_1_1Plotter.html',1,'hypro']]],
   ['point',['Point',['../classhypro_1_1Point.html',1,'hypro']]],
   ['point_3c_20int_20_3e',['Point&lt; int &gt;',['../classhypro_1_1Point.html',1,'hypro']]],
