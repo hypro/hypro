787c9fd29a12f40b0978d6ba2e164ca05e8973fc
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2017-06-08 10:04:39 +0200

    linking statically to glpk.

--- src/resources/glpk.cmake
+++ src/resources/glpk.cmake
@@ -3,7 +3,7 @@ ExternalProject_Add(
 	glpk
 	#URL ftp://ftp.gnu.org/gnu/glpk/glpk-${glpk_version}.tar.gz
 	SOURCE_DIR "${CMAKE_SOURCE_DIR}/src/resources/glpk-${glpk_version}"
-    CONFIGURE_COMMAND ./configure --prefix=${CMAKE_BINARY_DIR}/resources/glpk-${glpk_version}/ --with-gmp
+    CONFIGURE_COMMAND ./configure --prefix=${CMAKE_BINARY_DIR}/resources/glpk-${glpk_version}/ --with-gmp --with-pic CPPFLAGS=-fPIC
 	PREFIX ${CMAKE_BINARY_DIR}/resources/glpk-${glpk_version}/
             #BUILD_COMMAND make
             BUILD_IN_SOURCE 1
@@ -17,11 +17,12 @@ ExternalProject_Get_Property(glpk binary_dir)
 #add_imported_library(GLPK SHARED "${CMAKE_BINARY_DIR}/resources/glpk-${glpk_version}/lib/${CMAKE_FIND_LIBRARY_PREFIXES}glpk${CMAKE_SHARED_LIBRARY_SUFFIX}" "${CMAKE_BINARY_DIR}/resources/glpk-${glpk_version}/include")
 
 set( GLPK_INCLUDE_DIR "${CMAKE_BINARY_DIR}/resources/glpk-${glpk_version}/include" PARENT_SCOPE )
-set( GLPK_LIBRARIES "${CMAKE_BINARY_DIR}/resources/glpk-${glpk_version}/lib/${CMAKE_FIND_LIBRARY_PREFIXES}glpk${CMAKE_SHARED_LIBRARY_SUFFIX}" PARENT_SCOPE )
+set( GLPK_LIBRARIES "${CMAKE_BINARY_DIR}/resources/glpk-${glpk_version}/lib/${CMAKE_FIND_LIBRARY_PREFIXES}glpk${CMAKE_STATIC_LIBRARY_SUFFIX}" PARENT_SCOPE )
 
-add_library(GLPK_SHARED SHARED IMPORTED)
-set_target_properties(GLPK_SHARED PROPERTIES IMPORTED_LOCATION "${GLPK_LIBRARIES}")
-set_target_properties(GLPK_SHARED PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${GLPK_INCLUDE_DIR}")
+add_library(GLPK_STATIC STATIC IMPORTED)
+set_target_properties(GLPK_STATIC PROPERTIES IMPORTED_LOCATION "${GLPK_LIBRARIES}")
+set_target_properties(GLPK_STATIC PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${GLPK_INCLUDE_DIR}")
 set(hypro_LIBRARIES_DYNAMIC ${hypro_LIBRARIES_DYNAMIC} ${GLPK_LIBRARIES} )
+set(hypro_LIBRARIES_STATIC ${hypro_LIBRARIES_STATIC} ${GLPK_LIBRARIES} )
 
 add_dependencies(resources glpk)
