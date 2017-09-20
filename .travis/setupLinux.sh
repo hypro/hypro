07e7baad5185df850daa59856bf0428a2f6a7191
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2017-09-20 14:45:12 +0200

    Travis cmake fixes.

--- .travis/setupLinux.sh
+++ .travis/setupLinux.sh
@@ -28,3 +28,16 @@ if [[ ${FLAGS} == "-DHYPRO_COVERAGE=ON" ]]; then
   cd ${TRAVIS_BUILD_DIR}
   lcov --directory . --zerocounters
 fi
+
+mkdir -p ~/usr
+PREFIX=`cd ~/usr; pwd`
+
+if [ ! -f $PREFIX/bin/cmake ]; then
+	wget -nv https://cmake.org/files/v3.7/cmake-3.7.2-Linux-x86_64.sh
+	chmod +x cmake-3.7.2-Linux-x86_64.sh
+	./cmake-3.7.2-Linux-x86_64.sh --prefix=$PREFIX --exclude-subdir --skip-license
+fi
+
+export PATH="$PREFIX/bin:$PATH"
+
+cmake --version#
