bea3da121e836c3592daa09c299c68f945379554
Phillip Tse <phillip.tse@rwth-aachen.de>
2017-10-11 13:18:19 +0200

    Commit so i can pull

---
+++ src/hypro/representations/RepresentationSetting.h
@@ -0,0 +1,10 @@
+/* 
+ * RepresentationSetting.h
+ *
+ * Special settings within the different representations are summarized in this file.
+ * Each representation has its own RepresentationSettings-struct and is templated extra, 
+ * such that a representation can get its setting via this template.
+ *
+ *
+ *
+ */ 
