753928b71257dfafb4872eb91606ba36626d3adc
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2018-02-05 10:44:17 +0100

    ...

---
+++ src/hypro/datastructures/HybridAutomaton/output/Flowstar.h
@@ -0,0 +1,180 @@
+#pragma once
+#include "../HybridAutomaton.h"
+#include "../Condition.h"
+#include <iostream>
+
+namespace hypro {
+
+	template<typename Number>
+	std::string toFlowstarFormat(const Condition<Number>& in,
+								const std::map<Eigen::Index, std::string>& varNameMap,
+								const std::string& prefix)
+	{
+		std::stringstream res;
+		for(Eigen::Index rowI = 0; rowI < in.getMatrix().rows(); ++rowI) {
+			res << prefix;
+			bool first = true;
+			for(Eigen::Index colI = 0; colI < in.getMatrix().cols(); ++colI) {
+				if(in.getMatrix()(rowI,colI) > 0) {
+					if(!first){
+						res << " +";
+					} else {
+						first = false;
+					}
+					if(in.getMatrix()(rowI,colI) != 1) {
+						res << in.getMatrix()(rowI,colI) << "*";
+					}
+				} else if(in.getMatrix()(rowI,colI) < 0) {
+					res << " " << in.getMatrix()(rowI,colI) << "*";
+				}
+				if(in.getMatrix()(rowI,colI) != 0 && colI != in.getMatrix().cols()) {
+					res << varNameMap.at(colI);
+				}
+			}
+			res << " <= " << in.getVector()(rowI);
+		}
+		return res.str();
+	}
+
+	template<typename Number>
+	std::string toFlowstarFormat(const ConstraintSet<Number>& in,
+								const std::map<Eigen::Index, std::string>& varNameMap,
+								const std::string& prefix)
+	{
+		return toFlowstarFormat(Condition<Number>(in.getMatrix(),in.getVector()), varNameMap, prefix);
+	}
+
+	template<typename Number>
+	std::string toFlowstarFormat(const matrix_t<Number>& in,
+								const std::map<Eigen::Index, std::string>& varNameMap,
+								const std::string& prefix)
+	{
+		assert(in.rows() == 1);
+		std::stringstream res;
+		res << prefix;
+		bool first = true;
+		for(Eigen::Index colI = 0; colI < in.cols(); ++colI) {
+			if(in(0,colI) > 0) {
+				if(!first){
+					res << " +";
+				} else {
+					first = false;
+				}
+				if(in(0,colI) != 1) {
+					res << in(0,colI);
+				}
+			} else if(in(0,colI) < 0) {
+				res << " " << in(0,colI);
+			}
+			if(in(0,colI) != 0 && colI != in.cols()-1 && in(0,colI) != 1) {
+				res << "*";
+			}
+			if(in(0,colI) != 0 && colI != in.cols()-1) {
+				res << varNameMap.at(colI);
+			}
+		}
+		return res.str();
+	}
+
+	template<typename Number>
+	std::string toFlowstarFormat(const HybridAutomaton<Number>& in) {
+		std::stringstream res;
+		std::map<Eigen::Index, std::string> vars;
+
+		res << "hybrid reachability\n{\n";
+
+		if(!in.getLocations().empty()) {
+			if( in.dimension() > 0 ) {
+				// variables (note: the last dimension is for constants)
+				res << "\tstate var x_0";
+				vars[0] = "x_0";
+				for(std::size_t cnt = 1; cnt < in.dimension()-1; ++cnt) {
+					res << ", x_" << cnt;
+					std::stringstream tmp;
+					tmp << "x_" << cnt;
+					vars[cnt] = tmp.str();
+				}
+				res << "\n";
+			}
+
+			// Todo: add out-commented exemplary settings
+
+			// locations
+			res << "\tmodes\n\t{\n";
+			std::size_t lCnt = 0;
+			for(const auto locPtr : in.getLocations()) {
+				// location name
+				if(locPtr->getName() == "") {
+					res << "\t\tl_" << lCnt++;
+				} else {
+					res << "\t\t" << locPtr->getName();
+				}
+				res << "\n\t\t{";
+				if(in.dimension() > 0) {
+					// flow
+					res << "\n\t\t\tpoly ode 1";
+					res << "\n\t\t\t{";
+					for(Eigen::Index rowI = 0; rowI < locPtr->getFlow().rows()-1; ++rowI) {
+						std::stringstream tmp;
+						tmp << "\n\t\t\t\t" << vars[rowI] << "' = ";
+						res << toFlowstarFormat(matrix_t<Number>(locPtr->getFlow().row(rowI)), vars, tmp.str());
+					}
+					res << "\n\t\t\t}";
+					// invariant
+					res << "\n\t\t\tinv";
+					res << "\n\t\t\t{";
+					res << toFlowstarFormat(locPtr->getInvariant(), vars, "\n\t\t\t\t");
+					res << "\n\t\t\t}";
+				}
+				res << "\n\t\t}";
+			}
+			res << "\n\t}\n";
+
+			if(!in.getTransitions().empty()) {
+				// transitions
+				res << "\tjumps\n\t{";
+				for(const auto transPtr : in.getTransitions()) {
+					res << "\n\t\t" << transPtr->getSource()->getName() << " -> " << transPtr->getTarget()->getName();
+					res << "\n\t\tguard {";
+					res << toFlowstarFormat(transPtr->getGuard(), vars, " ");
+					res << " }";
+					res << "\n\t\treset {";
+					for(Eigen::Index rowI = 0; rowI < transPtr->getReset().getMatrix().rows(); ++rowI) {
+						std::stringstream tmp;
+						tmp << " " << vars[rowI] << "' := ";
+						matrix_t<Number> tmpBlock = matrix_t<Number>::Zero(1,transPtr->getReset().getMatrix().cols()+1);
+						tmpBlock.block(0,0,1,transPtr->getReset().getMatrix().cols()) = transPtr->getReset().getMatrix().row(rowI);
+						tmpBlock(0,transPtr->getReset().getMatrix().cols()) = transPtr->getReset().getVector()(rowI);
+						res << toFlowstarFormat(tmpBlock, vars, tmp.str());
+					}
+					res << " }";
+					if(transPtr->getAggregation()) {
+						res << "\n\t\tparallelotope aggregation {}";
+					}
+				}
+				res << "\n\t}\n";
+			}
+
+			// initial states
+			res << "\tinit\n\t{";
+			for(const auto& s : in.getInitialStates() ) {
+				res << "\t\t" << s.first->getName();
+				res << "\n\t\t{";
+				res << toFlowstarFormat(boost::apply_visitor(genericConversionVisitor<typename State_t<Number>::repVariant, Number>(representation_name::constraint_set), s.getSet()), vars, "\n\t\t\t" );
+				res << "\n\t\t}";
+			}
+
+			res << "\t}\n";
+
+			// bad states
+			res << "\tunsafe set\n\t{";
+
+			res << "\t}\n";
+
+		}
+
+ 		res << "}\n";
+		return res.str();
+	}
+
+}
