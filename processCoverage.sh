016ddc3690f4f7ae7f6bbd9c6072978ab14a89a4
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2018-02-09 16:29:05 +0100

    removed autogenerated parser folder from test coverage analysis.

--- processCoverage.sh
+++ processCoverage.sh
@@ -5,7 +5,7 @@ echo 'Starting coverage script ...'
 if [ "${FLAGS}" == "-DHYPRO_COVERAGE=ON" ]; then
 	echo 'Collecting files ...'
 	lcov --directory . --capture --no-external --output-file coverage.info # capture coverage info
-	lcov --remove coverage.info 'test/*' '/usr/*' '*/carl/*' --output-file coverage.info # filter out system and test code
+	lcov --remove coverage.info 'test/*' '/usr/*' '*/carl/*' '*/parser/*' --output-file coverage.info # filter out system and test code
 	lcov --list coverage.info # debug before upload
 	echo 'Upload files ...'
 	coveralls-lcov --repo-token ${COVERALLS_TOKEN} coverage.info # uploads to coveralls
