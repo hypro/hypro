5a805eeb5fda68c0b039bce07bda6c974ef84613
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2018-03-16 10:55:56 +0100

    fixed build script.

--- .travis/build.sh
+++ .travis/build.sh
@@ -7,10 +7,10 @@ cmake ../ || return 1
 
 if [[ ${TASK} == "sonarcloud" ]]; then
 	cmake ../ || return 1
-	build-wrapper-linux-x86-64 --out-dir=../bw-out make clean hypro -j4 || return 1
+	build-wrapper-linux-x86-64 --out-dir=../bw-out make clean hypro -j4
 	#make coverage-collect
 
-	cd ../ && sonar-scanner -X -Dproject.settings=.travis/sonar-project.properties -Dsonar.login&& cd build/
+	cd ../ && sonar-scanner -X -Dproject.settings=.travis/sonar-project.properties && cd build/
 
 else
 	cmake -j4 $FLAGS -DCMAKE_CXX_COMPILER=$COMPILER ..
