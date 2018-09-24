0b6581d6fe61da9fd57d79f035c2b9772850c902
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2018-09-24 17:40:36 +0200

    added documentation and minor changes to vertex enumeration.

--- src/hypro/algorithms/convexHull/vertexEnumeration.h
+++ src/hypro/algorithms/convexHull/vertexEnumeration.h
@@ -21,96 +21,93 @@ class VertexEnumeration {
 	public:
 		VertexEnumeration() = default;
 		VertexEnumeration(const VertexEnumeration<Number>& _orig) = default;
+		VertexEnumeration(const std::vector<Halfspace<Number>>& hsv);
+		VertexEnumeration(const matrix_t<Number>& constraints, const vector_t<Number>& constants);
 		~VertexEnumeration() = default;
 
-		std::vector<Halfspace<Number>> getHsv() const;
+		const std::vector<Halfspace<Number>>& getHsv() const;
 
-		std::vector<Dictionary<Number>> getDictionaries() const;
+		const std::vector<Dictionary<Number>>& getDictionaries() const;
 
-		std::vector<Point<Number>> getPositivePoints() const;
+		const std::vector<Point<Number>>& getPositivePoints() const;
 		void printPositivePoints() const;
 
-		std::vector<Point<Number>> getPoints() const;
+		const std::vector<Point<Number>>& getPoints() const;
 		void printPoints() const;
 
-		std::vector<vector_t<Number>> getLinealtySpace() const;
+		const std::vector<vector_t<Number>>& getLinealtySpace() const;
 		void printLinealtySpace() const;
 
-		std::vector<vector_t<Number>> getPositiveCones() const;
+		const std::vector<vector_t<Number>>& getPositiveCones() const;
 		void printPositiveCones() const;
 
-		std::vector<vector_t<Number>> getCones() const;
+		const std::vector<vector_t<Number>>& getCones() const;
 		void printCones() const;
 
 		void increment(Eigen::Index& i, Eigen::Index& j, std::size_t maxJ);
 
-		void enumerateVertices();
 		/**
 		 * @brief does everything with the halfspaces in mHsv
-		 *
 		 */
+		void enumerateVertices();
 
+		/**
+		 * @brief create vertices by permutating all d-combinations of half-spaces.
+		 */
 		void enumerateVerticesNaive();
 
-		VertexEnumeration(const std::vector<Halfspace<Number>>& hsv);
-		/**
-		 * @brief mHsv:=hsv
-		 */
-
-		VertexEnumeration(const matrix_t<Number>& constraints, const vector_t<Number>& constants);
-
-		void enumerateVertices(Dictionary<Number>& dictionary);
 		/**
 		 * @brief find all the vertices obtained from a given dictionary acording to Fukuda's algorithm.
 		 */
+		void enumerateVertices(Dictionary<Number>& dictionary);
 
-		void enumerateVerticesEachDictionary();
 		/**
 		 * @brief enumerate all the vertices of the dictionaries in mDictionaries.
 		 */
+		void enumerateVerticesEachDictionary();
 
-		void enumerateDictionaries();
 		/**
 		 * @brief Finds all the optimal dictionaries from the first one in mDictionaries.
 		 */
+		void enumerateDictionaries();
 
-		Dictionary<Number> findFirstVertex();
 		/**
 		 * @brief Finds a fist vertex, detects the linealty spaces and adds the corresponding constrains.
 		 */
+		Dictionary<Number> findFirstVertex();
 
-		int linearIndependance(std::map<int,vector_t<Number>> collection, vector_t<Number>& candidateRef) const;//not used
 		/**
 		 * @brief If the candidate is independent from the collection, returns the smallest non nul coordinate,
 		 * -1 otherwise.
 		 */
+		int linearIndependance(std::map<int,vector_t<Number>> collection, vector_t<Number>& candidateRef) const;//not used
 
-		std::vector<std::size_t> findIndepHs() const;//not used
 		/**
 		 * @return A set of the index of d independent hyperplanes of mHsv.
 		 */
+		std::vector<std::size_t> findIndepHs() const;//not used
 
-		Point<Number> findIntersection(const std::vector<std::size_t>& selectionRef) const;//not used
 		/**
 		 * @return the intersection of the hyperplanes in selectionRef.
 		 */
+		Point<Number> findIntersection(const std::vector<std::size_t>& selectionRef) const;//not used
 
-		bool findPositiveConstrains();
 		/**
 		 * @brief Transform Ax<=b into A'y<=b' & y>=0.
 		 */
+		bool findPositiveConstrains();
 
-		void toGeneralCoordinates();
 		/**
 		 * @brief Converts the positive cones and points into the original system of coordinates.
 		 */
+		void toGeneralCoordinates();
 
 		void findLinealtySpace();//not used
 
-		void addLinealtyConstrains();
 		/**
 		 * @brief Adds to halfspace set two halfspaces for each linealty direction to block it.
 		 */
+		void addLinealtyConstrains();
 	};
 } // namespace hypro
 
