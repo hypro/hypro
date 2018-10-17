26404a4fbb459a5b5bcbe6febc9f4ba9da28ff71
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2018-10-17 15:05:13 +0200

    added example to create a worker.

--- examples/example_worker.cpp
+++ examples/example_worker.cpp
@@ -2,11 +2,46 @@
  * @brief Example file which shows how to instanciate a concrete worker out of context handlers.
  */
 
+#include <datastructures/reachability/SettingsProvider.h>
+#include <datastructures/reachability/timing/EventTimingProvider.h>
 #include <algorithms/reachability/workers/ContextBasedReachabilityWorker.h>
+#include <parser/antlr4-flowstar/ParserWrapper.h>
+#include <iostream>
 
-int main() {
-    hypro::ReachabilitySettings<hypro::tNumber> settings;
+int main(int argc, char** argv) {
 
-    hypro::ContextBasedReachabilityWorker<double> worker = hypro::ContextBasedReachabilityWorker<double>(settings);
+    // read file from input
+    if(argc != 2) {
+        std::cout << "Need to pass input file as argument." << std::endl;
+        exit(1);
+    }
+
+    std::string filename = argv[1];
+    std::cout << "Read input file " << filename << std::endl;
+
+    std::pair<hypro::HybridAutomaton<double>, hypro::ReachabilitySettings<double>> parsedInput = hypro::parseFlowstarFile<double>(filename);
+    hypro::ReachabilitySettings<hypro::tNumber> convertedSettings(parsedInput.second);
+
+    std::vector<size_t> decomposition;
+    for(size_t i = 0; i < parsedInput.first.dimension();i++ ){
+        decomposition.push_back(i);
+    }
+    hypro::Decomposition decompositions;
+    decompositions.push_back(decomposition);
+    hypro::SettingsProvider<double>::getInstance().setSubspaceDecomposition(decompositions);
+
+    hypro::SettingsProvider<double>::getInstance().computeLocationSubspaceTypeMapping(parsedInput.first);
+    hypro::SettingsProvider<double>::getInstance().computeLocationTypeMapping(parsedInput.first);        
+
+
+    hypro::SettingsProvider<double>::getInstance().setHybridAutomaton(parsedInput.first);
+    hypro::SettingsProvider<double>::getInstance().setReachabilitySettings(convertedSettings);
+    hypro::EventTimingProvider<double>::getInstance().initialize(parsedInput.first);
+
+    hypro::WorkQueue<double> wq;
+
+
+
+    hypro::ContextBasedReachabilityWorker<double> worker = hypro::ContextBasedReachabilityWorker<double>(convertedSettings);
 	return 0;
 }
