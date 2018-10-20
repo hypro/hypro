bb8b3a78325114f09a505c4131028ec7ae909a01
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2018-10-20 12:00:38 +0200

    added function at() to access Decomposition objects.

--- src/hypro/datastructures/HybridAutomaton/decomposition/Decomposition.h
+++ src/hypro/datastructures/HybridAutomaton/decomposition/Decomposition.h
@@ -11,6 +11,7 @@ namespace hypro
         std::size_t size() const { return mDecomposition.size(); }
 
         void push_back(const std::vector<std::size_t>& decomp) {mDecomposition.push_back(decomp);}
+        const std::vector<std::size_t>& at(std::size_t pos) const {return mDecomposition.at(pos);}
 
         std::vector<std::vector<std::size_t>>::iterator begin() {return mDecomposition.begin();}
         std::vector<std::vector<std::size_t>>::const_iterator begin() const {return mDecomposition.begin();}
