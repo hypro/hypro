d0c5f6953698a0a2e9316b22512594d77531c99a
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2018-11-22 11:23:59 +0100

    bugfixes in carl polytope.

--- src/hypro/representations/Polytopes/carlPolytope/util.h
+++ src/hypro/representations/Polytopes/carlPolytope/util.h
@@ -27,19 +27,20 @@ namespace hypro {
         std::vector<Halfspace<N>> res;
         vector_t<N> normalV = vector_t<N>::Zero(dim);
         N offset = 0;
-        for(const auto& var : constraint.getVariables()) {
+        for(const auto& var : constraint.variables()) {
             assert(VariablePool::getInstance().hasDimension(var));
-            assert(constraint.getPolynomial().isLinear());
+            assert(constraint.lhs().isLinear());
+            assert(constraint.lhs().coeff(var,1).isNumber());
             if(constraint.relation() == carl::Relation::LEQ || constraint.relation() == carl::Relation::LESS || constraint.relation() == carl::Relation::EQ) {
-                normalV(VariablePool::getInstance().id(var)) = constraint.getPolynomial().coeff(var,1);
+                normalV(VariablePool::getInstance().id(var)) = constraint.lhs().coeff(var,1).constantPart();
             } else {
-                normalV(VariablePool::getInstance().id(var)) = -constraint.getPolynomial().coeff(var,1);
+                normalV(VariablePool::getInstance().id(var)) = -constraint.lhs().coeff(var,1).constantPart();
             }
         }
         if(constraint.relation() == carl::Relation::LEQ || constraint.relation() == carl::Relation::LESS || constraint.relation() == carl::Relation::EQ) {
-            offset = -constraint.getPolynomial().constantPart();
+            offset = -constraint.lhs().constantPart();
         } else {
-            offset = constraint.getPolynomial().constantPart();
+            offset = constraint.lhs().constantPart();
         }
         res.emplace_back(Halfspace<N>{normalV,offset});
         TRACE("hypro.representations.carlPolytope","Hsp: " << res.back());
@@ -60,9 +61,9 @@ namespace hypro {
         std::vector<ConstraintT<N>> constraints;
         formula.getConstraints(constraints);
 
-        for(const auto& c : formula.getConstraints) {
+        for(const auto& c : constraints) {
             auto tmp = constraintToHalfspace(c, dim);
-            res.emplace(res.end(), tmp.begin(), tmp.end());
+            res.insert(res.end(), tmp.begin(), tmp.end());
         }
 
         return res;
