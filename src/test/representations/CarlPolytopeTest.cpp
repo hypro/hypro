ed8d6cc1bbac7f422f74f369f66a91aec8340891
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2018-11-22 11:24:44 +0100

    added more extensive carl polytope tests.

--- src/test/representations/CarlPolytopeTest.cpp
+++ src/test/representations/CarlPolytopeTest.cpp
@@ -65,3 +65,56 @@ TYPED_TEST(CarlPolytopeTest, Intersection)
 
     EXPECT_EQ(result.getFormula(), result2.getFormula());
 }
+
+TYPED_TEST(CarlPolytopeTest, GetHalfspaces)
+{
+    carl::Interval<TypeParam> i1 = carl::Interval<TypeParam>{1,2};
+    carl::Interval<TypeParam> i2 = carl::Interval<TypeParam>{1,2};
+    std::vector<carl::Interval<TypeParam>> intervals;
+    intervals.push_back(i1);
+    intervals.push_back(i2);
+
+    hypro::CarlPolytope<TypeParam> c1{intervals};
+
+    auto hspVector = c1.getHalfspaces();
+
+    EXPECT_EQ(std::size_t(4),hspVector.size());
+    EXPECT_TRUE(std::find(hspVector.begin(), hspVector.end(), hypro::Halfspace<TypeParam>({1,0},2)) != hspVector.end());
+    EXPECT_TRUE(std::find(hspVector.begin(), hspVector.end(), hypro::Halfspace<TypeParam>({-1,0},-1)) != hspVector.end());
+    EXPECT_TRUE(std::find(hspVector.begin(), hspVector.end(), hypro::Halfspace<TypeParam>({0,1},2)) != hspVector.end());
+    EXPECT_TRUE(std::find(hspVector.begin(), hspVector.end(), hypro::Halfspace<TypeParam>({0,-1},-1)) != hspVector.end());
+}
+
+TYPED_TEST(CarlPolytopeTest, AddConstraints)
+{
+    carl::Interval<TypeParam> i1 = carl::Interval<TypeParam>{1,2};
+    carl::Interval<TypeParam> i2 = carl::Interval<TypeParam>{1,2};
+    std::vector<carl::Interval<TypeParam>> intervals;
+    intervals.push_back(i1);
+    intervals.push_back(i2);
+
+    hypro::CarlPolytope<TypeParam> c1{intervals};
+
+    auto hspVector = c1.getHalfspaces();
+
+    EXPECT_EQ(std::size_t(4),hspVector.size());
+    EXPECT_TRUE(std::find(hspVector.begin(), hspVector.end(), hypro::Halfspace<TypeParam>({1,0},2)) != hspVector.end());
+    EXPECT_TRUE(std::find(hspVector.begin(), hspVector.end(), hypro::Halfspace<TypeParam>({-1,0},-1)) != hspVector.end());
+    EXPECT_TRUE(std::find(hspVector.begin(), hspVector.end(), hypro::Halfspace<TypeParam>({0,1},2)) != hspVector.end());
+    EXPECT_TRUE(std::find(hspVector.begin(), hspVector.end(), hypro::Halfspace<TypeParam>({0,-1},-1)) != hspVector.end());
+    EXPECT_EQ(std::size_t(2),c1.dimension());
+
+    // add constraints which introduce a new variable - the dimension should be increased.
+    carl::Variable t = carl::freshRealVariable("t");
+    c1.addConstraint(hypro::ConstraintT<TypeParam>(hypro::PolyT<TypeParam>(t), carl::Relation::GEQ));
+    EXPECT_EQ(std::size_t(3),c1.dimension());
+
+    hspVector = c1.getHalfspaces();
+
+    EXPECT_EQ(std::size_t(5),hspVector.size());
+    EXPECT_TRUE(std::find(hspVector.begin(), hspVector.end(), hypro::Halfspace<TypeParam>({1,0,0},2)) != hspVector.end());
+    EXPECT_TRUE(std::find(hspVector.begin(), hspVector.end(), hypro::Halfspace<TypeParam>({-1,0,0},-1)) != hspVector.end());
+    EXPECT_TRUE(std::find(hspVector.begin(), hspVector.end(), hypro::Halfspace<TypeParam>({0,1,0},2)) != hspVector.end());
+    EXPECT_TRUE(std::find(hspVector.begin(), hspVector.end(), hypro::Halfspace<TypeParam>({0,-1,0},-1)) != hspVector.end());
+    EXPECT_TRUE(std::find(hspVector.begin(), hspVector.end(), hypro::Halfspace<TypeParam>({0,0,-1},0)) != hspVector.end());
+}
