87faae0b8d016e6ead88209a90616966832de4d3
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2018-12-03 08:34:23 +0100

    worked on travis setup.

--- .travis/setupOsx.sh
+++ .travis/setupOsx.sh
@@ -6,11 +6,7 @@ brew install llvm
 
 if [[ ${COMPILER} == "g++-4.8" ]]; then
 	echo "g++-4.8 is not supported"
-elif [[ ${COMPILER} == "g++-4.9" ]]; then
-	brew install gcc
-	defCXX gcc-4.9 g++-4.9
-elif [[ ${COMPILER} == "clang++-3.4" ]]; then
-	echo "clang++-3.4 is not supported"
-elif [[ ${COMPILER} == "clang++" ]]; then
-	defCXX clang clang++
+elif [[ ${COMPILER} == "g++-5" ]]; then
+	brew install gcc@5
+	brew link --overwrite gcc@5
 fi
