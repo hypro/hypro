be82a65c4359adafb3159042e10a43473547eb28
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2019-01-27 12:19:25 +0100

    improved box construction via constraints by checking for axis-aligned constraints.

--- src/hypro/representations/Box/BoxSetting.h
+++ src/hypro/representations/Box/BoxSetting.h
@@ -9,6 +9,8 @@ namespace hypro {
 		static constexpr bool HYPRO_BOX_AVOID_LINEAR_OPTIMIZATION = false;
 		// enables methods from interval arithmetic
 		static constexpr bool USE_INTERVAL_ARITHMETIC = true;
+		// detects upon construction of a box from constraints whether those already represent a box.
+		static constexpr bool DETECT_BOX = true;
 	};
 
 	struct BoxLinearOptimizationOff {
