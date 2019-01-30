a112ee1632e8ac919e23857e50ed793fb8a45570
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2019-01-30 11:26:51 +0100

    extended eigen utility + tests.

--- src/test/core/EigenAdaptionsTest.cpp
+++ src/test/core/EigenAdaptionsTest.cpp
@@ -7,10 +7,19 @@ class EigenAdaptionsTest : public ::testing::Test
 {
 protected:
     virtual void SetUp()
-    {}
+    {
+		matA = hypro::matrix_t<Number>(2,2);
+		matA << 1,2,3,4;
+
+		matB = hypro::matrix_t<Number>(4,4);
+		matB << 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16;
+	}
 
     virtual void TearDown()
     {}
+
+	hypro::matrix_t<Number> matA;
+	hypro::matrix_t<Number> matB;
 };
 
 TYPED_TEST(EigenAdaptionsTest, reduceNumberRepresentation)
@@ -20,9 +29,28 @@ TYPED_TEST(EigenAdaptionsTest, reduceNumberRepresentation)
 
 TYPED_TEST(EigenAdaptionsTest, createCode)
 {
-	hypro::matrix_t<TypeParam> a = hypro::matrix_t<TypeParam>(2,2);
-	a << 1,2,3,4;
-
-	std::string out = hypro::createCode(a,0);
+	std::string out = hypro::createCode(this->matA,0);
 	std::cout << out << std::endl;
+}
+
+TYPED_TEST(EigenAdaptionsTest, selectRows)
+{
+	hypro::matrix_t<TypeParam> resA = hypro::matrix_t<TypeParam>(2,4);
+	resA << 1,2,3,4,13,14,15,16;
+	EXPECT_EQ(resA, hypro::selectRows(this->matB,std::vector<Eigen::Index>({0,3})));
+
+	hypro::matrix_t<TypeParam> resB = hypro::matrix_t<TypeParam>(1,4);
+	resB << 5,6,7,8;
+	EXPECT_EQ(resB, hypro::selectRows(this->matB,std::vector<Eigen::Index>({1})));
+}
+
+TYPED_TEST(EigenAdaptionsTest, removeRows)
+{
+	hypro::matrix_t<TypeParam> resA = hypro::matrix_t<TypeParam>(2,4);
+	resA << 1,2,3,4,13,14,15,16;
+	EXPECT_EQ(resA, hypro::removeRows(this->matB,std::vector<Eigen::Index>({1,2})));
+
+	hypro::matrix_t<TypeParam> resB = hypro::matrix_t<TypeParam>(3,4);
+	resB << 1,2,3,4,9,10,11,12,13,14,15,16;
+	EXPECT_EQ(resB, hypro::removeRows(this->matB,std::vector<Eigen::Index>({1})));
 }
