89c7afa4a001cd7e558fb45b4efcab683d08f40f
mg632769 <marta.grobelna@rwth-aachen.de>
2019-03-03 11:13:03 +0100

    Adding tests.

---
+++ src/hypro/util/matlab/MTests/testMHyProLocation.m
@@ -0,0 +1,11 @@
+function res = testMHyProLocation
+
+loc1 = MHyProLocation();
+loc2 = MHyProLocation([1 0 1; 2 1 3; 1 1 1]);
+% loc3 = MHyProLocation([1 0 1; 2 1 3; 1 1 1], [1;1;2]);
+
+
+
+
+
+res = 1;
