7ba807fcbd0680899e21ee62e3a42831b2789568
mg632769 <marta.grobelna@rwth-aachen.de>
2019-06-24 16:01:18 +0200

    Adding benchmark for CORA

---
+++ src/hypro/util/matlab/CORA_benchmarks/car.m
@@ -0,0 +1,167 @@
+function complete = car()
+
+sim = 1;
+reacha = 0;
+
+% Load model
+if 1
+    % Polytope
+    HA = car_ha();
+    options.enclosureEnables = [3 5];
+    options.guardIntersect = 'polytope';
+    Zdelta = 0.00001 * ones(3,1);
+else
+    % Zono Girard
+    HA = car_ha_zG();
+    options.enclosureEnables = [1 2];
+    options.guardIntersect = 'zonoGirard';
+    Zdelta = zeros(3,1);
+end
+
+% options
+Zcenter = [13.88;50;0];
+options.R0 = zonotope([Zcenter,diag(Zdelta)]); %initial state for reachability analysis
+options.x0 = center(options.R0); %initial state for simulation
+
+
+options.taylorTerms = 10;
+options.zonotopeOrder = 20;
+options.polytopeOrder = 10;
+options.errorOrder=2;
+options.reductionTechnique = 'girard';
+options.isHyperplaneMap = 0;
+options.originContained = 0;
+
+%set input:
+for i = 1:3
+    options.timeStepLoc{i} = 0.01;
+    options.uLoc{i} = 0;
+    options.uLocTrans{i} = options.uLoc{i};
+    options.Uloc{i} = zonotope(options.uLoc{i});
+end
+
+figure 
+hold on
+
+% First location
+options.startLoc = 1; %initial location
+options.finalLoc = 0; %0: no final location
+options.tStart = 0; %start time
+options.tFinal = 4;
+options.projectedDimensions = [3 2];
+options.plotType = 'b';
+plot(HA,'simulation',options); %plot simulation
+
+% Second location
+% Zcenter = [37.63;20;4.75];
+% options.R0 = zonotope([Zcenter,diag(Zdelta)]); %initial state for reachability analysis
+% options.x0 = center(options.R0); %initial state for simulation
+% options.startLoc = 2; %initial location
+% options.finalLoc = 0; %0: no final location
+% options.tStart = 0; %start time
+% options.tFinal = 5;
+% options.projectedDimensions = [3 2];
+% options.plotType = 'b';
+% plot(HA,'simulation',options); %plot simulation
+
+
+
+% 
+% dim = 3;
+% vis = 1;
+% 
+% % Simulation --------------------------------------------------------------
+% 
+% if sim
+%     N = 50;
+%     tic;
+%     for i=1:N
+%         %set initial state, input
+%         if i == 1
+%             %simulate center
+%             options.x0 = center(options.R0);
+%         elseif i < 5
+%             % simulate extreme points
+%             options.x0 = randPointExtreme(options.R0);
+%         else
+%             % simulate random points
+%             options.x0 = randPoint(options.R0);
+%         end 
+% 
+%         %simulate hybrid automaton
+%         HAsim = simulate(HA,options);
+%         simRes{i} = get(HAsim,'trajectory');
+%     end
+%     toc;
+%     disp(['Time needed for the simulation: ', num2str(toc)]);
+% 
+%     % Visualization -------------------------------------------------------
+%     figure 
+%     hold on
+%     box on
+%     options.projectedDimensions = [3 1];
+%     options.plotType = {'b','m','g'};
+%     plotFilled(options.R0,options.projectedDimensions,'w','EdgeColor','k'); %plot initial set
+%     for i = 1:length(simRes)
+%        for j = 1:length(simRes{i}.x)
+%            plot(simRes{i}.x{j}(:,options.projectedDimensions(1)), ...
+%                 simRes{i}.x{j}(:,options.projectedDimensions(2)),'k'); 
+%        end
+%     end
+%     xlabel('t');
+%     ylabel('v');
+%     
+%     figure 
+%     hold on
+%     box on
+%     options.projectedDimensions = [3 2];
+%     options.plotType = {'b','m','g'};
+%     plotFilled(options.R0,options.projectedDimensions,'w','EdgeColor','k'); %plot initial set
+% 
+%     for i = 1:length(simRes)
+%        for j = 1:length(simRes{i}.x)
+%            plot(simRes{i}.x{j}(:,options.projectedDimensions(1)), ...
+%                 simRes{i}.x{j}(:,options.projectedDimensions(2)),'k'); 
+%        end
+%     end
+%     xlabel('t');
+%     ylabel('d');
+% end
+% 
+% 
+% % Reachability ------------------------------------------------------------
+% if reacha
+%     tic;
+%     [HA] = reach(HA,options);
+%     toc;
+%     disp(['Time needed for the analysis: ', num2str(toc)]);
+%     
+%     % Verification --------------------------------------------------------
+%     
+%     %TODO
+%     
+% % Visualization -------------------------------------------------------
+% if vis    
+%     figure 
+%     hold on
+%     options.projectedDimensions = [3 1];
+% 
+%     options.plotType = 'b';
+%     plot(HA,'reachableSet',options); %plot reachable set
+%     plotFilled(options.R0,options.projectedDimensions,'w','EdgeColor','k'); %plot initial set
+%     xlabel('t');
+%     ylabel('v');
+%     
+%     figure 
+%     hold on
+%     options.projectedDimensions = [3 2];
+% 
+%     options.plotType = 'b';
+%     plot(HA,'reachableSet',options); %plot reachable set
+%     plotFilled(options.R0,options.projectedDimensions,'w','EdgeColor','k'); %plot initial set
+%     xlabel('t');
+%     ylabel('d');
+% end
+% end
+
+complete = 1;
