7c903f85005fc162eff6593c4cb9ebd8f9c55aea
marta.grobelna <marta.grobelna@rwth-aachen.de>
2019-07-04 13:03:46 +0200

    Adding benchmark

---
+++ src/hypro/util/matlab/CORA_benchmarks/filtered_oscillator_8.m
@@ -0,0 +1,111 @@
+function complete = filtered_oscillator_8()
+
+sim = 1;
+reacha = 1;
+
+% Load model
+HA = filtered_oscillator_8_ha();
+options.enclosureEnables = [3 5];
+options.guardIntersect = 'polytope';
+Zdelta = [0.05;0.1;0;0;0;0;0;0;0;0];
+
+% options
+Zcenter = [0.25;0;0;0;0;0;0;0;0;0];
+options.R0 = zonotope([Zcenter,diag(Zdelta)]); %initial state for reachability analysis
+options.x0 = center(options.R0); %initial state for simulation
+
+
+options.taylorTerms = 10;
+options.zonotopeOrder = 20;
+options.polytopeOrder = 10;
+options.errorOrder=2;
+options.reductionTechnique = 'girard';
+options.isHyperplaneMap = 0;
+options.originContained = 0;
+
+%set input:
+for i = 1:4
+    options.timeStepLoc{i} = 0.05;
+    options.uLoc{i} = 0;
+    options.uLocTrans{i} = options.uLoc{i};
+    options.Uloc{i} = zonotope(options.uLoc{i});
+end
+
+% First location
+options.startLoc = 3; %initial location
+options.finalLoc = 0; %0: no final location
+options.tStart = 0; %start time
+options.tFinal = 4;
+
+dim = 10;
+vis = 1;
+
+% Simulation --------------------------------------------------------------
+
+if sim
+    N = 50;
+    tic;
+    for i=1:N
+        %set initial state, input
+        if i == 1
+            %simulate center
+            options.x0 = center(options.R0);
+        elseif i < 5
+            % simulate extreme points
+            options.x0 = randPointExtreme(options.R0);
+        else
+            % simulate random points
+            options.x0 = randPoint(options.R0);
+        end 
+
+        %simulate hybrid automaton
+        HAsim = simulate(HA,options);
+        simRes{i} = get(HAsim,'trajectory');
+    end
+    toc;
+    disp(['Time needed for the simulation: ', num2str(toc)]);
+
+    % Visualization -------------------------------------------------------
+    figure 
+    hold on
+    box on
+    options.projectedDimensions = [1 3];
+    options.plotType = {'b','m','g'};
+    plotFilled(options.R0,options.projectedDimensions,'w','EdgeColor','k'); %plot initial set
+    for i = 1:length(simRes)
+       for j = 1:length(simRes{i}.x)
+           plot(simRes{i}.x{j}(:,options.projectedDimensions(1)), ...
+                simRes{i}.x{j}(:,options.projectedDimensions(2)),'k'); 
+       end
+    end
+%     xlabel('t');
+%     ylabel('v');
+end
+
+
+% Reachability ------------------------------------------------------------
+if reacha
+    tic;
+    [HA] = reach(HA,options);
+    toc;
+    disp(['Time needed for the analysis: ', num2str(toc)]);
+    
+    % Verification --------------------------------------------------------
+    
+    %TODO
+    
+% Visualization -------------------------------------------------------
+if vis    
+    figure 
+    hold on
+    options.projectedDimensions = [1 3];
+
+    options.plotType = 'b';
+    plot(HA,'reachableSet',options); %plot reachable set
+    plotFilled(options.R0,options.projectedDimensions,'w','EdgeColor','k'); %plot initial set
+%     xlabel('t');
+%     ylabel('v');
+end
+end
+
+complete = 1;
