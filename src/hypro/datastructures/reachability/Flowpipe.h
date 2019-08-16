e8be517cc14bc23ec35a16e21ed22515cec88184
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2019-08-16 15:33:07 +0200

    added selectors for flowpipe.

--- src/hypro/datastructures/reachability/Flowpipe.h
+++ src/hypro/datastructures/reachability/Flowpipe.h
@@ -1,5 +1,7 @@
 #pragma once
-
+#include "../../util/plotting/PlotData.h"
+#include "../../util/Range.h"
+#include "../../datastructures/HybridAutomaton/Condition.h"
 
 /*
     Requirements:
@@ -11,16 +13,30 @@ namespace hypro {
 /**
  * @brief Container for a flowpipe, i.e. an ordered sequence of state sets (flowpipe segments).
  *
- * @tparam Number Used number type.
+ * @tparam State Used state type.
  */
-template<typename Number>
+template<typename State>
 class Flowpipe
 {
 private:
-    /* data */
+    std::vector<State> mStates;
 public:
     Flowpipe(/* args */);
     ~Flowpipe();
+
+    void setStates(const std::vector<State>& states) { mStates = states; }
+    void setStates(std::vector<State>&& states) { mStates = std::move(states); }
+    void addState(const State& state) { mStates.emplace_back(state); }
+    void addState(State&& state) { mStates.emplace_back(state); }
+
+    const std::vector<State>& getStates() const { return mStates; }
+    std::vector<State>& rGetStates() { return mStates; }
+    Range<std::vector<State>> selectTimestamps(const carl::Interval<tNumber>& span) { return selectFirstConsecutiveRange(mStates, [&](const State& in)->bool{ return in.timeStamp.intersects(span); } ); }
+
+    template<typename Number>
+    Range<std::vector<State>> selectSatisfyingStates(const Condition<Number>& cond) { return selectFirstConsecutiveRange(mStates, [&](const State& in)->bool{ return in.satisfiesHalfspaces(cond.getMatrix(), cond.getVector()); } ); }
+
+    std::vector<PlotData<State>> getPlotData(std::size_t refinementLevel = 0, std::size_t threadId = 0) const;
 };
 
 } // namespace hypro
--- src/hypro/datastructures/reachability/Flowpipe.tpp
+++ src/hypro/datastructures/reachability/Flowpipe.tpp
@@ -2,14 +2,23 @@
 
 namespace hypro
 {
-    template<typename Number>
-    Flowpipe<Number>::Flowpipe(/* args */)
+    template<typename State>
+    Flowpipe<State>::Flowpipe(/* args */)
     {
     }
 
-    template<typename Number>
-    Flowpipe<Number>::~Flowpipe()
+    template<typename State>
+    Flowpipe<State>::~Flowpipe()
     {
+    }
+
+    template<typename State>
+    std::vector<PlotData<State>> Flowpipe<State>::getPlotData(std::size_t refinementLevel, std::size_t threadId) const {
+        std::vector<PlotData<State>> res;
+        for(const auto& state : mStates) {
+            res.emplace_back(state,refinementLevel,threadId);
+        }
+        return res;
     }
 
 } // namespace hypro
