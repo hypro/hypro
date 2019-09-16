c5c1948f1cbacb927f6f11b250e2003b57782b03
marta <marta.grobelna@rwth-aachen.de>
2019-09-16 08:37:26 +0200

    Moving files

--- examples/example_box.cpp
+++ examples/example_box.cpp
@@ -7,212 +7,69 @@
 #include "representations/GeometricObject.h"
 #include "datastructures/Halfspace.h"
 #include "util/plotting/Plotter.h"
-#include <sys/time.h>
 
 int main()
 {
 	// use rational arithmetic.
 	typedef mpq_class Number;
-	struct timeval t1, t2;
-    	double elapsedTime;
 
-	carl::Interval<Number> *intv1 = new carl::Interval<Number>(0,1);
-	carl::Interval<Number> *intv2 = new carl::Interval<Number>(1,12);
-	carl::Interval<Number> *intv3 = new carl::Interval<Number>(-1,10);
-	carl::Interval<Number> *intv4 = new carl::Interval<Number>(-10,5);
-	carl::Interval<Number> *intv5 = new carl::Interval<Number>(20,34);
-	std::vector<carl::Interval<Number>> intervals;
-	intervals.emplace_back(*intv1);
-	intervals.emplace_back(*intv2);
-	intervals.emplace_back(*intv3);
-	intervals.emplace_back(*intv4);
-	intervals.emplace_back(*intv5);
-	
-	carl::Interval<Number> *intv1_2 = new carl::Interval<Number>(-10,1);
-	carl::Interval<Number> *intv2_2 = new carl::Interval<Number>(7,12);
-	carl::Interval<Number> *intv3_2 = new carl::Interval<Number>(-5,11);
-	carl::Interval<Number> *intv4_2 = new carl::Interval<Number>(-6,3);
-	carl::Interval<Number> *intv5_2 = new carl::Interval<Number>(0,3);
-	std::vector<carl::Interval<Number>> intervals2;
-	intervals2.emplace_back(*intv1_2);
-	intervals2.emplace_back(*intv2_2);
-	intervals2.emplace_back(*intv3_2);
-	intervals2.emplace_back(*intv4_2);
-	intervals2.emplace_back(*intv5_2);
-	
-	
-	hypro::Box<Number> box1(intervals);
-	hypro::Box<Number> box2(intervals2);
+	// get plotter reference.
+	hypro::Plotter<Number>& plotter = hypro::Plotter<Number>::getInstance();
 
 	// create some transformation matrix.
-	hypro::matrix_t<Number> A = hypro::matrix_t<Number>::Zero(5,5);
-	A << 10,14,7,-9,14,14,7,-6,-8,-10,7,9,8,
-	11,1,-10,9,-10,8,-1,12,0,-3,-2,9;
+	hypro::matrix_t<Number> A = hypro::matrix_t<Number>::Zero(3,3);
+	A(0,0) = 1;
+	A(1,1) = carl::convert<double,Number>(carl::cos(45));
+	A(1,2) = carl::convert<double,Number>(-carl::sin(45));
+	A(2,1) = carl::convert<double,Number>(carl::sin(45));
+	A(2,2) = carl::convert<double,Number>(carl::cos(45));
 
 	// create some translation vector.
-	hypro::vector_t<Number> b = hypro::vector_t<Number>::Zero(5);
-	b(0) = 1;
-	b(1) = 2;
-	b(2) = 3;
-	b(3) = 4;
-	b(4) = 5;	
+	hypro::vector_t<Number> b = hypro::vector_t<Number>::Zero(3);
 
-	std::cout << "------------------------------"<< std::endl;
- 	std::cout << "vertices" << std::endl;
- 	std::cout << "------------------------------"<< std::endl;
-
-	// start timer
-	gettimeofday(&t1, NULL);
+	// create a box out of two given limit points.
+	hypro::Box<Number> testbox(std::make_pair(hypro::Point<Number>({-2,2,-4}), hypro::Point<Number>({2,4,-2})));
 
 	// compute all vertices of the box and output them.
-	std::vector<hypro::Point<Number>> tvertices = box1.vertices();
-
-	// stop timer
-	gettimeofday(&t2, NULL);
-	
-	
+	std::vector<hypro::Point<Number>> tvertices = testbox.vertices();
 	for(const auto& vertex : tvertices)
 		std::cout << vertex << std::endl;
 
-	std::cout << box1 << std::endl;
-	
-
-	// compute and print the elapsed time in millisec
-	elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;      // sec to ms
-	elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;   // us to ms
-
-	std::cout << "Time spent: " << elapsedTime << "ms\n" << std::endl;
-
-	std::cout << "------------------------------"<< std::endl;
- 	std::cout << "Minkowski sum" << std::endl;
- 	std::cout << "------------------------------"<< std::endl;
-	
-	// start timer
-	gettimeofday(&t1, NULL);
-
-	// compute all vertices of the box and output them.
-		
-	hypro::Box<Number> res = box1.minkowskiSum(box2);
-	// stop timer
-	gettimeofday(&t2, NULL);
-	
-	// compute and print the elapsed time in millisec
-	elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;      // sec to ms
-	elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;   // us to ms
-
-	std::cout << "Time spent: " << elapsedTime << "ms\n" << std::endl;
-	
-	std::cout << "------------------------------"<< std::endl;
- 	std::cout << "Affine transformation" << std::endl;
- 	std::cout << "------------------------------"<< std::endl;	
-	
-	// start timer
-	gettimeofday(&t1, NULL);
+	std::cout << testbox << std::endl;
 
 	// transform the initial box with the created matrix and vector (affine transformation).
-	hypro::Box<Number> res2 = box1.affineTransformation(A,b);
+	hypro::Box<Number> res = testbox.affineTransformation(A,b);
 
-	std::vector<hypro::Point<Number>> tve = res2.vertices();
-	for(const auto& vertex : tve)
+	std::cout << res << std::endl;
+
+	std::vector<hypro::Point<Number>> vertices = res.vertices();
+	for(const auto& vertex : vertices)
 		std::cout << vertex << std::endl;
 
-	// stop timer
-	gettimeofday(&t2, NULL);
-	
-	// compute and print the elapsed time in millisec
-	elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;      // sec to ms
-	elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;   // us to ms
+	// create a second box which is two dimensional.
+	hypro::Box<Number> testbox2(std::make_pair(hypro::Point<Number>({-2,-2}), hypro::Point<Number>({2,2})));
 
-	std::cout << "Time spent: " << elapsedTime << "ms\n" << std::endl;
+	// create a halfspace (cutter) from a normal vector and an offset.
+	hypro::matrix_t<Number> normal = hypro::matrix_t<Number>(1,2);
+	hypro::vector_t<Number> offset = hypro::vector_t<Number>(1);
+	normal << 1,1;
+	offset << carl::rationalize<Number>(-0.5);
+	hypro::vector_t<Number> hsNormal = hypro::vector_t<Number>(2);
+	hsNormal << 1,1;
+	hypro::Halfspace<Number> cutter = hypro::Halfspace<Number>(hsNormal, carl::rationalize<Number>(-0.5));
 
+	// we can also plot halfspaces
+	plotter.addObject(cutter);
+	unsigned original = plotter.addObject(testbox2.vertices());
 
- 	std::cout << "------------------------------"<< std::endl;
- 	std::cout << "union" << std::endl;
- 	std::cout << "------------------------------"<< std::endl;
+	// add the intersection of the second box and the created halfspace to the plotter instance.
+	// Note: satisfiesHalfspaces returns a pair <bool, Representation>.
+	unsigned cutted = plotter.addObject(testbox2.satisfiesHalfspaces(normal, offset).second.vertices());
 
-	// start timer
-	gettimeofday(&t1, NULL);
-
-	hypro::Box<Number> res3 = box1.unite(box2);
-	// stop timer
-	gettimeofday(&t2, NULL);
-	
-	std::vector<hypro::Point<Number>> tve2 = res3.vertices();
-	for(const auto& vertex : tve2)
-		std::cout << vertex << std::endl;
-	
-	// compute and print the elapsed time in millisec
-	elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;      // sec to ms
-	elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;   // us to ms
-
-	std::cout << "Time spent: " << elapsedTime << "ms\n" << std::endl;
-	
-	std::cout << "------------------------------"<< std::endl;
- 	std::cout << "intersect" << std::endl;
- 	std::cout << "------------------------------"<< std::endl;
-
-	// start timer
-	gettimeofday(&t1, NULL);
-
-	hypro::Box<Number> res4 = box1.intersect(box2);
-	
-	// stop timer
-	gettimeofday(&t2, NULL);
-	
-	std::vector<hypro::Point<Number>> tve3 = res4.vertices();
-	for(const auto& vertex : tve3)
-		std::cout << vertex << std::endl;
-	
-	// compute and print the elapsed time in millisec
-	elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;      // sec to ms
-	elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;   // us to ms
-
-	std::cout << "Time spent: " << elapsedTime << "ms\n" << std::endl;
-
-	std::cout << "------------------------------"<< std::endl;
- 	std::cout << "supremum" << std::endl;
- 	std::cout << "------------------------------"<< std::endl;
-	
-	// start timer
-	gettimeofday(&t1, NULL);
-
-	auto res5 = box1.supremum();
-	
-	// stop timer
-	gettimeofday(&t2, NULL);
-
-	// compute and print the elapsed time in millisec
-	elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;      // sec to ms
-	elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;   // us to ms
-
-	std::cout << "Time spent: " << elapsedTime << "ms\n" << std::endl;
-
-	std::cout << "------------------------------"<< std::endl;
- 	std::cout << "scale" << std::endl;
- 	std::cout << "------------------------------"<< std::endl;
-
-	// start timer
-	gettimeofday(&t1, NULL);
-
-	auto res6 = box1 * 10;
-	
-	// stop timer
-	gettimeofday(&t2, NULL);
-
-	// compute and print the elapsed time in millisec
-	elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;      // sec to ms
-	elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;   // us to ms
-
-	std::cout << "Time spent: " << elapsedTime << "ms\n" << std::endl;
-	
-	//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-	
-	
-//	carl::Interval<Number> intv1 = new carl::Interval<Number>(0,1);
-//	carl::Interval<Number> intv2 = new carl::Interval<Number>(1,1);
-
-//	carl::Interval<Number> res1 = intv1 + intv2;
+	// set colors and plot (gnuplot).
+	plotter.setObjectColor(original, hypro::plotting::colors[hypro::plotting::green]);
+	plotter.setObjectColor(cutted, hypro::plotting::colors[hypro::plotting::red]);
+	plotter.plot2d();
 
 	return 0;
 }
-
