ecdc09e0ad22e95bb12d0890cff9f34b6ee62fc4
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2019-10-31 14:52:51 +0100

    fixed loading of carl.

--- src/resources/carl.cmake
+++ src/resources/carl.cmake
@@ -13,9 +13,11 @@ execute_process(
 ExternalProject_Add(
 	CArL-EP
 	DOWNLOAD_COMMAND ""
+	CMAKE_ARGS "-DUSE_BLISS=ON -DUSE_COCOA=ON -DEXPORT_TO_CMAKE=OFF"
 	CONFIGURE_COMMAND ""
 	BUILD_COMMAND ${CMAKE_MAKE_PROGRAM} carl
-	INSTALL_COMMAND ${CMAKE_MAKE_PROGRAM} install/fast
+	INSTALL_COMMAND ""
+	#INSTALL_COMMAND ${CMAKE_MAKE_PROGRAM} install/fast
 )
 
 include(${CMAKE_BINARY_DIR}/resources/src/CArL-EP-build/carlConfig.cmake)
@@ -23,32 +25,3 @@ message("Include ${CMAKE_BINARY_DIR}/resources/src/CArL-EP-build/carlConfig.cmak
 add_dependencies(carl-shared CArL-EP)
 add_dependencies(carl-static CArL-EP)
 add_dependencies(resources carl-static carl-shared)
-
-#ExternalProject_Add(
-#	carl
-#	GIT_REPOSITORY http://github.com/smtrat/carl.git
-#	#GIT_TAG "17.04"
-#	BUILD_COMMAND make carl
-#	INSTALL_COMMAND "")
-#
-#ExternalProject_Add_Step(carl ReloadCMake
-#	COMMAND cmake ../..
-#	COMMENT "Re-run CMake."
-#	DEPENDEES build
-#	)
-#
-#add_custom_target(Boost_SHARED)
-#
-#
-##Todo: Re-check, it should be possible to use load_library() afterwards, shouldn't it? Otherwise: Update lists
-#add_imported_library(carl SHARED "${carl_LIBRARIES}" "${carl_INCLUDE_DIR}")
-#add_imported_library(lib_carl_static STATIC "${carl_LIBRARIES}" "${carl_INCLUDE_DIR}")
-#list(APPEND ${PROJECT_NAME}_LIBRARIES_STATIC ${carl_LIBRARIES})
-#list(APPEND ${PROJECT_NAME}_INCLUDE_DIRS ${carl_INCLUDE_DIR})
-#set(${PROJECT_NAME}_INCLUDE_DIRS ${${PROJECT_NAME}_INCLUDE_DIRS} PARENT_SCOPE)
-#set(${PROJECT_NAME}_LIBRARIES_STATIC ${${PROJECT_NAME}_LIBRARIES_STATIC} PARENT_SCOPE)
-#
-#add_dependencies(carl carl)
-#add_dependencies(lib_carl_static carl)
-#add_dependencies(resources carl)
-#add_dependencies(resources lib_carl_static)
