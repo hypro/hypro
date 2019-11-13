9618e633046efd6e098b55292b7db80578b79950
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2019-11-13 16:19:40 +0100

    fixed bug in FM-variable elimination related to an interface-change in carl.

--- src/hypro/algorithms/quantifierElimination/FourierMotzkinQE.h
+++ src/hypro/algorithms/quantifierElimination/FourierMotzkinQE.h
@@ -4,41 +4,41 @@
 namespace hypro {
 
 /**
- * @brief Provides a simple implementation for Fourier Motzkin variable elimination for linear, existentially quantified constraints.
+ * @brief Provides a simple implementation for Fourier Motzkin variable elimination for linear, existentially quantified
+ * constraints.
  *
  */
-template<typename Number>
+template <typename Number>
 class FourierMotzkinQE {
-public:
-    // we use four vectors, one for the discovered lower bounds, one for the upper bounds, one for the equations and one for unrelated constraints.
-    using FormulaPartition = std::vector<FormulasT<Number>>;
+  public:
+	// we use four vectors, one for the discovered lower bounds, one for the upper bounds, one for the equations and one
+	// for unrelated constraints.
+	using FormulaPartition = std::vector<FormulasT<Number>>;
 
-private:
-    QEQuery mQuery;
-    FormulaT<Number> mFormula;
-public:
+  private:
+	QEQuery mQuery;
+	FormulaT<Number> mFormula;
 
-    FourierMotzkinQE(const FormulaT<Number>& qfree, const QEQuery& quantifiers)
-        : mQuery(quantifiers)
-        , mFormula(qfree)
-    {
-        assert(qfree.getType() == carl::FormulaType::CONSTRAINT || qfree.isRealConstraintConjunction());
-    }
+  public:
+	FourierMotzkinQE( const FormulaT<Number>& qfree, const QEQuery& quantifiers )
+		: mQuery( quantifiers ), mFormula( qfree ) {
+		assert( qfree.getType() == carl::FormulaType::CONSTRAINT || qfree.isRealConstraintConjunction() );
+	}
 
-    FormulaT<Number> eliminateQuantifiers();
+	FormulaT<Number> eliminateQuantifiers();
 
-private:
-    FormulaPartition findBounds(const carl::Variable& variable);
+  private:
+	FormulaPartition findBounds( const carl::Variable& variable );
 
-    FormulasT<Number> createNewConstraints(const FormulaPartition& bounds, carl::Variable v);
+	FormulasT<Number> createNewConstraints( const FormulaPartition& bounds, carl::Variable v );
 
-    FormulasT<Number> substituteEquations(const FormulaPartition& bounds, carl::Variable v);
+	FormulasT<Number> substituteEquations( const FormulaPartition& bounds, carl::Variable v );
 
-    bool isLinearLowerBound(const ConstraintT<Number>& f, carl::Variable v);
+	bool isLinearLowerBound( const ConstraintT<Number>& f, carl::Variable v );
 
-    PolyT<Number> getRemainder(const ConstraintT<Number>& c, carl::Variable v, bool isLowerBnd);
+	PolyT<Number> getRemainder( const ConstraintT<Number>& c, carl::Variable v, bool isLowerBnd );
 };
 
-} // hypro
+}  // namespace hypro
 
 #include "FourierMotzkinQE.tpp"
