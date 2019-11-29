e8c005172d2083307ed07c616d4c38a35e33b8ef
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2019-11-29 11:09:34 +0100

    fixed linkage and external projects for coin-or lp-solver.

--- src/resources/clp.cmake
+++ src/resources/clp.cmake
@@ -1,3 +1,6 @@
+
+file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/resources/src/coinUtils/include/coin/)
+
 ExternalProject_Add(
     coinUtils
     LOG_CONFIGURE 1
@@ -6,7 +9,7 @@ ExternalProject_Add(
     GIT_REPOSITORY https://github.com/coin-or/CoinUtils.git
     GIT_SHALLOW 1
     UPDATE_COMMAND ""
-    CONFIGURE_COMMAND ./configure
+    CONFIGURE_COMMAND ./configure --enable-static
     BUILD_IN_SOURCE 1
     INSTALL_COMMAND
 	)
@@ -17,10 +20,15 @@ ExternalProject_Get_Property(coinUtils binary_dir)
 message(STATUS "CoinUtils source dir: ${source_dir}")
 message(STATUS "CoinUtils binary dir: ${binary_dir}")
 
-add_imported_library(coinUtils STATIC "${binary_dir}/lib/libCoinUtils.la" "${source_dir}/include/coin")
+set( coinUtils_INCLUDE_DIR "${source_dir}/include/coin" )
+set( coinUtils_LIBRARIES "${binary_dir}/lib/libCoinUtils.a" )
+
+add_imported_library(coinUtils STATIC "${coinUtils_LIBRARIES}" "${coinUtils_INCLUDE_DIR}")
 
 add_dependencies(coinUtils_STATIC clp)
 add_dependencies(resources coinUtils_STATIC)
+
+file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/resources/src/clp/include/coin/)
 
 ExternalProject_Add(
     clp
@@ -31,7 +39,7 @@ ExternalProject_Add(
     GIT_REPOSITORY https://github.com/coin-or/Clp.git
     GIT_SHALLOW 1
     UPDATE_COMMAND ""
-    CONFIGURE_COMMAND ./configure --with-coinutils-lib=${binary_dir}/lib/libCoinUtils.la --with-coinutils-incdir=${source_dir}/include/coin
+    CONFIGURE_COMMAND ./configure --with-coinutils-lib=${binary_dir}/lib/libCoinUtils.la --with-coinutils-incdir=${source_dir}/include/coin --enable-static
     BUILD_IN_SOURCE 1
     INSTALL_COMMAND
     TEST_COMMAND ${CMAKE_MAKE_PROGRAM} test
@@ -45,8 +53,7 @@ message(STATUS "Clp source dir: ${source_dir}")
 message(STATUS "Clp binary dir: ${binary_dir}")
 
 set( clp_INCLUDE_DIR "${source_dir}/include/coin" )
-set( clp_LIBRARIES "${binary_dir}/lib/libClp.la" )
-
+set( clp_LIBRARIES "${binary_dir}/lib/libClp.a" )
 
 add_imported_library(clp STATIC ${clp_LIBRARIES} ${clp_INCLUDE_DIR})
 
