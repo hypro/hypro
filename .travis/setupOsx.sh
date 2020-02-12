dce62bb298461dc04d13dca361b1b0cf4c209cdb
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2020-02-12 10:43:25 +0100

    cleanup.

--- .travis/setupOsx.sh
+++ .travis/setupOsx.sh
@@ -18,14 +18,16 @@ fi
 # boost <= 1.70 is currently required until we can use a newer version of CArL which supports osx.
 # first remove version 1.71.0 which is currently installed (apparently)
 brew uninstall --ignore-dependencies boost
-wget https://dl.bintray.com/boostorg/release/1.65.1/source/boost_1_65_1.tar.gz
-tar -xzf boost_1_65_1.tar.gz
-cd boost_1_65_1
-mkdir build
-./bootstrap.sh --with-libraries=chrono,system,program_options,test,timer --prefix=build
-keep_waiting &
-		./b2 -d0 || return 1
-kill $!
-keep_waiting &
-		./b2 install || return 1
-kill $!
+
+# note: probably it is sufficient to just remove boost 1.71.0, as Carl maybe ships boost 1.69.0
+#wget https://dl.bintray.com/boostorg/release/1.65.1/source/boost_1_65_1.tar.gz
+#tar -xzf boost_1_65_1.tar.gz
+#cd boost_1_65_1
+#mkdir build
+#./bootstrap.sh --with-libraries=chrono,system,program_options,test,timer --prefix=build
+#keep_waiting &
+#		./b2 -d0 || return 1
+#kill $!
+#keep_waiting &
+#		./b2 install || return 1
+#kill $!
