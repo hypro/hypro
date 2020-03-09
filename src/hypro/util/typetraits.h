d66319735086e1a8615b529a44d8f5802aabc804
Leander Behr <l.behr19199@gmail.com>
2020-03-09 14:15:53 +0100

    Squashed commit of the following:
    commit e781bb55a77863dd89f10b7bc6aead92ce1e189f
    Author: Leander Behr <l.behr19199@gmail.com>
    Date:   Fri Mar 6 20:47:21 2020 +0100
    
        fixed perfect forwarding ctor to only take types that dont cause compile errors and not subsume the other ctors
    
    commit 4c7d7d3b25ecfa8ba73736b1d23dbcde1055a89a
    Author: Leander Behr <l.behr19199@gmail.com>
    Date:   Fri Mar 6 20:46:01 2020 +0100
    
        we do need a default ctor, but we should default it
    
    commit fb6f89610f450319059dc847a631b94cecda7ea7
    Author: Leander Behr <l.behr19199@gmail.com>
    Date:   Fri Mar 6 17:20:07 2020 +0100
    
        Added rvalue getters for normal and offset
    
    commit 18b20c99a27abd2edb22e2e974b98591ce10fee6
    Author: Leander Behr <l.behr19199@gmail.com>
    Date:   Fri Mar 6 16:49:09 2020 +0100
    
        added perfect forwarding ctor from vector and offset
    
    commit 3e52faa88dae5f33c8638b9baca7221a67d83cf9
    Author: Leander Behr <l.behr19199@gmail.com>
    Date:   Fri Mar 6 16:29:22 2020 +0100
    
        Enforced rule of zero. All 6 special member functions are defaulted.
    
    commit cb639ece1c374f4d23e6b4fdda15c1ef9081712e
    Author: Leander Behr <l.behr19199@gmail.com>
    Date:   Fri Mar 6 16:18:21 2020 +0100
    
        removed mHash and mIsInteger. mHash wasn't use
        and mIsInteger was replaced by an explicit check

--- src/hypro/util/typetraits.h
+++ src/hypro/util/typetraits.h
@@ -16,4 +16,11 @@ struct is_unique_ptr : std::false_type {};
 template <class T, class D>
 struct is_unique_ptr<std::unique_ptr<T, D>> : std::true_type {};
 
+// short hands
+template <bool B>
+using enable_if = std::enable_if_t<B, int>;
+
+template <typename T, typename U>
+constexpr bool convertible = std::is_convertible_v<T, U>;
+
 }  // namespace hypro
