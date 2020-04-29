4aeec9efc0c37e7f929ab83cbaa196c4c37a841d
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2020-04-29 14:58:57 +0200

    Further tool structure.

--- src/tool/main.cpp
+++ src/tool/main.cpp
@@ -1,11 +1,27 @@
 #include "../hypro/datastructures/reachability/SettingsProvider.h"
+#include "../hypro/parser/antlr4-flowstar/ParserWrapper.h"
 #include "../hypro/types.h"
 #include "../hypro/util/logging/Logger.h"
 #include "cli/cli.h"
+#include "preprocessing/preprocessing.h"
+#include "reachability/analysis.h"
 #include "typedefs.h"
 
 int main(int argc, char const *argv[]) {
   // parse command line arguments
   auto options = hydra::handleCMDArguments(argc, argv);
+
+  // parse model file
+  COUT("Passed model file: " << options["model"].as<std::string>());
+  auto automatonSettingsPair = hypro::parseFlowstarFile<hydra::Number>(
+      options["model"].as<std::string>());
+
+  // perform preprocessing
+  hydra::preprocessing::preprocess(automatonSettingsPair.first,
+                                   automatonSettingsPair.second);
+
+  // run reachability analysis
+  hydra::reachability::analyze();
+
   return 0;
 }
---
+++ src/tool/reachability/analysis.cpp
@@ -0,0 +1,9 @@
+#include "analysis.h"
+
+namespace hydra {
+namespace reachability {
+void analyze() {
+  // do interesting stuff.
+}
+} // namespace reachability
+} // namespace hydra
