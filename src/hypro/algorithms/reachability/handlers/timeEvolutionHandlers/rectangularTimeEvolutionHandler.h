21daaa10aab6524c5ba5ac51e6b0c082366f169f
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2020-05-29 14:09:35 +0200

    adapted time evolution handler.

--- src/hypro/algorithms/reachability/handlers/timeEvolutionHandlers/rectangularTimeEvolutionHandler.h
+++ src/hypro/algorithms/reachability/handlers/timeEvolutionHandlers/rectangularTimeEvolutionHandler.h
@@ -8,25 +8,10 @@
 #include <string>
 
 namespace hypro {
-template <typename State>
-class rectangularTimeEvolutionHandler {
-	using Number = typename State::NumberType;
 
-  protected:
-	State& mState;	// A state containing the first segment for each subspace
-	rectangularFlow<Number> mFlow;
-	std::size_t mIndex = 0;	 /// relict from multiset-computations after decomposition. Might be handy at some point.
-	bool mComputed = false;
+template <typename State, typename Number>
+State rectangularApplyTimeEvolution( const State& initialSet, const rectangularFlow<Number>& flow );
 
-  public:
-	rectangularTimeEvolutionHandler() = delete;
-	rectangularTimeEvolutionHandler( State& state, const rectangularFlow<Number>& flow )
-		: mState( state )
-		, mFlow( flow )
-		, mIndex( index ) {}
-
-	void operator()();
-};
 }  // namespace hypro
 
 #include "rectangularTimeEvolutionHandler.tpp"
