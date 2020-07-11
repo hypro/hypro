391fc4d4047c654755488d60f8382abbcda8e06d
Leander Behr <l.behr19199@gmail.com>
2020-07-11 23:23:50 +0200

    Added a header that makes boost::combine usable with structured bindings.

---
+++ src/hypro/util/combine.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <boost/range/combine.hpp>
+
+// destructuring fix for boost tuple
+namespace std {
+template <typename T, typename U>
+struct tuple_size<boost::tuples::cons<T, U>>
+	  : boost::tuples::length<boost::tuples::cons<T, U>>
+{ };
+
+template <size_t I, typename T, typename U>
+struct tuple_element<I, boost::tuples::cons<T, U>>
+	  : boost::tuples::element<I, boost::tuples::cons<T, U>>
+{ };
+}
