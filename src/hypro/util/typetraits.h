7589b9cb1748b9fd23dcec9457a5e47286bf7704
Leander Behr <l.behr19199@gmail.com>
2020-07-11 23:24:40 +0200

    Added a type trait to get the Representation type of an Analyzer type.

--- src/hypro/util/typetraits.h
+++ src/hypro/util/typetraits.h
@@ -16,18 +16,30 @@ struct is_unique_ptr : std::false_type {};
 template <class T, class D>
 struct is_unique_ptr<std::unique_ptr<T, D>> : std::true_type {};
 
-//representation traits
-template<class Representation>
+// TODO generalize with nth element type trait
+// representation traits
+template <class Representation>
 struct rep_number_i;
 
-template<template<class...> class Rep, class Number, class... Ts>
+template <template <class...> class Rep, class Number, class... Ts>
 struct rep_number_i<Rep<Number, Ts...>> {
-    using type = Number;
+	using type = Number;
 };
 
-template<class Representation>
+template <class Representation>
 using rep_number = typename rep_number_i<Representation>::type;
 
+// analyzer type trait
+template <class Representation>
+struct analyzer_rep_i;
+
+template <template <class...> class Analyzer, class Rep, class... Ts>
+struct analyzer_rep_i<Analyzer<Rep, Ts...>> {
+	using type = Rep;
+};
+
+template<class Analyzer>
+using analyzer_rep = typename analyzer_rep_i<std::remove_pointer_t<std::remove_reference_t<std::remove_cv_t<Analyzer>>>>::type;
 
 // short hands
 template <bool B>
