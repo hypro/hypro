a164d92871e38a7f398df6b7b466f03f1107a154
Leander Behr <l.behr19199@gmail.com>
2020-07-22 18:27:36 +0200

    Moved into hypro namespace and rootLocation to path.

--- src/hypro/datastructures/HybridAutomaton/Pathv2.h
+++ src/hypro/datastructures/HybridAutomaton/Pathv2.h
@@ -7,9 +7,12 @@ namespace hypro {
 
 template <class Number>
 struct Path {
+	Location<Number> const* rootLocation{};
+	std::vector<std::pair<carl::Interval<SegmentInd>, Transition<Number> const*> > elements{};	///< Path in pairs of time and discrete steps. Intervals holds the information when the transition was taken.
+
 	Path() = default;
-	Path(size_t numElements) : elements(numElements) {}
-	std::vector<std::pair<carl::Interval<SegmentInd>, Transition<Number> const* > > elements{};  ///< Path in pairs of time and discrete steps. Intervals holds the information when the transition was taken.
+	Path( size_t numElements )
+		: elements( numElements ) {}
 };
 
-}
+}  // namespace hypro
