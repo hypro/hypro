1777452d001af6d5ef6ab84aad10534ad717b73e
Leander Behr <l.behr19199@gmail.com>
2020-07-22 18:28:26 +0200

    Added return types for analysers to more conveniently handle different types of data being return on success and failure.

---
+++ src/hypro/algorithms/reachability/analyzer/ReturnTypes.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <types.h>
+#include <variant>
+
+namespace hypro {
+
+template <class Success, class Failure>
+struct AnalysisResult {
+	std::variant<Success, Failure> variant;
+
+	REACHABILITY_RESULT result() {
+		return std::holds_alternative<Success>( variant ) ? REACHABILITY_RESULT::SAFE : REACHABILITY_RESULT::UNKNOWN;
+	}
+
+	bool isSuccess() { return std::holds_alternative<Success>( variant ); }
+	bool isFailure() { return !success(); }
+
+	Success& success() { return std::get<Success>( variant ); }
+	Failure& failure() { return std::get<Failure>( variant ); }
+
+	operator std::variant<Success, Failure> &() {
+		return variant;
+	}
+};
+
+/**
+ * @brief Contains the results of a failed analysis
+ * @tparam Representation The representation type that the analysis used
+ */
+template <class Representation>
+struct Failure {
+	ReachTreeNode<Representation>* conflictNode{};	///< The node where a conflict occured, i.e. a bad state was intersected.
+};
+
+template<class Representation>
+Failure(ReachTreeNode<Representation>*) -> Failure<Representation>;
+
+}  // namespace hypro
