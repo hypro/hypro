259eff2aec989f6b3369212c94ed5af05a8a3bd4
tristan.ebert <tristan.ebert@googlemail.com>
2020-08-03 15:13:34 +0200

    Worked on soplex adaptions

--- src/hypro/util/linearOptimization/soplex/adaptions_soplex.h
+++ src/hypro/util/linearOptimization/soplex/adaptions_soplex.h
@@ -1,17 +1,21 @@
 
 #pragma once
 #include <soplex.h>
+#include <carl/core/Relation.h>
 
 namespace hypro {
 
 template <typename Number>
-EvaluationResult<Number> soplexOptimizeLinear( const vector_t<Number>& _direction, const matrix_t<Number>& constraints, const vector_t<Number>& constants, const EvaluationResult<Number>& preSolution = EvaluationResult<Number>() );
+soplex::SoPlex initializeSolver( const matrix_t<Number>& constraints, const vector_t<Number>& constants, const std::vector<carl::Relation>& relations );
 
 template <typename Number>
-bool soplexCheckConsistency( const matrix_t<Number>& constraints, const vector_t<Number>& constants );
+EvaluationResult<Number> soplexOptimizeLinear( const vector_t<Number>& _direction, const matrix_t<Number>& constraints, const vector_t<Number>& constants, const std::vector<carl::Relation>& relations, bool maximize );
 
 template <typename Number>
-bool soplexCheckPoint( const matrix_t<Number>& constraints, const vector_t<Number>& constants, const Point<Number>& point );
+bool soplexCheckConsistency( const matrix_t<Number>& constraints, const vector_t<Number>& constants, const std::vector<carl::Relation>& relations );
+
+template <typename Number>
+bool soplexCheckPoint( const matrix_t<Number>& constraints, const vector_t<Number>& constants, const std::vector<carl::Relation>& relations, const Point<Number>& point );
 
 }  // namespace hypro
 
