748db8f596bee6298e2cae6c3bb204259c922794
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2020-09-04 12:04:58 +0200

    add test for tree and counting nodes function

---
+++ src/hypro/datastructures/reachability/TreeUtil.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace hypro {
+template <typename R>
+std::size_t getNumberNodes( R& root ) {
+	TreeIterator<TreeNode<R>> cur{ root };
+	TreeIterator<TreeNode<R>> end{};
+	std::size_t count = 0;
+	while ( cur != end ) {
+		++cur;
+		++count;
+	}
+	return count;
+}
+}  // namespace hypro
