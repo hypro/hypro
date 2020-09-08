33cdcc2f2e2536df5b339f2c13f14540234d4dee
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2020-09-08 09:22:49 +0200

    fix in getNumberNodes

--- src/hypro/datastructures/reachability/TreeUtil.h
+++ src/hypro/datastructures/reachability/TreeUtil.h
@@ -4,8 +4,8 @@
 namespace hypro {
 template <typename R>
 std::size_t getNumberNodes( R& root ) {
-	TreeIterator<TreeNode<R>> cur{ root };
-	TreeIterator<TreeNode<R>> end{};
+	TreeIterator<R> cur{ root };
+	TreeIterator<R> end{};
 	std::size_t count = 0;
 	while ( cur != end ) {
 		++cur;
