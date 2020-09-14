850baf407cb3382763c33c3e0fe5cca76dc00d10
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2020-09-14 14:35:47 +0200

    fix assertion

--- src/hypro/algorithms/reachability/FlowpipeConstructionConvenience.h
+++ src/hypro/algorithms/reachability/FlowpipeConstructionConvenience.h
@@ -9,10 +9,10 @@ namespace hypro {
 
 template <class Representation, class Number>
 std::pair<CONTAINMENT, Representation> intersect( Representation const& valuationSet, Condition<Number> const& condition ) {
-	assert( valuationSet.dimension() == condition.dimension() );
 	if ( condition.empty() ) {
 		return std::make_pair( CONTAINMENT::FULL, valuationSet );
 	}
+	assert( valuationSet.dimension() == condition.dimension() );
 	return valuationSet.satisfiesHalfspaces( condition.getMatrix(), condition.getVector() );
 }
 
