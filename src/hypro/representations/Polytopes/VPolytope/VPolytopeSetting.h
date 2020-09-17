f8aca9091519c12adc499316c6844a787e3309f1
tristan.ebert <tristan.ebert@googlemail.com>
2020-09-17 15:44:41 +0200

    Added VPolytopeT to DefaultRepresentations list in dispatch

--- src/hypro/representations/Polytopes/VPolytope/VPolytopeSetting.h
+++ src/hypro/representations/Polytopes/VPolytope/VPolytopeSetting.h
@@ -2,7 +2,11 @@
 
 namespace hypro {
 
+BETTER_ENUM( vPolytopeSetting_name, int,
+             VPolytopeSetting )
+
 struct VPolytopeSetting {
+	static constexpr int type_enum = vPolytopeSetting_name::VPolytopeSetting;
 	static constexpr bool checkVerticesBeforeConversion = true;
 	static constexpr bool useLpForPointContainment = true;
 };
