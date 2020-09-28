2d554e40ef96fa1fcc92de5a7cca05fefe2154a7
tristan.ebert <tristan.ebert@googlemail.com>
2020-09-28 14:10:28 +0200

    Worked on HPoly construction benchmark

--- src/test/benchmark/hyproBenchmark/polytopes/hPolyConstruction.cpp
+++ src/test/benchmark/hyproBenchmark/polytopes/hPolyConstruction.cpp
@@ -13,22 +13,21 @@ Results<std::size_t> hPolyConstruction( const Settings& settings ) {
     // iterate over dimensions
     for ( std::size_t d = 1; d <= settings.maxDimension; ++d ) {
         Timer dimensionTimer;
-        for( std::size_t vertexCount = 1; vertexCount <= settings.iterations; ++vertexCount ) {
-            // create instances
-            std::vector<hypro::Point<::benchmark::Number>> samplePoints;
-            for ( std::size_t pointCount = 0; pointCount < vertexCount; ++pointCount ) {
-                hypro::vector_t<::benchmark::Number> coordinates = hypro::vector_t<::benchmark::Number>( d );
-                for ( std::size_t row = 0; row < d; ++row ) {
-                    coordinates( row ) = dist( generator );
-                }
-                samplePoints.emplace_back( hypro::Point( coordinates ) );
+        std::size_t vertexCount = settings.iterations;
+        // create instances
+        std::vector<hypro::Point<::benchmark::Number>> samplePoints;
+        for ( std::size_t pointCount = 0; pointCount < vertexCount; ++pointCount ) {
+            hypro::vector_t<::benchmark::Number> coordinates = hypro::vector_t<::benchmark::Number>( d );
+            for ( std::size_t row = 0; row < d; ++row ) {
+                coordinates( row ) = dist( generator );
             }
-            Timer constructionTimer;
-            hypro::HPolytope<::benchmark::Number> hpoly( samplePoints );
-            auto runningTime = constructionTimer.elapsed();
-            std::cout << "Construction of " << d << "-dimensional HPolytope with " << vertexCount << " vertices " << "took " << runningTime.count() << " sec\n";
-            ress.mRunningTime += runningTime;
+            samplePoints.emplace_back( hypro::Point( coordinates ) );
         }
+        Timer constructionTimer;
+        hypro::HPolytope<::benchmark::Number> hpoly( samplePoints );
+        auto runningTime = constructionTimer.elapsed();
+        std::cout << "Construction of " << d << "-dimensional HPolytope took " << runningTime.count() << " sec\n";
+        ress.mRunningTime += runningTime;
         ress.emplace_back( { "hPolyVertexConstruction", dimensionTimer.elapsed(), static_cast<int>( d ) } );
     }
     return ress;
