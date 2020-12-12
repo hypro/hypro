5cbe5ca4fd29e749e53e70774a8a76086e17564c
tristan.ebert <tristan.ebert@googlemail.com>
2020-12-12 17:07:01 +0100

    Added test with jumps for decompositional analyzer

--- src/test/algorithms/DecompositionalReachabilityTest.cpp
+++ src/test/algorithms/DecompositionalReachabilityTest.cpp
@@ -46,12 +46,161 @@ HybridAutomaton<Number> makeHa() {
     return ha;
 }
 
+template <typename Number>
+hypro::HybridAutomaton<Number> makeHaJumps() {
+    // Three-dimensional HA with two locations
+    using Matrix = hypro::matrix_t<Number>;
+    using Vector = hypro::vector_t<Number>;
+
+    hypro::HybridAutomaton<Number> res;
+
+    // Create locations
+    hypro::Location<Number> loc0{};
+    hypro::Location<Number> loc1{};
+    auto uniqueLoc0{ std::make_unique<hypro::Location<Number>>( loc0 ) };
+    auto uniqueLoc1{ std::make_unique<hypro::Location<Number>>( loc1 ) };
+    uniqueLoc0->setName( "l0" );
+    uniqueLoc1->setName( "l1" );
+
+    // Set flow x' = y, y' = -x, z' = 2 in loc0
+    Matrix flow0 = Matrix::Zero( 4, 4 );
+    flow0( 0, 1 ) = 1;
+    flow0( 1, 0 ) = -1;
+    flow0( 2, 3 ) = 2;
+    uniqueLoc0->setFlow( flow0 );
+
+    // Set flow x' = 1, y' = x + 2, z' = -1 in loc1
+    Matrix flow1 = Matrix::Zero( 3, 3 );
+    flow1( 0, 2 ) = 1;
+    flow1( 1, 1 ) = 1;
+    flow1( 1, 2 ) = 2;
+    flow1( 2, 3 ) = -1;
+    uniqueLoc1->setFlow( flow1 );
+
+    // Set invariant x <= 3 in loc0 and loc1
+    Matrix invariantConstraints = Matrix::Zero( 1, 2 );
+    invariantConstraints( 0, 0 ) = 1;
+    Vector invariantConstants = 3 * Vector::Ones( 1 );
+    uniqueLoc0->setInvariant( { invariantConstraints, invariantConstants } );
+    uniqueLoc1->setInvariant( { invariantConstraints, invariantConstants } );
+
+    // Construct transitions
+    // l0 -> l0 with guard x >= 1 and reset x := 0, y := 0
+    Matrix transConstraint = Matrix::Zero( 1, 2 );
+    Vector transConstants = -1 * Vector::Ones( 1 );
+    transConstraint( 0, 0 ) = -1;
+    hypro::Condition<Number> guard( transConstraint, transConstants );
+    hypro::Reset<Number> reset{ { { 0, 0 }, { 0, 0 } } };
+
+    std::unique_ptr<hypro::Transition<Number>> trans0 =
+          std::make_unique<hypro::Transition<Number>>( uniqueLoc0.get(), uniqueLoc0.get(), guard, reset );
+
+    // l0 -> l1 with guard y <= -2 and no reset
+    transConstraint = Matrix::Zero( 1, 2 );
+    transConstants = -2 * Vector::Ones( 1 );
+    transConstraint( 0, 1 ) = 1;
+    guard = hypro::Condition<Number>( transConstraint, transConstants );
+    reset = hypro::Reset<Number>( { carl::Interval<Number>(), carl::Interval<Number>() } );
+
+    std::unique_ptr<hypro::Transition<Number>> trans1 =
+          std::make_unique<hypro::Transition<Number>>( uniqueLoc0.get(), uniqueLoc1.get(), guard, reset );
+
+    // Set initial state x = 0, y = 0, aff = 1
+    Matrix initialConstraints = Matrix::Zero( 4, 2 );
+    Vector initialConstants = Vector::Zero( 4 );
+    initialConstraints << 1, 0, -1, 0, 0, 1, 0, -1;
+    initialConstants << 0, 0, 0, 0;
+
+    // Create HA
+    uniqueLoc0->addTransition( std::move( trans0 ) );
+    uniqueLoc0->addTransition( std::move( trans1 ) );
+
+    res.addInitialState( uniqueLoc0.get(), hypro::Condition<Number>( initialConstraints, initialConstants ) );
+
+    res.addLocation( std::move( uniqueLoc0 ) );
+    res.addLocation( std::move( uniqueLoc1 ) );
+
+    return res;
+}
+
+
+template <typename Number>
+hypro::HybridAutomaton<Number> singularHa() {
+    // Two-dimensional HA with two locations
+    using Matrix = hypro::matrix_t<Number>;
+    using Vector = hypro::vector_t<Number>;
+
+    hypro::HybridAutomaton<Number> res;
+
+    // Create locations
+    hypro::Location<Number> loc0{};
+    hypro::Location<Number> loc1{};
+    auto uniqueLoc0{ std::make_unique<hypro::Location<Number>>( loc0 ) };
+    auto uniqueLoc1{ std::make_unique<hypro::Location<Number>>( loc1 ) };
+    uniqueLoc0->setName( "l0" );
+    uniqueLoc1->setName( "l1" );
+
+    // Set flow x' = 1, y' = -1 in loc0
+    Matrix flow0 = Matrix::Zero( 3, 3 );
+    flow0( 0, 2 ) = 1;
+    flow0( 1, 2 ) = -1;
+    uniqueLoc0->setFlow( flow0 );
+
+    // Set flow x' = 0, y' = 1 in loc1
+    Matrix flow1 = Matrix::Zero( 3, 3 );
+    flow1( 1, 2 ) = 1;
+    uniqueLoc1->setFlow( flow1 );
+
+    // Set invariant x <= 3 in loc0 and loc1
+    Matrix invariantConstraints = Matrix::Zero( 1, 2 );
+    invariantConstraints( 0, 0 ) = 1;
+    Vector invariantConstants = 3 * Vector::Ones( 1 );
+    uniqueLoc0->setInvariant( { invariantConstraints, invariantConstants } );
+    uniqueLoc1->setInvariant( { invariantConstraints, invariantConstants } );
+
+    // Construct transitions
+    // l0 -> l0 with guard x >= 1 and reset x := 0, y := 0
+    Matrix transConstraint = Matrix::Zero( 1, 2 );
+    Vector transConstants = -1 * Vector::Ones( 1 );
+    transConstraint( 0, 0 ) = -1;
+    hypro::Condition<Number> guard( transConstraint, transConstants );
+    hypro::Reset<Number> reset{ { { 0, 0 }, { 0, 0 } } };
+
+    std::unique_ptr<hypro::Transition<Number>> trans0 =
+          std::make_unique<hypro::Transition<Number>>( uniqueLoc0.get(), uniqueLoc0.get(), guard, reset );
+
+    // l0 -> l1 with guard y <= -2 and no reset
+    transConstraint = Matrix::Zero( 1, 2 );
+    transConstants = -2 * Vector::Ones( 1 );
+    transConstraint( 0, 1 ) = 1;
+    guard = hypro::Condition<Number>( transConstraint, transConstants );
+    reset = hypro::Reset<Number>( { carl::Interval<Number>(), carl::Interval<Number>() } );
+
+    std::unique_ptr<hypro::Transition<Number>> trans1 =
+          std::make_unique<hypro::Transition<Number>>( uniqueLoc0.get(), uniqueLoc1.get(), guard, reset );
+
+    // Set initial state x = 0, y = 0
+    Matrix initialConstraints = Matrix::Zero( 4, 2 );
+    Vector initialConstants = Vector::Zero( 4 );
+    initialConstraints << 1, 0, -1, 0, 0, 1, 0, -1;
+    initialConstants << 0, 0, 0, 0;
+
+    // Create HA
+    uniqueLoc0->addTransition( std::move( trans0 ) );
+    uniqueLoc0->addTransition( std::move( trans1 ) );
+
+    res.addInitialState( uniqueLoc0.get(), hypro::Condition<Number>( initialConstraints, initialConstants ) );
+
+    res.addLocation( std::move( uniqueLoc0 ) );
+    res.addLocation( std::move( uniqueLoc1 ) );
+
+    return res;
+}
+
 
 
 TEST( DecompositionalAnalysis, NoBadStatesNoJumps ) {
     using Number = double;
-    using Matrix = matrix_t<Number>;
-    using Vector = vector_t<Number>;
     using Representation = VPolytope<Number>;
 
     auto ha = makeHa<Number>();
@@ -117,4 +266,20 @@ TEST( DecompositionalAnalysis, UnsafeNoJumps ) {
     auto result = analyzer.run();
     EXPECT_EQ( hypro::REACHABILITY_RESULT::UNKNOWN, result.result() );
 
+}
+
+TEST( DecompositionalAnalysis, SingularJumps ) {
+    using Number = mpq_class;
+    using Representation = VPolytope<Number>;
+
+    auto ha = singularHa<Number>();
+    auto [decomposedHa, decomposition] = decomposeAutomaton( ha );
+    for ( std::size_t subspace = 0; subspace < decomposition.subspaceTypes.size(); ++subspace ) {
+        if ( decomposition.subspaceTypes[ subspace ] != DynamicType::linear && decomposition.subspaceTypes[ subspace ] != DynamicType::affine ) {
+            addClockToAutomaton( decomposedHa, subspace );
+        }
+    }
+    std::vector<std::vector<ReachTreeNode<Representation>>> roots = makeDecompositionalRoots<Representation, Number>( decomposedHa, decomposition );
+    DecompositionalAnalyzer<Representation> analyzer( decomposedHa, decomposition, FixedAnalysisParameters{1, 50, 0.1}, AnalysisParameters{ 0.1 }, roots );
+    analyzer.run();
 }
