b0e835d07acc4629d1fe352e10bbcbca780cc33f
tristan.ebert <tristan.ebert@googlemail.com>
2021-05-18 14:07:41 +0200

    slicing for analysis paths

--- src/hypro/datastructures/HybridAutomaton/Pathv2.h
+++ src/hypro/datastructures/HybridAutomaton/Pathv2.h
@@ -13,6 +13,17 @@ struct Path {
 	Path() = default;
 	Path( size_t numElements )
 		: elements( numElements ) {}
+
+    std::pair<Path<Number>, Path<Number>> split( std::size_t n ) {
+        assert( n < elements.size() );
+        auto pathPrefix = std::vector<std::pair<carl::Interval<SegmentInd>, Transition<Number> const*>>(
+            elements.begin(), elements.begin() + n - 1 );
+        auto pathPostfix = std::vector<std::pair<carl::Interval<SegmentInd>, Transition<Number> const*>>(
+            elements.begin() + n, elements.end() );
+        return std::make_pair(
+            Path{ rootLocation, pathPrefix },
+            Path{ elements.at( n - 1 ).second->getTarget(), pathPostfix } );
+    }
 };
 
 }  // namespace hypro
