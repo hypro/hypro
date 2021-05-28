9804b399e22977a6ace731e491b836f30ec46534
Leander Behr <l.behr19199@gmail.com>
2021-05-28 20:23:35 +0200

    Split cegar analyzer source file so it can be built in parallel. Saves ~10 seconds with -j 14 and only Box enabled in dispatcher.

--- src/hypro/algorithms/reachability/analyzer/CEGARAnalyzer_mpq.cpp
+++ src/hypro/algorithms/reachability/analyzer/CEGARAnalyzer_mpq.cpp
@@ -1,6 +1,5 @@
 #include "CEGARAnalyzer.tpp"
 using namespace hypro;
 
-template class detail::CEGARAnalyzer_impl<concat<TypeList<double>, RepresentationsList<double, Converter<double>>>>;
 template class detail::CEGARAnalyzer_impl<concat<TypeList<mpq_class>, RepresentationsList<mpq_class, Converter<mpq_class>>>>;
 
