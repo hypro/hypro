c6c78d8f5ad5f8c74b25d5f67cb008ed7ef0ceda
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2021-06-10 12:46:19 +0200

    temporarily remove explicit instantiation of the cegar-analyzer in hypro

--- src/hypro/algorithms/reachability/analyzer/CEGARAnalyzer_mpq.cpp
+++ src/hypro/algorithms/reachability/analyzer/CEGARAnalyzer_mpq.cpp
@@ -1,5 +1,4 @@
 #include "CEGARAnalyzer.tpp"
 using namespace hypro;
 
-template class detail::CEGARAnalyzer_impl<concat<TypeList<mpq_class>, RepresentationsList<mpq_class, Converter<mpq_class>>>>;
-
+//template class detail::CEGARAnalyzer_impl<concat<TypeList<mpq_class>, RepresentationsList<mpq_class, Converter<mpq_class>>>>;
