5373e63b0f53fd4a4f6ad4bd1762bb16d89cebe4
Stefan Schupp <stefan.schupp@tuwien.ac.at>
2021-08-02 17:39:00 +0200

    add google-benchmark for case studies
    * cmake setup
    * add bouncing ball as a first benchmark

--- src/hypro/representations/Box/Box.h
+++ src/hypro/representations/Box/Box.h
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2021-2021.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 /**
  * @file Box.h
  * \example example_box.cpp
@@ -104,9 +113,9 @@ class BoxT : private GeometricObjectBase {
 	BoxT( BoxT&& orig ) = default;
 
 	/**
-	  * @brief      Box constructor from one interval, results in a one-dimensional box.
-	  * @param[in]  val   An interval.
-	  */
+	 * @brief      Box constructor from one interval, results in a one-dimensional box.
+	 * @param[in]  val   An interval.
+	 */
 	explicit BoxT( const carl::Interval<Number>& val ) {
 		mLimits.push_back( val );
 		if ( val.isEmpty() )
@@ -188,9 +197,9 @@ class BoxT : private GeometricObjectBase {
 	Setting getSettings() const { return Setting{}; }
 
 	/**
-	  * @brief Static method for the construction of an empty box of required dimension.
-	  * @return Empty box.
-	  */
+	 * @brief Static method for the construction of an empty box of required dimension.
+	 * @return Empty box.
+	 */
 	static BoxT Empty() {
 		auto tmp = BoxT();
 		assert( tmp.empty() );
@@ -443,9 +452,9 @@ class BoxT : private GeometricObjectBase {
 	 **************************************************************************/
 
 	/**
-	  * @brief      Getter for the state space dimension.
-	  * @return     The dimension of the space.
-	  */
+	 * @brief      Getter for the state space dimension.
+	 * @return     The dimension of the space.
+	 */
 	std::size_t dimension() const { return mLimits.size(); }
 
 	/**
