ef26c392e220d92623b389013e68bd534571f983
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2021-10-25 16:47:34 +0200

    Work on Hyperoctree data-structure
    * fix insertion of sets
    * fix update of coverage
    * add tests
    * extend simplex-watertanks case study + plotting of the result

--- include/hypro/algorithms/reachability/FlowpipeConstructionConvenience.h
+++ include/hypro/algorithms/reachability/FlowpipeConstructionConvenience.h
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2021.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #pragma once
 
 #include "../../datastructures/HybridAutomaton/Condition.h"
@@ -92,6 +101,24 @@ Representation applyReset( Representation const& valuationSet, Reset<Number> con
 template <class Representation>
 Box<typename Representation::NumberType> computeBoundingBox( const Representation& set ) {
 	return hypro::Converter<typename Representation::NumberType>::toBox( set );
+}
+
+template <class Representation>
+Box<typename Representation::NumberType> computeBoundingBox( const ReachTreeNode<Representation>& node ) {
+	auto box = computeBoundingBox( node.getFlowpipe().front() );
+	for ( const auto& s : getSegments( node ) ) {
+		box = box.unite( computeBoundingBox( s ) );
+	}
+	return box;
+}
+
+template <class Representation>
+Box<typename Representation::NumberType> computeBoundingBox( const std::vector<ReachTreeNode<Representation>>& trees ) {
+	auto box = computeBoundingBox( trees.front() );
+	for ( auto treeIt = std::next( std::begin( trees ) ); treeIt != std::end( trees ); ++treeIt ) {
+		box = box.unite( computeBoundingBox( *treeIt ) );
+	}
+	return box;
 }
 
 }  // namespace hypro
--- include/hypro/datastructures/reachability/ReachTreev2Util.h
+++ include/hypro/datastructures/reachability/ReachTreev2Util.h
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2021.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #pragma once
 
 #include "ReachTreev2.h"
@@ -15,7 +24,17 @@ struct UnpackRepresentation<ReachTreeNode<R>&> {
 };
 
 template <typename R>
+struct UnpackRepresentation<const ReachTreeNode<R>&> {
+	using Representation = R;
+};
+
+template <typename R>
 struct UnpackRepresentation<std::vector<ReachTreeNode<R>>&> {
+	using Representation = R;
+};
+
+template <typename R>
+struct UnpackRepresentation<std::vector<const ReachTreeNode<R>>&> {
 	using Representation = R;
 };
 
@@ -32,6 +51,13 @@ template <typename RootType>
 auto getFlowpipes( RootType&& root ) -> std::vector<std::vector<typename detail::UnpackRepresentation<RootType>::Representation>> {
 	std::vector<std::vector<typename detail::UnpackRepresentation<RootType>::Representation>> res;
 	visitPreorder( std::forward<RootType>( root ), [&res]( auto& node ) { res.insert( res.end(), node.getFlowpipe() ); } );
+	return res;
+}
+
+template <typename RootType>
+auto getSegments( RootType&& root ) -> std::vector<typename detail::UnpackRepresentation<RootType>::Representation> {
+	std::vector<typename detail::UnpackRepresentation<RootType>::Representation> res;
+	visitPreorder( std::forward<RootType>( root ), [&res]( auto& node ) { res.insert( res.end(), std::begin( node.getFlowpipe() ), std::end( node.getFlowpipe() ) ); } );
 	return res;
 }
 
