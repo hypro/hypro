ea2bed7bf0549ce344c283aa7a2eae59d638ae1c
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2021-10-25 22:15:51 +0200

    update case study
    * make hyperoctree initial box integer to increase stability
    * create benchmark setup to test efficiency against classic containment test

--- test/case_studies/cs_simplex_architecture_watertanks_benchmark.cpp
+++ test/case_studies/cs_simplex_architecture_watertanks_benchmark.cpp
@@ -19,6 +19,7 @@
 #include <hypro/parser/antlr4-flowstar/ParserWrapper.h>
 #include <hypro/paths.h>
 #include <hypro/util/plotting/Plotter.h>
+#include <random>
 #include <string>
 
 namespace hypro::benchmark {
@@ -37,6 +38,96 @@ void plotRecursive( const hypro::Hyperoctree<double>& octree, hypro::Plotter<dou
 						   hypro::plotting::colors[hypro::plotting::red] );
 		}
 	}
+}
+
+class HyperOctreeFixture : public ::benchmark::Fixture {
+	using Number = double;
+
+  public:
+	void SetUp( const ::benchmark::State& state ) {
+		std::string filename{ "21_simplex_watertanks_deterministic_monitor_small_init.model" };
+
+		auto [automaton, reachSettings] = hypro::parseFlowstarFile<Number>( getCSModelsPath() + filename );
+
+		auto settings = hypro::convert( reachSettings );
+		settings.rStrategy().front().detectJumpFixedPoints = true;
+		settings.rStrategy().front().detectFixedPointsByCoverage = false;
+		settings.rStrategy().front().detectContinuousFixedPointsLocally = false;
+		settings.rFixedParameters().localTimeHorizon = 100;
+		settings.rFixedParameters().jumpDepth = 15;
+		settings.rStrategy().begin()->aggregation = AGG_SETTING::AGG;
+		std::vector<hypro::Path<Number>> last_paths{};
+
+		roots = hypro::makeRoots<hypro::Box<Number>>( automaton );
+		auto reacher = hypro::reachability::Reach<hypro::Box<Number>>( automaton, settings.fixedParameters(),
+																	   settings.strategy().front(), roots );
+		auto result = reacher.computeForwardReachability();
+	}
+
+	void TearDown( const ::benchmark::State& state ) {}
+
+	std::vector<hypro::ReachTreeNode<hypro::Box<Number>>> roots;
+	std::mt19937 generator;
+	std::uniform_real_distribution<double> dist = std::uniform_real_distribution<double>( 0, 1 );
+};
+
+BENCHMARK_F( HyperOctreeFixture, HyperOctree )( ::benchmark::State& st ) {
+	// create hyperoctree
+	auto bbox = computeBoundingBox( roots ).projectOn( { 0, 1 } );
+	// widen dimensions until they are integer to increase stability
+	for ( auto& interval : bbox.rIntervals() ) {
+		interval = carl::Interval<double>( std::floor( interval.lower() ), std::ceil( interval.upper() ) );
+	}
+
+	hypro::Hyperoctree<double> octree{ 2, 8, bbox };
+	// add to hyperoctree
+	for ( const auto& r : roots ) {
+		auto segments = getSegments( r );
+		for ( const auto& s : segments ) {
+			octree.add( s.projectOn( { 0, 1 } ) );
+		}
+	}
+
+	std::size_t count = 0;
+	for ( auto _ : st ) {
+		// create point
+		hypro::vector_t<double> coordinates = hypro::vector_t<double>( 2 );
+		for ( std::size_t d = 0; d < 2; ++d ) {
+			coordinates( d ) = dist( generator );
+		}
+		hypro::Point<double> point{ coordinates };
+		auto res = octree.contains( point );
+		if ( res ) {
+			++count;
+		}
+	}
+	st.counters["contained"] = count;
+}
+
+BENCHMARK_F( HyperOctreeFixture, ClassicLookup )( ::benchmark::State& st ) {
+	std::vector<hypro::Box<double>> segments;
+	for ( const auto& r : roots ) {
+		auto tmp = getSegments( r );
+		for ( auto& segment : tmp ) {
+			segments.push_back( segment.projectOn( { 0, 1 } ) );
+		}
+	}
+
+	std::size_t count = 0;
+	for ( auto _ : st ) {
+		// create point
+		hypro::vector_t<double> coordinates = hypro::vector_t<double>( 2 );
+		for ( std::size_t d = 0; d < 2; ++d ) {
+			coordinates( d ) = dist( generator );
+		}
+		hypro::Point<double> point{ coordinates };
+		auto res = std::any_of( std::begin( segments ), std::end( segments ),
+								[&point]( const auto& seg ) { return seg.contains( point ); } );
+		if ( res ) {
+			++count;
+		}
+	}
+	st.counters["contained"] = count;
 }
 
 template <class Representation>
@@ -124,8 +215,15 @@ static void Simplex_Watertanks_Reachability( ::benchmark::State& state ) {
 	plt.rSettings().cummulative = false;
 	plt.setFilename( "simplex_watertanks" );
 
+	// create hyperoctree
+	auto bbox = computeBoundingBox( roots ).projectOn( { 0, 1 } );
+	// widen dimensions until they are integer to increase stability
+	for ( auto& interval : bbox.rIntervals() ) {
+		interval = carl::Interval<double>( std::floor( interval.lower() ), std::ceil( interval.upper() ) );
+	}
+
+	hypro::Hyperoctree<Number> octree{ 2, 8, bbox };
 	// add to hyperoctree
-	hypro::Hyperoctree<Number> octree{ 2, 5, computeBoundingBox( roots ).projectOn( { 0, 1 } ) };
 	for ( const auto& r : roots ) {
 		auto segments = getSegments( r );
 		for ( const auto& s : segments ) {
