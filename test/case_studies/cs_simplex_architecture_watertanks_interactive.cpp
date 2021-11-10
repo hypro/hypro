0b388b6189b99cab13653813806ad26582965f6d
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2021-11-10 14:56:19 +0100

    add debug model file

--- test/case_studies/cs_simplex_architecture_watertanks_interactive.cpp
+++ test/case_studies/cs_simplex_architecture_watertanks_interactive.cpp
@@ -1,4 +1,13 @@
 /*
+ * Copyright (c) 2021.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/*
  * Created by stefan on 04.08.21.
  */
 
@@ -13,7 +22,7 @@ void Simplex_Watertanks_Reachability( std::size_t maxJumps = 5 ) {
 	using Number = double;
 	using Representation = hypro::Box<Number>;
 
-	std::string filename{ "21_simplex_watertanks_deterministic_monitor_small_init.model" };
+	std::string filename{ "21_simplex_watertanks_deterministic_monitor_dbg_init_ticks.model" };
 
 	auto [automaton, reachSettings] = hypro::parseFlowstarFile<Number>( hypro::getCSModelsPath() + filename );
 
@@ -38,6 +47,6 @@ void Simplex_Watertanks_Reachability( std::size_t maxJumps = 5 ) {
 }
 
 int main() {
-	Simplex_Watertanks_Reachability( 12 );
+	Simplex_Watertanks_Reachability( 20 );
 	return 0;
 }
