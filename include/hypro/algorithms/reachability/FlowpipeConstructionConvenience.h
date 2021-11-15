e27aaff768c17da5e53fade22073b8f0463049f3
tristan.ebert <tristan.ebert@googlemail.com>
2021-11-15 12:43:31 +0100

    handle empty resets

--- include/hypro/algorithms/reachability/FlowpipeConstructionConvenience.h
+++ include/hypro/algorithms/reachability/FlowpipeConstructionConvenience.h
@@ -75,11 +75,14 @@ Representation applyTimeEvolution( Representation const& valuationSet, matrix_t<
  */
 template <class Representation, class Number>
 Representation applyReset( Representation const& valuationSet, Reset<Number> const& reset, std::size_t subspace = 0 ) {
-    if ( std::any_of( reset.getIntervalReset( subspace ).getIntervals().begin(), reset.getIntervalReset( subspace ).getIntervals().end(),
+    if ( !reset.isIntervalIdentity() && std::any_of( reset.getIntervalReset( subspace ).getIntervals().begin(), reset.getIntervalReset( subspace ).getIntervals().end(),
                 []( const auto& interval ){ return !interval.isEmpty(); } ) ) {
         assert( false && "lti analyzer does not currently support interval assignments on reset" );
         WARN( "hypro.reachability", "lti analyzer does not currently support interval assignments on reset" );
     }
+	if ( reset.isAffineIdentity() ) {
+		return valuationSet;
+	}
     return valuationSet.affineTransformation( reset.getMatrix( subspace ), reset.getVector( subspace ) );
 }
 
