d6ef46fa7013f8eeb26cada04402d23a4101a2fa
Stefan Schupp <stefan.schupp@tuwien.ac.at>
2022-01-06 12:47:15 +0100

    cleanup output operators
    Signed-off-by: Stefan Schupp <stefan.schupp@tuwien.ac.at>

--- include/hypro/datastructures/HybridAutomaton/flow/linear.h
+++ include/hypro/datastructures/HybridAutomaton/flow/linear.h
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2022.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #pragma once
 #include "../../../types.h"
 #include "../../../util/convenienceOperators.h"
@@ -181,13 +190,18 @@ class linearFlow {
 		const matrix_t<Number>& fMat{ in.mFlowMatrix };
 		if ( fMat.rows() > 0 ) {
 			for ( Eigen::Index row = 0; row < fMat.rows(); ++row ) {
-				if ( !firstRow ) {
-					out << "\n";
-				} else {
-					firstRow = false;
-				}
-				out << "x" << row << "' = " << to_string<Number>( fMat.row( row ) );
-			}
+                if (!firstRow) {
+                    out << "\n";
+                } else {
+                    firstRow = false;
+                }
+                out << "x" << row << "' = ";
+                if (in.hasNoFlow(std::size_t(row))) {
+                    out << "0";
+                } else {
+                    out << to_string<Number>(fMat.row(row));
+                }
+            }
 		}
 		return out;
 	}
