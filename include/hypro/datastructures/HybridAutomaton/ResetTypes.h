95bc1b6760a781d315d8599010602fd9bee2c610
Stefan Schupp <stefan.schupp@tuwien.ac.at>
2022-01-06 12:47:15 +0100

    cleanup output operators
    Signed-off-by: Stefan Schupp <stefan.schupp@tuwien.ac.at>

--- include/hypro/datastructures/HybridAutomaton/ResetTypes.h
+++ include/hypro/datastructures/HybridAutomaton/ResetTypes.h
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 2021.
+ * Copyright (c) 2022.
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *
- *   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
  *
- *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 #pragma once
@@ -86,13 +86,22 @@ struct AffineTransformation {
 		}
 		bool firstrow = true;
 		for ( Eigen::Index row = 0; row < in.mTransformation.matrix().rows(); ++row ) {
-			if ( !firstrow ) {
-				out << "\n";
-			} else {
-				firstrow = false;
-			}
-			out << "x" << row << " := " << to_string<Number>( in.mTransformation.matrix().row( row ) );
-		}
+            // skip identity resets
+            if (in.isIdentity(std::size_t(row))) {
+                continue;
+            }
+            if (!firstrow) {
+                out << "\n";
+            } else {
+                firstrow = false;
+            }
+            out << "x" << row << " := ";
+            if (in.mTransformation.matrix().row(row) == matrix_t<Number>::Zero(1, in.mTransformation.matrix().cols())) {
+                out << "0";
+            } else {
+                out << to_string<Number>(in.mTransformation.matrix().row(row));
+            }
+        }
 		return out;
 	}
 
