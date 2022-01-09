9e1c896daa4e2e0e90a0612434c9167d006726f0
Stefan Schupp <stefan.schupp@tuwien.ac.at>
2022-01-09 18:48:42 +0100

    update model file and benchmark file of watertanks model
    Signed-off-by: Stefan Schupp <stefan.schupp@tuwien.ac.at>

--- test/case_studies/cs_simplex_architecture_watertanks_benchmark.cpp
+++ test/case_studies/cs_simplex_architecture_watertanks_benchmark.cpp
@@ -30,26 +30,38 @@ void plotRecursive( const hypro::Hyperoctree<double>& octree, hypro::Plotter<dou
 		plt.addObject( octree.getContainer().projectOn( { 0, 1 } ).vertices(),
 					   hypro::plotting::colors[hypro::plotting::green] );
 	} else {
-		if ( !octree.getChildren().empty() ) {
-			for ( const auto& child : octree.getChildren() ) {
-				plotRecursive( *child, plt );
-			}
-		} else {
-			plt.addObject( octree.getContainer().projectOn( { 0, 1 } ).vertices(),
-						   hypro::plotting::colors[hypro::plotting::red] );
-		}
-	}
+        if (!octree.getChildren().empty()) {
+            for (const auto &child: octree.getChildren()) {
+                plotRecursive(*child, plt);
+            }
+        } else {
+            plt.addObject(octree.getContainer().projectOn({0, 1}).vertices(),
+                          hypro::plotting::colors[hypro::plotting::red]);
+        }
+    }
 }
 
-template <class Representation>
-static void Simplex_Watertanks_Reachability( ::benchmark::State& state ) {
-	// store arguments
-	std::size_t maxJumps = state.range( 0 );
-	// clear statistics
-	RESET_STATS();
-	// Perform setup here
-	using Number = double;
-	std::string filename{ "21_simplex_watertanks_deterministic_monitor_dbg_init_ticks.model" };
+    template<typename R>
+    void addPathChronologously(const hypro::ReachTreeNode<R> *node, hypro::Plotter<typename R::NumberType> &plt) {
+        if (node != nullptr) {
+            // recursive call to keep order
+            addPathChronologously(node->getParent(), plt);
+            // add segments of the current node
+            for (const auto &seg: node->getFlowpipe()) {
+                plt.addObject(seg.projectOn({0, 1}).vertices());
+            }
+        }
+    }
+
+    template<class Representation>
+    static void Simplex_Watertanks_Reachability(::benchmark::State &state) {
+        // store arguments
+        std::size_t maxJumps = state.range(0);
+        // clear statistics
+        RESET_STATS();
+        // Perform setup here
+        using Number = double;
+        std::string filename{"21_simplex_watertanks_deterministic_monitor_dbg_init_ticks.model"};
 	auto& plt = hypro::Plotter<Number>::getInstance();
 
 	auto [automaton, reachSettings] = hypro::parseFlowstarFile<Number>( getCSModelsPath() + filename );
@@ -88,93 +100,106 @@ static void Simplex_Watertanks_Reachability( ::benchmark::State& state ) {
         auto result = reacher.computeForwardReachability();
         INFO("hypro.casestudies", "System is safe: " << result);
 
-		// statistics
-		auto finished_leaves = std::size_t( 0 );
-		auto unfinished_leaves = std::size_t( 0 );
-		auto leaves = std::size_t( 0 );
-		auto nodes = std::size_t( 0 );
-		auto cyclic_path_count = std::size_t( 0 );
-		auto segments = std::size_t( 0 );
-		std::optional<hypro::Path<Number>> shortest{ std::nullopt };
-		const hypro::ReachTreeNode<Representation>* shortest_node{ nullptr };
-		std::optional<hypro::Path<Number>> shortest_cycle{ std::nullopt };
-		const hypro::ReachTreeNode<Representation>* shortest_cycle_node{ nullptr };
+        // statistics
+        auto finished_leaves = std::size_t(0);
+        auto unfinished_leaves = std::size_t(0);
+        auto timelock_leaves = std::size_t(0);
+        auto leaves = std::size_t(0);
+        auto nodes = std::size_t(0);
+        auto cyclic_path_count = std::size_t(0);
+        auto segments = std::size_t(0);
+        std::optional<hypro::Path<Number>> shortest{std::nullopt};
+        const hypro::ReachTreeNode<Representation> *shortest_node{nullptr};
+        std::optional<hypro::Path<Number>> shortest_timelock{std::nullopt};
+        const hypro::ReachTreeNode<Representation> *shortest_timelock_node{nullptr};
+        std::optional<hypro::Path<Number>> shortest_cycle{std::nullopt};
+        const hypro::ReachTreeNode<Representation> *shortest_cycle_node{nullptr};
 
-		for ( const ReachTreeNode<Representation>& node : hypro::preorder( roots ) ) {
-			++nodes;
-			segments += node.getFlowpipe().size();
-			if ( node.getChildren().empty() ) {
-				if ( node.hasFixedPoint() == TRIBOOL::TRUE ) {
-					++finished_leaves;
-				} else {
-					++unfinished_leaves;
-					last_paths.push_back( std::make_pair( node.getPath(), &node ) );
-					if ( has_discrete_cycle( node.getPath() ) ) {
-						INFO( "hypro.casestudies", "Path " << node.getPath() << " is cyclic." );
-						++cyclic_path_count;
-						if ( !shortest_cycle ||
-							 node.getPath().elements.size() < shortest_cycle.value().elements.size() ) {
-							shortest_cycle = node.getPath();
-							shortest_cycle_node = &node;
-						}
-					}
-#ifdef HYPRO_LOGGING
-					else {
-						INFO( "hypro.casestudies", "Path " << node.getPath() << " is truly unfinished." );
-						if ( !shortest || node.getPath().elements.size() < shortest.value().elements.size() ) {
-							shortest = node.getPath();
-							shortest_node = &node;
-						}
-					}
-#endif
-				}
-				++leaves;
-				COUNT( "leaves" );
-			}
-		}
-		// state.counters["fin.leaves"] = double( double( finished_leaves ) / double( leaves ) );
-		// state.counters["nfin.leaves"] = double( double( unfinished_leaves ) / double( leaves ) );
-		state.counters["#fin.leaves"] = ::benchmark::Counter( finished_leaves, ::benchmark::Counter::kAvgIterations );
-		state.counters["#nfin.leaves"] =
-			  ::benchmark::Counter( unfinished_leaves, ::benchmark::Counter::kAvgIterations );
-		state.counters["leaves"] = ::benchmark::Counter( leaves, ::benchmark::Counter::kAvgIterations );
-		state.counters["jumps"] = ::benchmark::Counter( maxJumps, ::benchmark::Counter::kAvgIterations );
-		state.counters["nodes"] = ::benchmark::Counter( nodes, ::benchmark::Counter::kAvgIterations );
-		state.counters["cycles"] = ::benchmark::Counter( cyclic_path_count, ::benchmark::Counter::kAvgIterations );
-		state.counters["segments"] = ::benchmark::Counter( segments, ::benchmark::Counter::kAvgIterations );
+        for (const ReachTreeNode<Representation> &node: hypro::preorder(roots)) {
+            ++nodes;
+            segments += node.getFlowpipe().size();
+            if (node.getChildren().empty()) {
+                if (node.hasFixedPoint() == TRIBOOL::TRUE) {
+                    ++finished_leaves;
+                } else if (node.hasTimelock()) {
+                    ++timelock_leaves;
+                    if (!shortest_timelock || node.getPath().elements.size() < shortest_timelock.value().elements.size()) {
+                        shortest_timelock = node.getPath();
+                        shortest_timelock_node = &node;
+                    }
+                } else {
+                    ++unfinished_leaves;
+                    last_paths.push_back(std::make_pair(node.getPath(), &node));
+                    if (has_discrete_cycle(node.getPath())) {
+                        INFO("hypro.casestudies", "Path " << node.getPath() << " is cyclic.");
+                        ++cyclic_path_count;
+                        if (!shortest_cycle ||
+                            node.getPath().elements.size() < shortest_cycle.value().elements.size()) {
+                            shortest_cycle = node.getPath();
+                            shortest_cycle_node = &node;
+                        }
+                    } else {
+                        INFO("hypro.casestudies", "Path " << node.getPath() << " is truly unfinished.");
+                        if (!shortest || node.getPath().elements.size() < shortest.value().elements.size()) {
+                            shortest = node.getPath();
+                            shortest_node = &node;
+                        }
+                    }
+                }
+                ++leaves;
+                COUNT("leaves");
+            }
+        }
+        // state.counters["fin.leaves"] = double( double( finished_leaves ) / double( leaves ) );
+        // state.counters["nfin.leaves"] = double( double( unfinished_leaves ) / double( leaves ) );
+        state.counters["#fin.leaves"] = ::benchmark::Counter(finished_leaves, ::benchmark::Counter::kAvgIterations);
+        state.counters["#nfin.leaves"] =
+                ::benchmark::Counter(unfinished_leaves, ::benchmark::Counter::kAvgIterations);
+        state.counters["#timelock.leaves"] =
+                ::benchmark::Counter(timelock_leaves, ::benchmark::Counter::kAvgIterations);
+        state.counters["leaves"] = ::benchmark::Counter(leaves, ::benchmark::Counter::kAvgIterations);
+        state.counters["jumps"] = ::benchmark::Counter(maxJumps, ::benchmark::Counter::kAvgIterations);
+        state.counters["nodes"] = ::benchmark::Counter(nodes, ::benchmark::Counter::kAvgIterations);
+        state.counters["cycles"] = ::benchmark::Counter(cyclic_path_count, ::benchmark::Counter::kAvgIterations);
+        state.counters["segments"] = ::benchmark::Counter(segments, ::benchmark::Counter::kAvgIterations);
 #ifdef HYPRO_STATISTICS
-		state.counters["fp-cov"] = hypro::Statistician::getInstance().getCounter( "FP-by-coverage" ).val;
+        state.counters["fp-cov"] = hypro::Statistician::getInstance().getCounter( "FP-by-coverage" ).val;
 #endif
-		if ( shortest ) {
-			std::cout << "Shortest non-cyclic, unfinished path: " << shortest.value() << std::endl;
-			plt.clear();
-			plt.rSettings().overwriteFiles = true;
-			plt.rSettings().cummulative = false;
-			plt.setFilename( "shortest_unfinished_path" );
-			while ( shortest_node != nullptr ) {
-				for ( const auto& seg : shortest_node->getFlowpipe() ) {
-					plt.addObject( seg.projectOn( { 0, 1 } ).vertices() );
-				}
-				shortest_node = shortest_node->getParent();
-			}
-			plt.plot2d( hypro::PLOTTYPE::pdf );
-		}
+        if (shortest) {
+            std::cout << "Shortest non-cyclic, unfinished path (length " << shortest.value().elements.size() << "): " << shortest.value() << std::endl;
+            plt.clear();
+            plt.rSettings().overwriteFiles = true;
+            plt.rSettings().cummulative = false;
+            plt.setFilename("shortest_unfinished_path_" + std::to_string(maxJumps) + "_jumps");
+            addPathChronologously(shortest_node, plt);
+            plt.plot2d(hypro::PLOTTYPE::pdf);
+        }
 		if ( shortest_cycle ) {
-			std::cout << "Shortest cyclic, unfinished path: " << shortest_cycle.value() << std::endl;
-			plt.clear();
-			plt.rSettings().overwriteFiles = true;
-			plt.rSettings().cummulative = true;
-			plt.setFilename( "shortest_cyclic_path" );
-			while ( shortest_cycle_node != nullptr ) {
-				for ( const auto& seg : shortest_cycle_node->getFlowpipe() ) {
-					plt.addObject( seg.projectOn( { 0, 1 } ).vertices() );
-				}
-				shortest_cycle_node = shortest_cycle_node->getParent();
-			}
-			plt.plot2d( hypro::PLOTTYPE::pdf );
-			plt.plot2d( hypro::PLOTTYPE::gen );
-		}
-	}
+            std::cout << "Shortest cyclic, unfinished path (length " << shortest_cycle.value().elements.size() << "): " << shortest_cycle.value() << std::endl;
+            std::cout << "Flowpipe of the second to last node:\n";
+            for (const auto &seg: shortest_cycle_node->getParent()->getFlowpipe()) {
+                std::cout << seg << "\n";
+            }
+            std::cout << std::endl;
+            plt.clear();
+            plt.rSettings().overwriteFiles = true;
+            plt.rSettings().cummulative = true;
+            plt.setFilename("shortest_cyclic_path");
+            addPathChronologously(shortest_cycle_node, plt);
+            plt.plot2d(hypro::PLOTTYPE::pdf);
+            plt.plot2d(hypro::PLOTTYPE::gen);
+        }
+        if (shortest_timelock) {
+            std::cout << "Shortest timelock path (length " << shortest_timelock.value().elements.size() << "): " << shortest_timelock.value() << std::endl;
+            plt.clear();
+            plt.rSettings().overwriteFiles = true;
+            plt.rSettings().cummulative = true;
+            plt.setFilename("shortest_timelock_path");
+            addPathChronologously(shortest_timelock_node, plt);
+            plt.plot2d(hypro::PLOTTYPE::pdf);
+            plt.plot2d(hypro::PLOTTYPE::gen);
+        }
+    }
 
 	// only plot the last run
 	// plottingtrue
@@ -249,31 +274,29 @@ static void Simplex_Watertanks_Reachability( ::benchmark::State& state ) {
 		  "_TH." + std::to_string( state.range( 0 ) ) + "_jumps";
 	TRACE( channel, "#################################################" );
 	*/
-
 	std::cout << "Start plotting, " << std::to_string( last_paths.size() ) << " paths." << std::endl;
 	std::size_t path_count{ 0 };
 	for ( auto [path, leaf] : last_paths ) {
-		// TRACE( channel, path );
-		plt.clear();
-		plt.rSettings().overwriteFiles = true;
-		plt.rSettings().cummulative = false;
-		plt.setFilename( "unfinished_" + std::to_string( path_count ) );
-		while ( leaf != nullptr ) {
-			for ( const auto& seg : leaf->getFlowpipe() ) {
-				plt.addObject( seg.projectOn( { 0, 1 } ).vertices() );
-			}
-			leaf = leaf->getParent();
-		}
-		plt.plot2d( hypro::PLOTTYPE::png );
-		std::cout << "Plotted path " << std::to_string( path_count ) << std::endl;
-		++path_count;
-	}
+        // TRACE( channel, path );
+        plt.clear();
+        plt.rSettings().overwriteFiles = true;
+        plt.rSettings().cummulative = false;
+        plt.setFilename("unfinished_" + std::to_string(maxJumps) + "_jumps_" + std::to_string(path_count));
+        std::ofstream filestream;
+        filestream.open("unfinished_" + std::to_string(maxJumps) + "_jumps_" + std::to_string(path_count) + ".txt");
+        filestream << path << std::endl;
+        filestream.close();
+        addPathChronologously(leaf, plt);
+        plt.plot2d(hypro::PLOTTYPE::png);
+        //std::cout << "Plotted path " << std::to_string( path_count ) << std::endl;
+        ++path_count;
+    }
 	PRINT_STATS()
 }
 // Register the function as a benchmark
 // BENCHMARK_TEMPLATE( Simplex_Watertanks_Reachability, hypro::SupportFunction<double> )->DenseRange(1, 3, 1);
 BENCHMARK_TEMPLATE( Simplex_Watertanks_Reachability, hypro::Box<double> )
-        ->DenseRange(100, 200, 10)
+        ->DenseRange(26, 26, 1)
 	  ->Unit( ::benchmark::kSecond );
 
 }  // namespace hypro::benchmark
