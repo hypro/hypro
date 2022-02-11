f1fc1890b3bee968db98a9a5975fe4cbc61ce596
Stefan Schupp <stefan.schupp@tuwien.ac.at>
2022-02-11 15:08:13 +0100

    add dbg-output

--- test/case_studies/cs_simplex_architecture_watertanks_interactive.cpp
+++ test/case_studies/cs_simplex_architecture_watertanks_interactive.cpp
@@ -19,35 +19,36 @@
 #include <string>
 
 void Simplex_Watertanks_Reachability( std::size_t maxJumps = 5 ) {
-    using Number = double;
-    using Representation = hypro::Box<Number>;
+	using Number = double;
+	using Representation = hypro::Box<Number>;
 
-    std::string filename{"21_simplex_watertanks_deterministic_monitor_dbg_init_ticks.model"};
+	std::string filename{ "21_simplex_watertanks_deterministic_monitor_dbg_init_ticks.model" };
 
-    auto[automaton, reachSettings] = hypro::parseFlowstarFile<Number>(hypro::getCSModelsPath() + filename);
+	auto [automaton, reachSettings] = hypro::parseFlowstarFile<Number>( hypro::getCSModelsPath() + filename );
 
-    // artificially bloat dimensions 0,1,3 to a width of 0.01
-    auto initialStates = automaton.getInitialStates();
-    for (auto &[loc, condition]: initialStates) {
-        auto box = hypro::Box<Number>(condition.getMatrix(), condition.getVector());
-        auto intervals = box.intervals();
-        for (std::size_t i = 0; i < 5; ++i) {
+	// artificially bloat dimensions 0,1,3 to a width of 0.01
+	auto initialStates = automaton.getInitialStates();
+	for ( auto& [loc, condition] : initialStates ) {
+		auto box = hypro::Box<Number>( condition.getMatrix(), condition.getVector() );
+		auto intervals = box.intervals();
+		for ( std::size_t i = 0; i < 5; ++i ) {
 			if ( i == 0 || i == 1 ) {  //|| i == 3) {
 				intervals[i] = carl::Interval<Number>( intervals[i].lower() - 0.05, intervals[i].upper() + 0.05 );
 			}
 		}
-        condition = hypro::Condition<Number>(intervals);
-    }
-    automaton.setInitialStates(initialStates);
+		condition = hypro::Condition<Number>( intervals );
+	}
+	automaton.setInitialStates( initialStates );
 
-    std::cout << "automaton \n" << automaton << std::endl;
+	std::cout << "automaton \n"
+			  << automaton << std::endl;
 
-    auto settings = hypro::convert(reachSettings);
-    settings.rStrategy().front().detectJumpFixedPoints = true;
-    settings.rStrategy().front().detectContinuousFixedPointsLocally = false;
-    // settings.rFixedParameters().localTimeHorizon = 1;
-    settings.rFixedParameters().jumpDepth = maxJumps;
-    settings.rStrategy().begin()->aggregation = hypro::AGG_SETTING::AGG;
+	auto settings = hypro::convert( reachSettings );
+	settings.rStrategy().front().detectJumpFixedPoints = true;
+	settings.rStrategy().front().detectContinuousFixedPointsLocally = false;
+	// settings.rFixedParameters().localTimeHorizon = 1;
+	settings.rFixedParameters().jumpDepth = maxJumps;
+	settings.rStrategy().begin()->aggregation = hypro::AGG_SETTING::AGG;
 
 	auto roots = hypro::makeRoots<Representation>( automaton );
 	auto reacher = hypro::reachability::Reach<Representation>( automaton, settings.fixedParameters(),
@@ -61,6 +62,6 @@ void Simplex_Watertanks_Reachability( std::size_t maxJumps = 5 ) {
 }
 
 int main() {
-    Simplex_Watertanks_Reachability(100);
+	Simplex_Watertanks_Reachability( 100 );
 	return 0;
 }
