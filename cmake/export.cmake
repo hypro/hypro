75b23a8c5620ee9e0dc000192cd1022363b2d533
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2022-02-21 09:36:20 +0100

    minor cmake cleanup and formatting

--- cmake/export.cmake
+++ cmake/export.cmake
@@ -7,23 +7,23 @@ export(TARGETS ${HYPRO_TARGETS} FILE "${PROJECT_BINARY_DIR}/hyproTargets.cmake")
 
 # Export the package for use from the build-tree
 # (this registers the build-tree with a global CMake-registry)
-if(EXPORT_TO_CMAKE)
-	message(STATUS "Exporting to cmake")
-	set(CMAKE_EXPORT_PACKAGE_REGISTRY True)
-	export(PACKAGE ${PROJECT_NAME})
-else()
-	message(STATUS "Not exporting to cmake")
-endif()
+if (EXPORT_TO_CMAKE)
+    message(STATUS "Exporting to cmake")
+    set(CMAKE_EXPORT_PACKAGE_REGISTRY True)
+    export(PACKAGE ${PROJECT_NAME})
+else ()
+    message(STATUS "Not exporting to cmake")
+endif ()
 
-if(STATICLIB_SWITCH)
-	if(HYPRO_MATLAB_BINDINGS)
-		export_target(DEPENDENT_TARGETS Matlab_STATIC)
-	endif()
-else()
-	if(HYPRO_MATLAB_BINDINGS)
-		#export_target(DEPENDENT_TARGETS Matlab_SHARED)
-	endif()
-endif()
+if (STATICLIB_SWITCH)
+    if (HYPRO_MATLAB_BINDINGS)
+        export_target(DEPENDENT_TARGETS Matlab_STATIC)
+    endif ()
+else ()
+    if (HYPRO_MATLAB_BINDINGS)
+        #export_target(DEPENDENT_TARGETS Matlab_SHARED)
+    endif ()
+endif ()
 
 export_target(DEPENDENT_TARGETS GLPK_STATIC)
 export_target(DEPENDENT_TARGETS ANTLR4_STATIC)
@@ -44,23 +44,23 @@ if (HYPRO_USE_SMTRAT)
     export_target(DEPENDENT_TARGETS smtrat-module-LRAModule-static)
 endif ()
 if (HYPRO_USE_Z3)
-	export_target(DEPENDENT_TARGETS z3_SHARED)
+    export_target(DEPENDENT_TARGETS z3_SHARED)
 endif ()
 if (HYPRO_GSL_INTEGRATION)
-	export_target(DEPENDENT_TARGETS GSL_SHARED)
-	export_target(DEPENDENT_TARGETS GSL_STATIC)
+    export_target(DEPENDENT_TARGETS GSL_SHARED)
+    export_target(DEPENDENT_TARGETS GSL_STATIC)
 endif ()
 if (HYPRO_HAS_GRAPHVIZ)
-	export_target(DEPENDENT_TARGETS CGRAPH_SHARED)
-	export_target(DEPENDENT_TARGETS GVC_SHARED)
+    export_target(DEPENDENT_TARGETS CGRAPH_SHARED)
+    export_target(DEPENDENT_TARGETS GVC_SHARED)
 endif ()
 
 
 include(CMakePackageConfigHelpers)
 
 write_basic_package_version_file(${CMAKE_CURRENT_BINARY_DIR}/hyproConfigVersion.cmake
-		VERSION ${hypro_VERSION}
-		COMPATIBILITY SameMajorVersion)
+        VERSION ${hypro_VERSION}
+        COMPATIBILITY SameMajorVersion)
 
 set(TARGET ${PROJECT_NAME})
 
@@ -70,20 +70,20 @@ set(CONF_INCLUDES ${INCLUDES})
 set(CONF_INCLUDE_DIRS "${CMAKE_CURRENT_SOURCE_DIR}/include")
 
 configure_package_config_file(cmake/hyproConfig.cmake.in ${CMAKE_CURRENT_BINARY_DIR}/hyproConfig.cmake
-							  INSTALL_DESTINATION ${PROJECT_BINARY_DIR}
-							  PATH_VARS PROJECT_BINARY_DIR #SYSCONFIG_INSTALL_DIR
-							 )
+        INSTALL_DESTINATION ${PROJECT_BINARY_DIR}
+        PATH_VARS PROJECT_BINARY_DIR #SYSCONFIG_INSTALL_DIR
+        )
 
 file(RELATIVE_PATH REL_INCLUDE_DIR "${CMAKE_INSTALL_DIR}" "${INCLUDE_INSTALL_DIR}")
 # ... for the install tree
 set(CONF_INCLUDE_DIRS "\${hypro_CMAKE_DIR}/${REL_INCLUDE_DIR}")
 
 configure_package_config_file(cmake/hyproConfig.cmake.in ${CMAKE_CURRENT_BINARY_DIR}/installedConfig/hyproConfig.cmake
-							  INSTALL_DESTINATION ${CMAKE_INSTALL_DIR}
-							  PATH_VARS INCLUDE_INSTALL_DIR #SYSCONFIG_INSTALL_DIR
-			 )
+        INSTALL_DESTINATION ${CMAKE_INSTALL_DIR}
+        PATH_VARS INCLUDE_INSTALL_DIR #SYSCONFIG_INSTALL_DIR
+        )
 install(FILES ${CMAKE_CURRENT_BINARY_DIR}/installedConfig/hyproConfig.cmake ${CMAKE_CURRENT_BINARY_DIR}/hyproConfigVersion.cmake
-		DESTINATION ${CMAKE_INSTALL_DIR} )
+        DESTINATION ${CMAKE_INSTALL_DIR})
 
 install(EXPORT ${PROJECT_NAME} FILE "${PROJECT_NAME}Targets.cmake" DESTINATION ${CMAKE_INSTALL_DIR})
 
