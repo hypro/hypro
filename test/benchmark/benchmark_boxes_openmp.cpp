4f084d5585e62da3f9022fb121673f9068166706
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2022-05-25 13:14:16 +0200

    extend benchmark for boxes and openmp to vertices-computation
    Signed-off-by: Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>

--- test/benchmark/benchmark_boxes_openmp.cpp
+++ test/benchmark/benchmark_boxes_openmp.cpp
@@ -2,9 +2,9 @@
  * Copyright (c) 2022.
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 /*
@@ -39,7 +39,21 @@ static void boxAffineTransformation( ::benchmark::State& st ) {
 	}
 }
 
-BENCHMARK( boxAffineTransformation )->DenseRange( 1, 100, 2 );
+static void boxVertices( ::benchmark::State& st ) {
+	using Number = double;
+	// initialize random number generator
+	std::mt19937 generator{ 123 };
+	std::uniform_int_distribution<int> dist = std::uniform_int_distribution<int>( 0, 10 );
+
+	Eigen::Index d{ st.range( 0 ) };
+	hypro::Box<Number> box{ std::vector<carl::Interval<Number>>{ d, carl::Interval<Number>{ -1, 1 } } };
+	for ( auto _ : st ) {
+		::benchmark::DoNotOptimize( box.vertices() );
+	}
+}
+
+// BENCHMARK( boxAffineTransformation )->DenseRange( 1, 100, 5 );
+BENCHMARK( boxVertices )->DenseRange( 1, 20, 1 );
 // BENCHMARK_MAIN();
 
 }  // namespace hypro::benchmark
