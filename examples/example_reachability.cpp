bd0c67d58c67c4acbfd008b866f398422f171671
Benedikt Maderbacher <benedikt.maderbacher@iaik.tugraz.at>
2022-05-25 17:43:37 +0200

    reachability example fix plotter

--- examples/example_reachability.cpp
+++ examples/example_reachability.cpp
@@ -89,14 +89,8 @@ static void computeReachableStates( const std::string& filename,
 		plotter.setFilename( extendedFilename );
 		std::vector<std::size_t> plottingDimensions =
 			  settings.plotting().plotDimensions.at( 0 );
-
-		for ( size_t plottingDimension : plottingDimensions ) {
-			std::cout << plottingDimension << " ";
-		}
-		std::cout << std::endl;
-		
-		plotter.rSettings().dimensions.first = plottingDimensions.front();
-		plotter.rSettings().dimensions.second = plottingDimensions.back();
+		plotter.rSettings().dimensions.push_back(plottingDimensions.front());
+		plotter.rSettings().dimensions.push_back(plottingDimensions.back());
 		plotter.rSettings().cummulative = false;
 
 		// bad states plotting
@@ -158,8 +152,8 @@ int main( int argc, char** argv ) {
 #ifdef USE_CLN_NUMBERS
 	using Number = cln::cl_RA;
 #else
-	using Number = mpq_class;
-	// using Number = double;
+	//using Number = mpq_class;
+	using Number = double;
 #endif
 
 	switch ( rep ) {
