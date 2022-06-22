7b91d3feb39d5f2190f51de6b4746693e6fe750c
Stefan Schupp <stefan.schupp@tuwien.ac.at>
2022-06-22 17:45:04 +0200

    fix bug in flowstar output
    Signed-off-by: Stefan Schupp <stefan.schupp@tuwien.ac.at>

--- include/hypro/datastructures/HybridAutomaton/output/Flowstar.h
+++ include/hypro/datastructures/HybridAutomaton/output/Flowstar.h
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2022.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #pragma once
 #include "../../reachability/Settings.h"
 #include "../Condition.h"
@@ -103,7 +112,7 @@ std::string toFlowstarFormat( const flowVariant<Number>& f,
 		case DynamicType::discrete:
 		case DynamicType::linear: {
 			matrix_t<Number> flow = std::get<linearFlow<Number>>( f ).getFlowMatrix();
-			for ( Eigen::Index rowI = 0; rowI < flow.rows(); ++rowI ) {
+			for ( Eigen::Index rowI = 0; rowI < flow.rows() - 1; ++rowI ) {
 				std::stringstream tmp;
 				tmp << prefix << "\t\t" << varNameMap.at( rowI ) << "' = ";
 				out << toFlowstarFormat( matrix_t<Number>( flow.row( rowI ) ), varNameMap, tmp.str() );
