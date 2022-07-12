a80384006d1d36bb1b992a9d57e12718eb5f2841
Stefan Schupp <stefan.schupp@tuwien.ac.at>
2022-07-12 08:23:18 +0200

    generalize conversion methods from and to carl constraints
    Signed-off-by: Stefan Schupp <stefan.schupp@tuwien.ac.at>

--- include/hypro/representations/Polytopes/carlPolytope/util.h
+++ include/hypro/representations/Polytopes/carlPolytope/util.h
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2022.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #pragma once
 #include "../../../carlTypes.h"
 #include "../../../datastructures/Halfspace.h"
@@ -7,14 +16,14 @@
 namespace hypro {
 
 template <typename N, typename D>
-inline ConstraintT<N> halfspaceToConstraint( const vector_t<D>& normal, const D& offset ) {
+inline ConstraintT<N> rowToConstraint( const vector_t<D>& normal, const D& offset, carl::Relation rel = carl::Relation::LEQ ) {
 	PolyT<N> p;
 	for ( Eigen::Index col = 0; col < normal.rows(); ++col ) {
 		p += carl::convert<D, N>( normal( col ) ) *
 			 PolyT<N>( VariablePool::getInstance().carlVarByIndex( col ) );
 	}
 	p -= carl::convert<D, N>( offset );
-	return ConstraintT<N>( p, carl::Relation::LEQ );
+	return ConstraintT<N>( p, rel );
 }
 
 template <typename N, typename D>
@@ -22,7 +31,17 @@ inline FormulasT<N> halfspacesToFormulas( const matrix_t<D>& constraints, const 
 	FormulasT<N> newConstraints;
 
 	for ( Eigen::Index row = 0; row < constraints.rows(); ++row ) {
-		newConstraints.emplace_back( FormulaT<N>( halfspaceToConstraint<N, D>( vector_t<D>( constraints.row( row ) ), constants( row ) ) ) );
+		newConstraints.emplace_back( FormulaT<N>( rowToConstraint<N, D>( vector_t<D>( constraints.row( row ) ), constants( row ), carl::Relation::LEQ ) ) );
+	}
+	return newConstraints;
+}
+
+template <typename N, typename D>
+inline FormulasT<N> equationsToFormulas( const matrix_t<D>& constraints, const vector_t<D>& constants ) {
+	FormulasT<N> newConstraints;
+
+	for ( Eigen::Index row = 0; row < constraints.rows(); ++row ) {
+		newConstraints.emplace_back( FormulaT<N>( rowToConstraint<N, D>( vector_t<D>( constraints.row( row ) ), constants( row ), carl::Relation::EQ ) ) );
 	}
 	return newConstraints;
 }
@@ -32,7 +51,17 @@ inline ConstraintsT<N> halfspacesToConstraints( const matrix_t<D>& constraints, 
 	ConstraintsT<N> newConstraints;
 
 	for ( Eigen::Index row = 0; row < constraints.rows(); ++row ) {
-		newConstraints.emplace_back( halfspaceToConstraint<N, D>( constraints.row( row ), constants( row ) ) );
+		newConstraints.emplace_back( rowToConstraint<N, D>( constraints.row( row ), constants( row ), carl::Relation::LEQ ) );
+	}
+	return newConstraints;
+}
+
+template <typename N, typename D>
+inline ConstraintsT<N> equationsToConstraints( const matrix_t<D>& constraints, const vector_t<D>& constants ) {
+	ConstraintsT<N> newConstraints;
+
+	for ( Eigen::Index row = 0; row < constraints.rows(); ++row ) {
+		newConstraints.emplace_back( rowToConstraint<N, D>( constraints.row( row ), constants( row ), carl::Relation::EQ ) );
 	}
 	return newConstraints;
 }
