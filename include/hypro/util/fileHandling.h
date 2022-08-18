93c2987bc702e52159d1d29ee475fd68d245f35e
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2022-08-18 12:28:41 +0200

    move fileExists function to separate fileHandling header
    Signed-off-by: Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>

--- include/hypro/util/fileHandling.h
+++ include/hypro/util/fileHandling.h
@@ -7,37 +7,23 @@
  *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-#pragma once
+/*
+ * Created by Stefan Schupp <stefan.schupp@tuwien.ac.at> on 18.08.22.
+ */
 
-#include <algorithm>
-#include <cassert>
-#include <cmath>
-#include <iostream>
+#ifndef HYPRO_FILEHANDLING_H
+#define HYPRO_FILEHANDLING_H
+
 #include <string>
 #include <sys/stat.h>
-#include <vector>
 
 namespace hypro {
 
-std::vector<std::size_t> HEXtoRGB( std::size_t color );
-std::size_t RGBtoHEX( std::vector<std::size_t> color );
-
-std::vector<double> RGBtoHSV( const std::vector<std::size_t>& color );
-std::vector<std::size_t> HSVtoRGB( const std::vector<double>& color );
-
-std::vector<std::size_t> adjustSaturation( const std::vector<std::size_t>& rgbColor, double factor );
-std::size_t adjustSaturation( std::size_t rgbColor, double factor );
-
-template <typename N>
-class Point;
-
-template <typename N>
-std::vector<Point<N>> reduceToDimensions( const std::vector<Point<N>>& vertices, const std::vector<std::size_t>& dims ) {
-	auto res = vertices;
-	for ( auto& p : res ) {
-		p = p.reduceToDimensions( dims );
-	}
-	return res;
+inline bool file_exists( const std::string& filename ) {
+	struct stat buffer;
+	return ( stat( filename.c_str(), &buffer ) == 0 );
 }
 
 }  // namespace hypro
+
+#endif	// HYPRO_FILEHANDLING_H
