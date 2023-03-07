1eac2c1f2d976021628f7a5f3f6b05080581e1fc
Stefan Schupp <stefan.schupp@tuwien.ac.at>
2023-03-07 17:06:11 +0100

    fix example_reachability.cpp
    Signed-off-by: Stefan Schupp <stefan.schupp@tuwien.ac.at>

--- include/hypro/datastructures/reachability/ReachTreev2.h
+++ include/hypro/datastructures/reachability/ReachTreev2.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022.
+ * Copyright (c) 2022-2023.
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
@@ -187,8 +187,8 @@ std::ostream& operator<<( std::ostream& out, const std::vector<ReachTreeNode<Rep
 
 /**
  * @brief Convenience function to create roots of a search tree from the initial states of the passed hybrid automaton.
- * @tparam Representation
- * @tparam Number
+ * @tparam Representation The representation type used to represent state sets.
+ * @tparam Automaton The type of hybrid automaton used.
  * @param ha
  * @return std::vector<ReachTreeNode<Representation,Location>>
  */
