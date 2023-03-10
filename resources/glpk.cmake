bfdc669b3c18057a48dd4b5d28ae5abfa217293f
Stefan Schupp <stefan.schupp@tuwien.ac.at>
2023-03-10 08:38:29 +0100

    pass gmp-library information from CArL to glpk during build
    Signed-off-by: Stefan Schupp <stefan.schupp@tuwien.ac.at>

--- resources/glpk.cmake
+++ resources/glpk.cmake
@@ -1,12 +1,17 @@
 set(glpk_version "5.0")
 
+# get gmp location
+get_target_property(gmp_include_dir GMP_SHARED INTERFACE_INCLUDE_DIRECTORIES)
+get_target_property(gmp_library_dir GMP_SHARED IMPORTED_LOCATION)
+message(STATUS "glpk use gmp at ${gmp_library_dir} with include dir ${gmp_include_dir}")
+
 # create build folder to be able to use it as an include folder
 file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/resources/src/glpk-build/include)
 
 ExternalProject_Add(
         glpk
         SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/glpk-${glpk_version}"
-        CONFIGURE_COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/glpk-${glpk_version}/configure --prefix=${CMAKE_BINARY_DIR}/resources/src/glpk-build --with-gmp --with-pic CPPFLAGS=-fPIC --disable-shared --disable-dl --enable-static
+        CONFIGURE_COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/glpk-${glpk_version}/configure --prefix=${CMAKE_BINARY_DIR}/resources/src/glpk-build --with-gmp --with-pic LDFLAGS=-L${gmp_library_dir} CFLAGS=-I${gmp_include_dir} CPPFLAGS=-fPIC --disable-shared --disable-dl --enable-static
         # build in source is needed here, as configure moves all relevant files to glpk-build and then the make and make install need to know that it is
         # in-source (from this perspective on)
         BUILD_IN_SOURCE 1
