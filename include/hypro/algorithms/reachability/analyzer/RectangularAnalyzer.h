8f889ccb941f36021dd613d921e0645723b307ad
Stefan Schupp <stefan.schupp@tuwien.ac.at>
2023-03-13 11:27:31 +0100

    enable multithreading for rectangular analyzer
    Signed-off-by: Stefan Schupp <stefan.schupp@tuwien.ac.at>

--- include/hypro/algorithms/reachability/analyzer/RectangularAnalyzer.h
+++ include/hypro/algorithms/reachability/analyzer/RectangularAnalyzer.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022.
+ * Copyright (c) 2022-2023.
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
@@ -15,12 +15,15 @@
 #include <queue>
 
 namespace hypro {
+
 /**
  * @brief Class which implements a forward reachability analysis method for rectangular automata
  * @details Requires the usage of a suitable state set representation, e.g., carlPolytope.
- * @tparam State
+ * @tparam State state set representation type (must be polyhedral)
+ * @tparam Automaton hybrid automaton type
+ * @tparam Multithreading enable/disable multithreading
  */
-template <typename State, typename Automaton>
+template <typename State, typename Automaton, typename Multithreading = NoMultithreading>
 class RectangularAnalyzer {
 	static_assert( std::is_same<typename State::NumberType, typename Automaton::NumberType>() );
 	using Number = typename State::NumberType;
@@ -42,12 +45,60 @@ class RectangularAnalyzer {
 	/// getter for computed flowpipes
 	const std::vector<Flowpipe<State>>& getFlowpipes() const { return mFlowpipes; }
 
+	void addToQueue( ReachTreeNode<State, LocationT>* node ) {
+		if ( std::is_same_v<Multithreading, UseMultithreading> ) {
+			{
+				std::unique_lock<std::mutex> lock( mQueueMutex );
+				mWorkQueue.push( node );
+			}
+			mQueueNonEmpty.notify_one();
+		} else {
+			mWorkQueue.push( node );
+		}
+	}
+
+	ReachTreeNode<State, LocationT>* getNodeFromQueue() {
+		auto* res = mWorkQueue.front();
+		mWorkQueue.pop();
+		return res;
+	}
+
+	void shutdown() {
+		{
+			std::unique_lock<std::mutex> lock( mThreadPoolMutex );
+			mTerminate = true;	// use this flag in condition.wait
+		}
+
+		mQueueNonEmpty.notify_all();  // wake up all threads.
+
+		// Join all threads.
+		for ( std::thread& th : mThreads ) {
+			th.join();
+		}
+
+		mThreads.clear();
+		mStopped = true;
+	}
+
+  private:
+	REACHABILITY_RESULT processNode( RectangularWorker<State, Automaton>& worker, ReachTreeNode<State, LocationT>* node );
+
   protected:
 	std::queue<ReachTreeNode<State, LocationT>*> mWorkQueue;   ///< Queue holds all nodes that require processing
 	std::vector<Flowpipe<State>> mFlowpipes;				   ///< Storage for already computed flowpipes
 	HybridAutomaton<Number> mHybridAutomaton;				   ///< Automaton which is analyzed
 	Settings mAnalysisSettings;								   ///< Settings used for analysis
 	std::vector<ReachTreeNode<State, LocationT>>& mReachTree;  ///< Forest of ReachTrees computed
+	int mNumThreads = std::thread::hardware_concurrency();	   ///< number of used threads
+	std::vector<std::thread> mThreads;						   ///< vector of threads
+	std::vector<bool> mIdle;								   ///< vector of idle threads
+	std::mutex mQueueMutex;									   ///< mutex to access the queue
+	std::mutex mThreadPoolMutex;							   ///< mutex for the thread pool itself
+	std::condition_variable mQueueNonEmpty;					   ///< notification variable to indicate the queue is nonempty
+	std::atomic<bool> mTerminate = false;					   ///< indicates termination request
+	std::atomic<bool> mStopped = false;						   ///< indicator, whether shutdown was already invoked
+	std::mutex mIdleWorkerMutex;							   ///< mutex to access the idle-flag array
+	std::condition_variable mAllIdle;						   ///< notification variable to trigger shutdown of the threadpool
 };
 
 }  // namespace hypro
