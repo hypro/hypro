8dca952f5439990c6189de263edb46ef6291f254
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2023-04-03 16:14:44 +0200

    fix example_boxIcpIntersection.cpp
    Signed-off-by: Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>

--- examples/example_boxIcpIntersection.cpp
+++ examples/example_boxIcpIntersection.cpp
@@ -1,12 +1,24 @@
-#include "datastructures/Halfspace.h"
-#include "representations/GeometricObjectBase.h"
+/*
+ * Copyright (c) 2023.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/**
+ * \example example_boxIcpIntersection.cpp This example shows how a box is intersected with a
+ * halfspace using a method based on Interval Constraint Propagation (ICP), i.e., exploiting
+ * interval arithmetic.
+ */
+
+#include <hypro/datastructures/Halfspace.h>
+#include <hypro/representations/GeometricObjectBase.h>
 
 int main() {
 	// use rational arithmetic.
 	typedef mpq_class Number;
-
-	// hypro::Box<Number> testbox(std::make_pair(hypro::Point<Number>({1,1}),
-	// hypro::Point<Number>({2,2})));
 
 	std::vector<carl::Interval<Number>> intervals;
 	intervals.emplace_back( 1, 2 );
