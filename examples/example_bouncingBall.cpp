29c312bd689bab7dfc0b8f8aae7f466df5f13e69
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2023-04-03 16:05:42 +0200

    fix example_bouncingBall.cpp
    Signed-off-by: Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>

--- examples/example_bouncingBall.cpp
+++ examples/example_bouncingBall.cpp
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2023.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 /**
  * \example example_bouncingBall.cpp
  * An example file showing how to construct the automaton for a bouncing ball
@@ -23,7 +32,7 @@ int main() {
 	HybridAutomaton<Number> bBallAutomaton = HybridAutomaton<Number>();
 	Location<Number>* loc1 = bBallAutomaton.createLocation();
 	// Transition<Number>* trans = loc1->createTransition(loc1);
-	Transition<hypro::Location<Number>>* trans = loc1->createTransition(loc1);
+	Transition<hypro::Location<Number>>* trans = loc1->createTransition( loc1 );
 
 	// matrix defining the flow (note: 3rd dimension for constant parts).
 	matrix_t<Number> flowMatrix = matrix_t<Number>( 3, 3 );
@@ -93,7 +102,7 @@ int main() {
 	reset.setMatrix( linearReset );
 
 	// setup transition
-	trans->setAggregation( Aggregation::aggregation );
+	trans->setAggregation( AGG_SETTING::AGG );
 	trans->setGuard( guard );
 	trans->setSource( loc1 );
 	trans->setTarget( loc1 );
@@ -148,15 +157,15 @@ int main() {
 	Plotter<Number>& plotter = Plotter<Number>::getInstance();
 	plotter.setFilename( "bouncingBall" );
 
-	for(const auto& node : preorder(roots)) {
-		for(const auto& state_set : node.getFlowpipe()) {	// flowpipes
-			plotter.addObject(state_set.projectOn({0,1}).vertices());
+	for ( const auto& node : preorder( roots ) ) {
+		for ( const auto& state_set : node.getFlowpipe() ) {  // flowpipes
+			plotter.addObject( state_set.projectOn( { 0, 1 } ).vertices() );
 		}
 	}
 	std::cout << "done." << std::endl;
 
 	// write output.
-	plotter.plot2d(PLOTTYPE::png);
+	plotter.plot2d( PLOTTYPE::png );
 
 	return 0;
 }
