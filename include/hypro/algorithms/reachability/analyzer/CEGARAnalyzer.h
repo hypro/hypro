62c99e9802eb7804649e11bc7791756ffb29cfea
Stefan Schupp <stefan.schupp@tuwien.ac.at>
2023-05-11 11:01:04 +0200

    fix bug in hydra cegar analyzer
    Signed-off-by: Stefan Schupp <stefan.schupp@tuwien.ac.at>

--- include/hypro/algorithms/reachability/analyzer/CEGARAnalyzer.h
+++ include/hypro/algorithms/reachability/analyzer/CEGARAnalyzer.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023.
+ * Copyright (c) 2023-2023.
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
@@ -70,7 +70,7 @@ namespace hypro {
                     if (rootIt == roots.end()) {
                         auto const &condition = automaton.getInitialStates().at(loc);
                         return roots.emplace_back(loc, Representation{condition.getMatrix(), condition.getVector()},
-                                                  carl::Interval{0, 0});
+                                                  carl::Interval<SegmentInd>{0, 0});
                     }
                     return *rootIt;
                 }
