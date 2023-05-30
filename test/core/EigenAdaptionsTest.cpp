ca360243e54960bd6cca17a2f292a3c3c6af4e1a
Stefan Schupp <stefan.schupp@tuwien.ac.at>
2023-05-30 14:27:52 +0200

    extend test to addRows
    Signed-off-by: Stefan Schupp <stefan.schupp@tuwien.ac.at>

--- test/core/EigenAdaptionsTest.cpp
+++ test/core/EigenAdaptionsTest.cpp
@@ -1,9 +1,18 @@
+/*
+ * Copyright (c) 2023.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #include "test/defines.h"
 
 #include "gtest/gtest.h"
 #include <hypro/util/adaptions_eigen/adaptions_eigen.h>
 
-template <typename Number>
+template<typename Number>
 class EigenAdaptionsTest : public ::testing::Test {
 protected:
     virtual void SetUp() {
@@ -68,9 +77,13 @@ TYPED_TEST(EigenAdaptionsTest, addRows) {
     hypro::appendRow(resA, row4);
     EXPECT_EQ(this->matB, resA);
 
-    hypro::matrix_t<TypeParam> resB = hypro::matrix_t<TypeParam>(3, 4);
-    resB << 1, 2, 3, 4, 9, 10, 11, 12, 13, 14, 15, 16;
-    EXPECT_EQ(resB, hypro::removeRows(this->matB, std::vector<Eigen::Index>({1})));
+    hypro::matrix_t<TypeParam> resB = hypro::matrix_t<TypeParam>(0, 2);
+    hypro::vector_t<TypeParam> row1 = hypro::vector_t<TypeParam>(2);
+    row1 << 1, 2;
+    hypro::appendRow(resB, row1);
+    hypro::matrix_t<TypeParam> expectedB = hypro::matrix_t<TypeParam>(1, 2);
+    expectedB << 1, 2;
+    EXPECT_EQ(expectedB, resB);
 }
 
 TYPED_TEST(EigenAdaptionsTest, removeCol) {
