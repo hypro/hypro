321cc0388f154c5926ad5849caed1d6cafaa77ca
Hana Masara <hana.masara@rwth-aachen.de>
2023-06-13 07:09:35 +0200

    Add Sonar benchmarks verification

---
+++ examples/example_robustness.cpp
@@ -0,0 +1,97 @@
+#include "hypro/datastructures/Point.h"
+#include "hypro/neuralnets/network/NeuralNetwork.h"
+#include "hypro/neuralnets/reachability/ReachNN.h"
+#include "hypro/neuralnets/reachability_tree/ReachabilityTree.h"
+#include "hypro/parser/neuralnets/nnet/NNet.h"
+#include "hypro/parser/representations/parseHPolytope.tpp"
+#include "hypro/representations/GeometricObjectBase.h"
+#include "hypro/representations/Starset/Starset.h"
+#include "hypro/representations/sampling/sampling.h"
+
+#include <chrono>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+
+typedef double Number;
+
+int main( int argc, char* argv[] ) {
+	std::cout << "\nSystem arguments: ";
+	for ( int i = 0; i < argc; i++ ) {
+		std::cout << argv[i] << " ";
+	}
+	std::cout << "\n\n";
+
+	// Define the reachability method
+	hypro::NN_REACH_METHOD method = hypro::NN_REACH_METHOD::CEGAR;
+	if ( argc > 1 ) {
+		switch ( argv[1][0] ) {
+			case 'c':
+			case 'C':
+				method = hypro::NN_REACH_METHOD::CEGAR;
+				break;
+			case 'e':
+			case 'E':
+				method = hypro::NN_REACH_METHOD::EXACT;
+				break;
+			case 'o':
+			case 'O':
+				method = hypro::NN_REACH_METHOD::OVERAPPRX;
+				break;
+			default:
+				method = hypro::NN_REACH_METHOD::CEGAR;
+		}
+	}
+	std::cout << "The reachability method is " << method._to_string() << std::endl
+			  << std::endl;
+
+	// Define NN input file name
+	const char* filename;
+	if ( argc > 2 )
+		filename = argv[2];
+	std::cout << "NN input filename is: " << filename << std::endl;
+
+	// Read and build neural network
+	hypro::NNet<Number> nn_NNet = hypro::NNet<Number>( filename );
+	hypro::NeuralNetwork<Number> neuralNetwork = hypro::NeuralNetwork<Number>( nn_NNet );
+
+	// read an input property
+	hypro::HPolytope<Number> inputPoly;
+	if ( argc > 3 ) {
+		std::cout << "Reading input constraints from: " << argv[3] << std::endl;
+		inputPoly = hypro::readHpolytopeFromFile<Number>( argv[3] );
+	}
+	std::cout << "The input property:\n"
+			  << inputPoly << std::endl;
+
+	// apply forwardpass method + measure the time
+	auto start = std::chrono::steady_clock::now();
+	std::vector<hypro::Starset<Number>> output = neuralNetwork.forwardPass( hypro::Starset<Number>( inputPoly ) , method, false );
+
+
+
+
+	std::shared_ptr<hypro::LayerBase<Number>> layer1 = std::make_shared<hypro::HardSigmoidLayer<Number>>( 1, 0, -3, 3 );
+	auto result1 = layer1->forwardPass( output, method, false );
+
+
+	auto end = std::chrono::steady_clock::now();
+	auto analysisTime = std::chrono::duration_cast<std::chrono::milliseconds>( end - start ).count();
+	std::cout << "Total time elapsed during NN reachability analysis: " << analysisTime << " ms" << std::endl;
+
+
+	for ( const auto& o : output ) {
+		std::cout << "Vertices before hardsigmoid: " << o.vertices() << std::endl;
+	}
+
+	for ( const auto& o : result1 ) {
+		std::cout << "Vertices after hardsigmoid: " << o.vertices() << std::endl;
+	}
+
+	std::shared_ptr<hypro::LayerBase<Number>> layer2 = std::make_shared<hypro::StepFunctionLayer<Number>>( 1, 0, 0.5, 0, 1 );
+	auto result2 = layer2->forwardPass( result1, hypro::NN_REACH_METHOD::EXACT, false );
+
+	for ( const auto& o : result2 ) {
+		std::cout << "Vertices after stepfunction: " << o.vertices() << std::endl;
+	}
+}
---
+++ examples/nn_prop_inputs/sonar/convert_sonar.py
@@ -0,0 +1,24 @@
+import csv
+
+delta = 0.00001
+
+# Open the CSV file
+with open('sonar.csv', 'r') as file:
+    # Create a CSV reader object
+    reader = csv.reader(file)
+
+    j = 1
+    for i, row in enumerate(reader, start=1):
+        if (1 <= i <= 5) or (98 <= i <= 102):
+            with open(f'sonar_{j}_{row[-1]}.in', 'w') as target:
+                j += 1
+                target.write("120 60\n")
+                for k in range(len(row[:-1])):
+                    line = ['0'] * k + [row[k]] + ['0'] * (59 - k)
+                    line_negative = ['0'] * k + ["-" + row[k]] + ['0'] * (59 - k)
+                    target.write(' '.join(line) + '\n')
+                    target.write(' '.join(line_negative) + '\n')
+
+                for k in range(len(row[:-1])):
+                    target.write(str(delta) + "\n")
+                    target.write("-" + str(delta) + "\n")
