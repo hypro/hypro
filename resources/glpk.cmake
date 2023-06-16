020a80b4b53443795d6dcca2eeb77b3f37b387c2
Stefan Schupp <stefan.schupp@tuwien.ac.at>
2023-06-16 10:29:43 +0200

    re-arrange order of linkage to allow static builds
    Signed-off-by: Stefan Schupp <stefan.schupp@tuwien.ac.at>

--- resources/glpk.cmake
+++ resources/glpk.cmake
@@ -1,15 +1,15 @@
 set(glpk_version "5.0")
 
 # get gmp location
-get_target_property(gmp_include_dir GMP_SHARED INTERFACE_INCLUDE_DIRECTORIES)
-get_target_property(gmp_library_dir GMP_SHARED IMPORTED_LOCATION)
+#get_target_property(gmp_include_dir GMP_STATIC INTERFACE_INCLUDE_DIRECTORIES)
+#get_target_property(gmp_library_dir GMP_STATIC IMPORTED_LOCATION)
 
 # it may happen that the result is not only the path but includes the library as well - strip it then
-if (NOT IS_DIRECTORY ${gmp_library_dir})
-    get_filename_component(gmp_library_dir ${gmp_library_dir} DIRECTORY)
-endif ()
+#if (NOT IS_DIRECTORY ${gmp_library_dir})
+#    get_filename_component(gmp_library_dir ${gmp_library_dir} DIRECTORY)
+#endif ()
 
-message(STATUS "glpk use gmp at ${gmp_library_dir} with include dir ${gmp_include_dir}")
+#message(STATUS "glpk use gmp at ${gmp_library_dir} with include dir ${gmp_include_dir}")
 
 # create build folder to be able to use it as an include folder
 file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/resources/src/glpk-build/include)
@@ -17,7 +17,8 @@ file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/resources/src/glpk-build/include)
 ExternalProject_Add(
         glpk
         SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/glpk-${glpk_version}"
-        CONFIGURE_COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/glpk-${glpk_version}/configure --prefix=${CMAKE_BINARY_DIR}/resources/src/glpk-build --with-gmp --with-pic LDFLAGS=-L${gmp_library_dir} CFLAGS=-I${gmp_include_dir} CPPFLAGS=-fPIC --disable-shared --disable-dl --enable-static
+        #CONFIGURE_COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/glpk-${glpk_version}/configure --prefix=${CMAKE_BINARY_DIR}/resources/src/glpk-build --with-gmp --with-pic LDFLAGS=-L${gmp_library_dir} CFLAGS=-I${gmp_include_dir} CPPFLAGS=-fPIC --disable-shared --disable-dl --enable-static
+        CONFIGURE_COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/glpk-${glpk_version}/configure --prefix=${CMAKE_BINARY_DIR}/resources/src/glpk-build --with-gmp --with-pic CPPFLAGS=-fPIC --disable-shared --disable-dl --enable-static
         # build in source is needed here, as configure moves all relevant files to glpk-build and then the make and make install need to know that it is
         # in-source (from this perspective on)
         BUILD_IN_SOURCE 1
